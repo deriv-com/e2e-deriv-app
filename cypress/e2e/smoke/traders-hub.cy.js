25b65ca7a35189633c0517df7a53712d58e71be8
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-10-20 15:36:15 +0100

    Initial traders hub tests (#1)

--- cypress/e2e/smoke/traders-hub.cy.js
+++ cypress/e2e/smoke/traders-hub.cy.js
@@ -1,12 +1,21 @@
 import '@testing-library/cypress/add-commands'
 
-describe('Traders Hub', () => {
-
-  it('Access apps', () => {
-    cy.visitResponsive('/appstore/traders-hub', 'large')
-
-
+describe('QATEST-5778, QATEST-5781, QATEST-5615', () => {
+  beforeEach(() => {
+    cy.c_login()
+    cy.c_visitResponsive(Cypress.env('tradersHubUrl'), 'large')
   })
 
+  it('Verify account selector, Verify Real and Demo pages and total assets', () => {
+
+    cy.findByTestId('dti_dropdown_display').findByText('Real').click()
+    cy.get('#demo').click()
+    cy.findByText('Swap-Free Demo').should('be.visible')
+    //TODO Match totals
+    cy.findByTestId('dti_dropdown_display').findByText('Demo').click()
+    cy.get('#real').click()
+    cy.findByText('Swap-Free', { exact: true }).should('be.visible')
+  
+  })
 
 })
