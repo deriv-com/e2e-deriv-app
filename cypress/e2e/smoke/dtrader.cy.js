8d4a8a3dcad2579fcd892a90b1aec62743726892
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-10-20 17:42:55 +0100

    Main page and multipliers

--- cypress/e2e/smoke/dtrader.cy.js
+++ cypress/e2e/smoke/dtrader.cy.js
@@ -1,30 +1,72 @@
 import '@testing-library/cypress/add-commands'
 
-describe('Deriv Trader', () => {
+describe('QATEST-5014 - Verify Main Page', () => {
   beforeEach(() => {
     cy.c_login()
-    cy.findAllByRole('button', { name: 'Open' }).first().click()
   })
 
-  it('Markets menu', () => {
+  it('Account switcher / Markets menu / Trade Types', () => {
 
-    cy.findByText('Baskets', { exact: true }).click({ force: true })
-    cy.findByText('AUD Basket', { exact: true }).click({ force: true })
+    //Account switcher
+    cy.findByTestId('dt_acc_info').click()
+    cy.findByText('Demo').click()
+    cy.findByTestId('dt_acc_info').click()
+    cy.findByTestId('acc-switcher').findByText('Real').click()
+
+    //Markets
+    cy.get('.cq-symbol-select-btn').click()
+    cy.findByText('Baskets', { exact: true }).click()
+    cy.findByText('Commodities Basket', { exact: true }).should('be.visible')
+    cy.findByText('Forex Basket', { exact: true }).should('be.visible')
+    cy.get('.sc-mcd__category__content--commodities-basket > .subcategory > a > .ic-icon').should('be.visible') //Cypress multiple tabs support issue
+    //TODO
+
+    //Trade Types
+    cy.findByTestId('dt_contract_dropdown').click()
+    cy.findByText('Options').click()
+    cy.findByTestId('dt_contract_wrapper').findByText('Multipliers').click()
+    cy.findByTestId('dt_contract_wrapper').findByPlaceholderText('Search').click({force: true})
+    cy.findByTestId('dt_contract_wrapper').findByPlaceholderText('Search').type('mul')
+    cy.findByTestId('dt_contract_item').findByText('Multipliers').should('be.visible')
+    cy.get('#info-icon').click({force: true})
+    //cy.findByText('Use multipliers to leverage your potential returns. Predict if the asset price w').should('exist')
 
   })
 
-  it('Demo and Real Account Switcher', () => {
+  it('QATEST-5055 - Verify contract for Multipliers', () => {
 
-    cy.c_login()
+    cy.findByTestId('dt_acc_info').click()
+    cy.findByText('Demo').click()
+    cy.get('.acc-switcher__id > :nth-child(2)')
 
-    cy.findByTestId('dt_contract_dropdown').findByText('Rise/Fall').click()
-    cy.findByTestId('dt_contract_wrapper').findByText('Higher/Lower').click()
-    cy.findByText('5 Ticks', { exact: true }).click()
-    cy.findByLabelText('Toggle between advanced and simple duration settings').click()
-    cy.findByRole('button', { name: 'Payout' }).click()
-    cy.findByRole('button', { name: 'End time' }).click()
+    //Stake
+    
+    cy.findByTestId('dt_contract_dropdown').click({force: true})
+    cy.get('#dt_contract_multiplier_item').click({force: true})
+    cy.findByRole('button', { name: 'Up 10.00 USD' }).should('exist')
+    cy.findByRole('button', { name: 'Down 10.00 USD' }).should('exist')
+    cy.findByLabelText('Increment value').click()
+    cy.findByRole('button', { name: 'Up 11.00 USD' }).should('exist')
+    cy.findByRole('button', { name: 'Down 11.00 USD' }).should('exist')
+    cy.findByLabelText('Decrement value').click()
+    cy.findByLabelText('Decrement value').click()
+    cy.findByRole('button', { name: 'Up 9.00 USD' }).should('exist')
+    cy.findByRole('button', { name: 'Down 9.00 USD' }).should('exist')
+
+  });
+
+  // it('Demo and Real Account Switcher', () => {
+
+  //   cy.c_login()
+
+  //   cy.findByTestId('dt_contract_dropdown').findByText('Rise/Fall').click()
+  //   cy.findByTestId('dt_contract_wrapper').findByText('Higher/Lower').click()
+  //   cy.findByText('5 Ticks', { exact: true }).click()
+  //   cy.findByLabelText('Toggle between advanced and simple duration settings').click()
+  //   cy.findByRole('button', { name: 'Payout' }).click()
+  //   cy.findByRole('button', { name: 'End time' }).click()
   
-  })
+  // })
 
   // it('Rise and Fall Options', () => {
 
