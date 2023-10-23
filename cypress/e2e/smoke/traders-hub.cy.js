21fe7cb510f1508847f3b80ce856cc422ba5dd5d
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-10-23 17:57:26 +0100

    Patch-1

--- cypress/e2e/smoke/traders-hub.cy.js
+++ cypress/e2e/smoke/traders-hub.cy.js
@@ -8,13 +8,13 @@ describe('QATEST-5778, QATEST-5781, QATEST-5615', () => {
 
   it('Verify account selector, Verify Real and Demo pages and total assets', () => {
 
-    cy.findByTestId('dti_dropdown_display').findByText('Real').click()
-    cy.get('#demo').click()
-    cy.findByText('Swap-Free Demo').should('be.visible')
-    //TODO Match totals
     cy.findByTestId('dti_dropdown_display').findByText('Demo').click()
     cy.get('#real').click()
     cy.findByText('Swap-Free', { exact: true }).should('be.visible')
+    //TODO Match totals
+    cy.findByTestId('dti_dropdown_display').findByText('Real').click()
+    cy.get('#demo').click()
+    cy.findByText('Swap-Free Demo', { exact: true }).should('be.visible')
   
   })
 
