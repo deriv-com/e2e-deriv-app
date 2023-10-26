0b90468dbb9fa27005c014e54f3569d2814c75a9
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-10-26 21:06:16 +0100

    t1

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -38,6 +38,10 @@ Cypress.Commands.add('c_visitResponsive', (path, size) => {
 
 Cypress.Commands.add('c_login', () => {
 
+    cy.log('serv>' + Cypress.env('configServer') + '<')
+    cy.log('appid>' + Cypress.env('configAppId') + '<')
+    cy.log('oauth>' + Cypress.env('oAuthUrl') + '<')
+    
     cy.c_visitResponsive('/', 'large')
 
     //NB. This is being replaced by 
@@ -47,9 +51,9 @@ Cypress.Commands.add('c_login', () => {
     // cy.findByText('Enable Service Worker registration for this URL').click()
     // cy.findByRole('button', { name: 'Submit' }).click()
 
-    // cy.log('serv>' + Cypress.env('configServer') + '<')
-    // cy.log('appid>' + Cypress.env('configAppId') + '<')
-    // cy.log('oauth>' + Cypress.env('oAuthUrl') + '<')
+    cy.log('serv>' + Cypress.env('configServer') + '<')
+    cy.log('appid>' + Cypress.env('configAppId') + '<')
+    cy.log('oauth>' + Cypress.env('oAuthUrl') + '<')
 
     // localStorage.setItem('config.server_url', Cypress.env('configServer'))
     // localStorage.setItem('config.app_id', Cypress.env('configAppId'))
