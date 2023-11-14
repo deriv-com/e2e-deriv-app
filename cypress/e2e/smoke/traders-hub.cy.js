e19536f0b32eba10c7f5cccaeb7f074290198d9b
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-11-14 11:32:07 +0800

    Added new MT5 tests

--- cypress/e2e/smoke/traders-hub.cy.js
+++ cypress/e2e/smoke/traders-hub.cy.js
@@ -1,12 +1,12 @@
 import '@testing-library/cypress/add-commands'
 
-describe('QATEST-5778, QATEST-5781, QATEST-5615', () => {
+describe('QATEST-5778, QATEST-5781, QATEST-5615, QA-TEST-20318', () => {
   beforeEach(() => {
     cy.c_login()
     cy.c_visitResponsive('/appstore/traders-hub', 'large')
   })
 
-  it('Verify account selector, Verify Real and Demo pages and total assets.', () => {
+  it('Should be able to use account selector and verify Real and Demo pages.', () => {
 
     cy.findByTestId('dti_dropdown_display').findByText('Demo').click()
     cy.get('#real').click()
@@ -17,6 +17,12 @@ describe('QATEST-5778, QATEST-5781, QATEST-5615', () => {
   
   })
 
+  it('Should be able to open MT5 login from Traders Hub.', () => {
+
+    cy.get('button:nth-child(2)').first().click()
+    cy.get('#modal_root').findByRole('link', { name: 'Open' }).should('have.attr', 'href', Cypress.env('mt5BaseUrl') + '/terminal')
+  
+  })
 
 
 
