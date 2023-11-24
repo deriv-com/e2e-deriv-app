1ee277b177fc0afe22b142c8f9a83ca6c4d7697d
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-11-24 17:00:31 +0000

    Wallets update

--- cypress/e2e/wip/wallets.cy.js
+++ cypress/e2e/wip/wallets.cy.js
@@ -2,19 +2,21 @@ import '@testing-library/cypress/add-commands'
 
 describe('QATEST-9999 - <Clickup description here>', () => {
   beforeEach(() => {
-    cy.c_login()
-    cy.c_visitResponsive('/appstore/traders-hub', 'large')
+    cy.c_login('wallets')
+    cy.c_visitResponsive('/wallets', 'large')
   })
 
-  it('should be able to ...', () => {
+  it('should be able to test 1 ...', () => {
 
     cy.log('Tests to go here!')
+    cy.findByRole('heading', { name: 'CFDs' }).should('be.visible')
   
   })
 
-  it('should be able to ...', () => {
+  it('should be able to test 2 ...', () => {
 
     cy.log('Tests to go here!')
+    cy.findByRole('heading', { name: 'CFDs' }).should('be.visible')
   
   })
 
