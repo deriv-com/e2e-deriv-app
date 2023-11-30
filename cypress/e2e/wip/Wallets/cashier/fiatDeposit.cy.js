978b92c37d81a4b43e9a7cf62d9a21f37c2e82f1
lubega-deriv <142860499+lubega-deriv@users.noreply.github.com>
2023-11-30 19:03:06 +0800

    fiat deposit (#15)
    * fiat deposit
    
    * fiat deposit
    
    * fix: applied comments

---
+++ cypress/e2e/wip/Wallets/cashier/fiatDeposit.cy.js
@@ -0,0 +1,34 @@
+import "@testing-library/cypress/add-commands"
+
+describe("WALL-2817 - Fiat deposit iframe access", () => {
+  beforeEach(() => {
+    cy.c_login("doughflow")
+    cy.c_visitResponsive("/wallets", "large")
+  })
+
+  it("should be able to access doughflow iframe", () => {
+    cy.log("Access Fiat Deposit Iframe")
+    cy.contains("Wallet", { timeout: 10000 }).should("exist")
+    cy.get(".wallets-accordion__header").contains("Deposit").first().click()
+    cy.wait(10000) //to account for third party doughflow provider loading time
+    cy.contains("iframe")
+  })
+})
+
+describe("WALL-2817 - Fiat deposit error", () => {
+  beforeEach(() => {
+    cy.c_login("wallets")
+    cy.c_visitResponsive("/wallets", "large")
+  })
+
+  it("should be able to see error message when no access provided", () => {
+    cy.log("Error for Fiat Deposit")
+    cy.contains("Wallet", { timeout: 10000 }).should("exist")
+    cy.get(".wallets-accordion__header").contains("Deposit").first().click()
+    cy.get(".wallets-action-screen")
+      .findByText("Oops, something went wrong!", {
+        exact: true,
+      })
+      .should("be.visible")
+  })
+})
