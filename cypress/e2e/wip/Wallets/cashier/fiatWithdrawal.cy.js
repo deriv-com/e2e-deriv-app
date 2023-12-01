4c191bc57d49f30ed9a51ca9decfd85561ce4bbe
lubega-deriv <lubega@deriv.com>
2023-12-01 17:24:48 +0800

    commented out email test

--- cypress/e2e/wip/Wallets/cashier/fiatWithdrawal.cy.js
+++ cypress/e2e/wip/Wallets/cashier/fiatWithdrawal.cy.js
@@ -1,6 +1,6 @@
 import "@testing-library/cypress/add-commands"
 
-describe("WALL-XXXX - Fiat withdrawal send email", () => {
+describe("WALL-2830 - Fiat withdrawal send email", () => {
     beforeEach(() => {
         cy.c_login("doughflow")
         cy.c_visitResponsive("/wallets", "large")
@@ -17,16 +17,18 @@ describe("WALL-XXXX - Fiat withdrawal send email", () => {
     })
 })
 
-describe("WALL-XXXX - Fiat withdrawal iframe access from email", { baseUrl: null }, () => {
-    beforeEach(() => {
-        cy.c_qaboxlogin()
-    })
+//TODO: Check if qa box email is able to be accessed in cypress to call for verification link
+// describe("WALL-2830 - Fiat withdrawal iframe access from email", () => {
+//     beforeEach(() => {
+//         cy.c_qaboxlogin()
+//     })
   
-    it("should be able to access doughflow iframe", () => {
-      cy.log("Access Fiat Withdrawal Iframe Through Email Link")
-      cy.findByRole('link', { name: /Rudderstack_request_payment_withd/ }).click()
-      cy.findByRole('link', { name: /test-app.deriv.com/ }).click();
-      cy.wait(10000) //to account for third party doughflow provider loading time
-      cy.contains("iframe")
-    })
-})
+//     it("should be able to access doughflow iframe", () => {
+//       cy.log("Access Fiat Withdrawal Iframe Through Email Link")
+//       cy.scrollTo('bottom')
+//       cy.findByRole('link', { name: /Rudderstack_request_payment/ }).last().click()
+//       cy.findByRole('link', { name: /action=payment_withdraw/ }).click();
+//       cy.wait(10000) //to account for third party doughflow provider loading time
+//       cy.contains("iframe")
+//     })
+// })
