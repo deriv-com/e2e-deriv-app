c493e506d1607065621c0bd3b273001c2be01331
anitha-deriv <anitha.rengaraj@regentmarkets.com>
2023-12-15 11:34:09 +0400

    sorting

--- cypress/e2e/wip/Wallets/cashier/addMoreWallet.cy.js
+++ cypress/e2e/wip/Wallets/cashier/addMoreWallet.cy.js
@@ -9,15 +9,31 @@ cy.get('.wallets-add-more__card')
     // Filter wallets that contain only blockchain 
     const walletList = $element.filter((index, element) => {
     const allwalletText = Cypress.$(element).text()
-    return allwalletText.includes('blockchain.')  //Return only the crypto wallets in the wallet carousel
-    }).toArray()
+    const cryptoText  = allwalletText.includes('blockchain')
+    const wallettoadd = allwalletText.includes('Add')
+    if (cryptoText) {
+      if(wallettoadd) {
+        return allwalletText // It will return the text having crypto and Add
+      } else {
+        return allwalletText // It will return the text having crypto and not Add
+      }
+    }
+      else if (wallettoadd) {
+        return allwalletText // It will return the text having Fiat and Add
+      }
+       else {
+        return allwalletText // It will return the text having Fiat and not Add
+       }
+    })
+  }).toArray()
+     //Return only the crypto wallets in the wallet carouse
 
     // Extract text content of filtered wallets in an Array
     const CarouselList = walletList.map(element => Cypress.$(element).text());
     cy.log({CarouselList: CarouselList})
     //Check if the filtered wallets are sorted alphabetically
-    const expectedSortedList = [...CarouselList].sort();
-    expect(CarouselList).to.deep.equal(expectedSortedList);
+    const expectedSortedList = [...CarouselList].sort()
+    expect(CarouselList).to.deep.equal(expectedSortedList)
     cy.log({expectedlist: expectedSortedList})
   })
   
@@ -28,6 +44,12 @@ cy.get('.wallets-add-more__card')
     const fiatwalletList = $element.filter((index, element) => {
     const walletList = Cypress.$(element).text()
     const includesFiat = !walletList.includes('blockchain.');
+    const wallettoadd = allwalletText.includes('Add')
+    if (includesFiat && wallettoadd)
+    {
+      return allwalletText
+    }
+    
     return includesFiat  //Return only the Fiat wallets in the wallet carousel
     })
 
@@ -41,16 +63,24 @@ cy.get('.wallets-add-more__card')
   })
 
 }
-
+function addmorewallets(){
+  cy.get('.wallets-add-more__card')
+  .then(($element) => {
+    cy.get($element).findByRole('button', { name: 'Add' }).eq(0).click()
+})
+}
 describe("WALL-3094 - Add wallets from wallets carousels", () => {
     beforeEach(() => {
       cy.c_login("demoonlywallet")
       cy.c_visitResponsive("/wallets", "large")
+      cy.contains("Add more Wallets")
+      cy.findByTestId('dt-wallets-add-more').scrollIntoView()
     })
 
 it("should be able to see the tour for Demo Only Wallets", () => {
     cy.wait(1000)
-    walletdefaultsorting()
+    // walletdefaultsorting()
+    addmorewallets()
   })
 })
 
