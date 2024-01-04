4413c6ea67d332666b9321f5d64b038998c64873
Jyothi Lakshmi <jyothilakshmi@Jyothi-Lakshmis-MacBook-Pro-C02DXPUSMD6M-Standard.local>
2024-01-04 18:33:11 +0400

    mt5 mf demo change- wip, POM and common methods

---
+++ cypress/e2e/wip/CFD/cfd/cfd.cy.js
@@ -0,0 +1,18 @@
+import "@testing-library/cypress/add-commands"
+
+describe("QATEST-9999 - <Clickup description here>", () => {
+  beforeEach(() => {
+    cy.c_login("wallets")
+    cy.c_visitResponsive("/wallets", "large")
+  })
+
+  it("should be able to test cfd step 1 ...", () => {
+    cy.log("Tests to go here!")
+    cy.findByText("CFDs", { exact: true }).should("be.visible")
+  })
+
+  it("should be able to test cfd step 2 ...", () => {
+    cy.log("Tests to go here!")
+    cy.findByText("CFDs", { exact: true }).should("be.visible")
+  })
+})
---
+++ cypress/e2e/wip/CFD/cfd/pageobjects/mt5_tradershub_page.js
@@ -0,0 +1,83 @@
+const mt5locators=require('../PageElements/mt5Page.json')
+const mt5timeout=10000
+class mt5_tradershub{
+
+    get tradershubDropDown() {
+        return cy.get(mt5locators.mt5Locators.tradershubDropDown);
+      }
+
+    get selectDemo()
+    {
+        return cy.get(mt5locators.mt5Locators.demo);
+    }
+    get cfdstitle()
+    {
+      return cy.xpath(mt5locators.mt5Locators.cfdsaccounttitle)
+    }
+    get mt5tradingdetails()
+    {
+      return cy.get(mt5locators.mt5Locators.mt5tradingdetails)
+    }
+    waitForElementVisible(element)
+    {
+        //cy.get(element, { timeout: mt5timeout}).should('be.visible');
+        //const multiSelector = `${this.timerLocator}, :contains("No Match Found")`;
+        //return cy.get(multiSelector, { timeout: 60000 });
+        return cy.get(element).should('be.visible').then(() => true).catch(() => false);
+    }
+ buttonClick=(el)=>
+    {
+       // cy.wait(10000);
+        el.should('be.visible').click();
+    
+    };
+    dismissNewChartDialouge=()=>
+    {
+        cy.get('#modal_root, .modal-root', { timeout: 10000 })
+        .then(($element) => {
+         if ($element.children().length > 0) {
+        cy.contains("Continue").then(($element) => {
+        if ($element.length) {
+        cy.wrap($element).click()
+        }
+     
+        })
+            } else {
+                    cy.log("fixed.");
+                    }
+            })
+    };
+  
+    verifyText=(webelement,expectedText)=>
+    {
+        cy.get(webelement)
+        .then(($element) => {
+          expect($element.text()).to.be.eq(expectedText)
+          
+        });
+    };
+    // combined functions
+    selectDemoAccount =() =>{
+        
+        //this.tradershubDropDown.click();
+       const tradersHub=this.tradershubDropDown;
+       this.buttonClick(tradersHub);
+       this.buttonClick(this.selectDemo);
+       
+    };
+verifyMFHasCFDsDemoAccount=()=>{
+
+  let demoName
+  cy.get(mt5locators.mt5Locators.mt5tradingdetails)
+  .find('span')
+  .eq(4)
+  .invoke('text')
+  .then((text) => {
+    demoName = text.trim()
+    cy.log("CFD offering for MF :: "+demoName);
+    expect(demoName.trim()).to.equal("CFDs Demo");
+  })
+    };
+
+}
+export default new mt5_tradershub();
