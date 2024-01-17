8aa178713c09ba4d8eabb2ff7bbe01224ffbd262
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2024-01-17 15:09:40 +0000

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
-    projectId: '6xkxq4',
+    projectId: 'qc89n6',
     setupNodeEvents(on, config) {},
     baseUrl: 'https://staging-app.deriv.com',
     defaultCommandTimeout: 15000,
