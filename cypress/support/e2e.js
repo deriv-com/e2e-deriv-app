068f2a0256c15ad681433aadab485df306e0e53a
shariqh-deriv <shariqh@deriv.com>
2024-01-26 10:38:37 +0400

    creating separate file for dtrader common funciton

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,5 @@
 import './commands'
+import './dtrader'
 require('cypress-xpath')
 
 const { getLoginToken } = require("./common")
@@ -247,24 +248,3 @@ Cypress.Commands.add("c_selectDemoAccount", () => {
   cy.get('.dc-content-expander__content').should('be.visible').click()
   cy.findByTestId('dt_acc_info').should('be.visible')
 })
-
-
-Cypress.Commands.add("c_selectSymbol", (symbolName) => {
-  cy.get('.cq-symbol-select-btn', { timeout: 20000 }).should('be.visible')
-  cy.get('.cq-symbol-select-btn').click()
-  cy.get('.ic-icon.sc-mcd__filter__group-icon.sc-mcd__filter__group-icon--open').should('be.visible')
-  cy.findByText('Synthetics').should('be.visible').click()
-  cy.contains('div.sc-mcd__item__name', symbolName).click()
-})
-
-
-Cypress.Commands.add("c_selectTradeType", (category,tradeType) => {
-  cy.findByTestId('dt_contract_dropdown').click()
-  cy.findByText(category).click()
-  cy.findByText(tradeType).should('be.visible').click()
-})
-
-
-
-
-  
