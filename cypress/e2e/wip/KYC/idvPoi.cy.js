86e17b1185cb166137feab153ab5673af48009be
bertrand <bertrand@regentmarkets.com>
2024-02-05 15:45:03 +0200

    Add steps to reach POI page

--- cypress/e2e/wip/KYC/idvPoi.cy.js
+++ cypress/e2e/wip/KYC/idvPoi.cy.js
@@ -15,44 +15,24 @@ describe('IDV POI submission', () => {
         
     // })
 
-    beforeEach(()=> {
-        
-        // cy.viewport(1280, 720);
-        // localStorage.setItem("config.server_url", Cypress.env("configServer"))
-        // localStorage.setItem("config.app_id", Cypress.env("configAppId"))
-        // cy.visit('https://qa10.deriv.dev/oauth2/authorize?app_id=1007&l=EN&signup_device=desktop&date_first_contact=2024-01-23&brand=deriv')
-        // // cy.get('#dt_login_button').click();
-        // cy.get('#txtEmail').type(Cypress.env("loginEmail"));
-        // cy.get('#txtPass').type(Cypress.env("loginPassword"));
-        // cy.contains('button', 'Log in').click();
+    beforeEach(()=> {   
+        cy.viewport(1280, 720);
+        localStorage.setItem("config.server_url", Cypress.env("configServer"))
+        localStorage.setItem("config.app_id", Cypress.env("configAppId"))
+        cy.visit('https://qa10.deriv.dev/oauth2/authorize?app_id=1007&l=EN&signup_device=desktop&date_first_contact=2024-01-23&brand=deriv')
+        cy.get('#txtEmail').type(Cypress.env("loginEmail"));
+        cy.get('#txtPass').type(Cypress.env("loginPassword"));
+        cy.contains('button', 'Log in').click();
+
+        cy.get('a[href="/account/personal-details"]').click()
+        cy.get('a[href="/account/proof-of-identity"]').click()
+        cy.get('input[name="country_input"]').click()
+        cy.get('input[name="country_input"]').type("Gh");
+        cy.contains('Ghana').click()
+        cy.contains('button', 'Next').click()
 
     })
     
     it('should return Name mismatch', () => {
-        cy.get('a[href="/account/personal-details"]').click()
-        cy.get('a[href="/account/proof-of-identity"]').click()
-        cy.get('.proof-of-identity').contains('Proof of identity')
-        // cy.get('input[name="country_input"]').click().type("Ghana")
-        cy.wait(1000)
-        cy.get('input[name="country_input"]').type('Ghana')
-        // cy.get('input[name="country_input"]').invoke('val', 'Ghana').trigger('change');
-        // cy.get('input[name="country_input"]').click()
-        // cy.get('.dc_dropdown-list--enter-done').contains('Ghana').click()
-        // cy.contains('button', 'Next').click()
-        // cy.get('input[name="country_input"]').type("Ghan", { delay: 100 });
-           
-
-        // cy.get('#btnGrant').contains('Authorise this app').click()
-        // botBuilder.changeMarketOnBlocklyWorkspace(1, "Stock Indices");
-        // botBuilder.changeMarketOnBlocklyWorkspace(2, "European indices");
-        // botBuilder.changeMarketOnBlocklyWorkspace(3, "Netherlands 25");
-        // botBuilder.saveStrategyFromToolbar(strategyName);
-        // cy.wait(5000);
-        // botDashboard.goToDashboard();
-        // botDashboard.strategySaveStatus(strategyName).should('have.text', 'Local');
-        // cy.get('.platform-switcher').should('be.visible')
-        // .should('contain', "Deriv Trader")
-        // cy.get('.account-settings-toggle').click()
-
     });
 });
