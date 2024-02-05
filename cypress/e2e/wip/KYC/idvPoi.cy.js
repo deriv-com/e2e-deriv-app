bc761dc5eeff210b94d5b0945c81dd3952066cea
bertrand <bertrand@regentmarkets.com>
2024-02-05 17:24:53 +0200

    Add steps for name mismatch scenario

--- cypress/e2e/wip/KYC/idvPoi.cy.js
+++ cypress/e2e/wip/KYC/idvPoi.cy.js
@@ -8,5 +8,14 @@ describe('IDV POI submission', () => {
     })
     
     it('should return Name mismatch', () => {
+        cy.get('input[name="document_type"]').click()
+        cy.contains("Drivers License").click()
+        cy.get('input[name="document_number"]').type("B0000001");
+        cy.contains('button', 'Verify').should('be.disabled');
+        cy.get('.dc-checkbox__box').click()
+        cy.contains('button', 'Verify').should('be.enabled');
+        // cy.contains('button', 'Verify').click()
+        cy.reload()
+        // making assertions on the expected behavior
     });
 });
