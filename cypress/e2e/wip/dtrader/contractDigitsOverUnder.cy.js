9ff4347e7182d8de23756d4adba37de4e7f8dfbe
shariqh-deriv <shariqh@deriv.com>
2024-02-07 09:10:23 +0400

    Creating digits contract for Over and Under

---
+++ cypress/e2e/wip/dtrader/contractDigitsOverUnder.cy.js
@@ -0,0 +1,47 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-5040 -  Verify contract for Digits', () => {
+    beforeEach(() => {
+      cy.c_login()
+    })
+
+    function createOverUnderContract(tradeType){
+      cy.get('span.number-selector__selection[data-value="5"]').click()
+      cy.findByRole('button', { name: 'Stake' }).click()
+      cy.findByLabelText('Amount').clear().type('10')
+      if(tradeType == 'Over'){
+        cy.get('button.btn-purchase.btn-purchase--1').click()
+      }else if(tradeType == 'Under'){
+        cy.get('button.btn-purchase.btn-purchase--2').click()
+      }else{
+        cy.log("Please check trade type entered and locator")
+      }
+    }
+
+    function checkContractDetailsPage(){
+      cy.get('a.dc-result__caption-wrapper').click()
+      cy.findByText('Contract details').should('be.visible')  
+      cy.contains('span[data-testid="dt_span"]', '10.00').should('be.visible')    //verify stake amount
+    }
+
+    it('Should buy Even contract from Over/Under Trade Type', () => {
+      cy.c_selectDemoAccount()
+      cy.c_selectSymbol('Volatility 100 (1s) Index')
+      cy.c_selectTradeType('Options','Over/Under')
+      createOverUnderContract('Over')
+      cy.get('a.dc-result__caption-wrapper', { timeout: 8000 }).should('be.visible');
+      checkContractDetailsPage()  
+            
+    })
+
+    it('Should buy Odd contract from Over/Under Trade Type', () => {
+      cy.c_selectDemoAccount()
+      cy.c_selectSymbol('Volatility 100 (1s) Index')
+      cy.c_selectTradeType('Options','Over/Under')
+      createOverUnderContract('Under')
+      cy.get('a.dc-result__caption-wrapper', { timeout: 8000 }).should('be.visible');
+      checkContractDetailsPage()
+
+  })
+
+})
