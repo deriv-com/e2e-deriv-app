7a69be6f0623e5e171e53ae8acae9ede6e6dcf52
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-02-16 09:00:23 +0200

    POI verified

--- cypress/e2e/wip/KYC/idvPoiVerified.cy.js
+++ cypress/e2e/wip/KYC/idvPoiVerified.cy.js
@@ -3,16 +3,16 @@ import '@testing-library/cypress/add-commands'
 describe('QATEST-22037 IDV verified by Smile Identity provider', () => {
     beforeEach(()=> {
         cy.c_login();
-        cy.navigate_to_poi('Ghana');
+        cy.navigate_to_poi('Republic of QA');
 
     })
     
     it('Should Return IDV Verified', () => {
         cy.get('input[name="document_type"]').click()
-        cy.contains("Social Security and National Insurance Trust (SSNIT)").click()
-        cy.get('input[name="document_number"]').type("C000000000000")
-        cy.get('input[name="first_name"').clear().type("Doe Joe")
-        cy.get('input[name="last_name"').clear().type("Leo")
+        cy.contains("Passport").click()
+        cy.get('input[name="document_number"]').type("12760978")
+        cy.get('input[name="first_name"').clear().type("Verified")
+        cy.get('input[name="last_name"').clear().type("Namees")
         cy.get('input[name="date_of_birth"').click()
         cy.get('span[data-year="2000"]').click()
         cy.get('span[data-month="8"]').click()
