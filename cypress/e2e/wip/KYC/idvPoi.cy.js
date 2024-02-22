12b9728fbb2eaccaa771f4f3037883f239533666
bertrand <bertrand@regentmarkets.com>
2024-02-22 15:02:08 +0200

    Refactor to use qa provider

--- cypress/e2e/wip/KYC/idvPoi.cy.js
+++ cypress/e2e/wip/KYC/idvPoi.cy.js
@@ -1,21 +1,23 @@
 import '@testing-library/cypress/add-commands'
 
-describe('QATEST-23015 - IDV POI submission', () => {
+describe('QATEST-23015 - IDV POI Name Mismatch', () => {
     beforeEach(()=> {
         cy.c_login();
-        cy.navigate_to_poi('Ghana');
+        cy.navigate_to_poi(/Republic of QA/i);
 
     })
     
     it('should return Name mismatch', () => {
         cy.get('input[name="document_type"]').click()
-        cy.contains("Drivers License").click()
-        cy.get('input[name="document_number"]').type("B0000001");
+        cy.contains("Passport").click()
+        cy.get('input[name="document_number"]').type("1234678");
         cy.contains('button', 'Verify').should('be.disabled');
         cy.get('.dc-checkbox__box').click()
         cy.contains('button', 'Verify').should('be.enabled');
-        // cy.contains('button', 'Verify').click()
+        cy.contains('button', 'Verify').click()
         cy.reload()
+        cy.wait(100)
         // making assertions on the expected behavior
+        cy.contains(/Your ID is verified/i).should('exist');
     });
 });
