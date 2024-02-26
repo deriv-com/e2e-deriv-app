3688374a01ce5185df0acb4c63f28720b417e472
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-02-26 11:26:00 +0800

    wip

--- cypress/e2e/wip/Wallets/cashier/addcryptoWallet.cy.js
+++ cypress/e2e/wip/Wallets/cashier/addcryptoWallet.cy.js
@@ -9,32 +9,36 @@ function addcryptowallet(){
      cy.log ('Button with text "Add" found') 
      for (let i = buttoncount; i>0; i--) {
       cy.findByTestId('dt-wallets-add-more').scrollIntoView()
+      let walletname
+      cy.get('.wallets-add-more__content')
+        .eq(0)
+        .find('span')
+        .eq(0)
+        .invoke('text')
+        .then((text) => {
+          walletname = text.trim()
+        // })
+      cy.log(walletname)
       cy.get('.wallets-add-more__card')
         .eq(0)
         .find('button')
         .click()
-
-        let walletname
+        cy.findByRole('button', { name: 'Maybe later' }).click()
         //To verify newly added wallet widgets are expanded in dashboard
-        cy.get('.wallets-card__details__bottom')
-          .find('span')
-          .eq(1)
-          .invoke('text')
-          .then((text) => {
-            walletname = text.trim()
-            cy.log({wallet: walletname})
-            cy.findByRole('button', { name: 'Maybe later' }).click()
-            cy.wait(3000)
-            cy.findByTestId('dt-wallets-add-more').scrollIntoView()
-            cy.findByTestId('dt_themed_scrollbars').then((element) => {
-              element.scrollIntoView()
-            })
-            cy.get('[class*="wallets-add-more__card"]')
+        cy.findByTestId('dt-wallets-add-more').scrollIntoView()
+        cy.wait(3000)
+        for (let j = buttoncount; i>j; i++){
+          cy.get('[data-testid="dt-wallets-add-more"]').within(()=>{
+            cy.get('div[class="wallets-icon-button__icon"]').eq(1).click({ force: true })
+          })
+        }
+        cy.get('[class*="wallets-add-more__card"]')
           .contains(walletname).find('button')
           .then((button) => {
             expect(button).to.contain('Added')
           })
-            })
+        })
+            // })
           }
     } else {
       cy.log('All wallets are already added')
