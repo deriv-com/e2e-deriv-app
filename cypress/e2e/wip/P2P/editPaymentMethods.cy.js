4301f5d1aa61107107693e280c5c449d16ead118
usman-deriv <usman@deriv.com>
2024-02-27 20:43:17 +0800

    removed semi colons as per coding standard

--- cypress/e2e/wip/P2P/editPaymentMethods.cy.js
+++ cypress/e2e/wip/P2P/editPaymentMethods.cy.js
@@ -48,12 +48,12 @@ function setText(fieldName, fieldText) {
 
 function generateAccountNumberString(length) {
     let result = ''
-    const characters = '0123456789';
-    const charactersLength = characters.length;
+    const characters = '0123456789'
+    const charactersLength = characters.length
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength))
     }
-    return result;
+    return result
 }
 
 function editPaymentMethod(paymentMethod) {
