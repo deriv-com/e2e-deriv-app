7dc4ca571297f63bbb2f0048416b5ef2368a5d20
usman-deriv <usman@deriv.com>
2024-02-27 09:54:11 +0800

    feat: resolved conflicts

--- cypress/e2e/wip/P2P/editPaymentMethods.cy.js
+++ cypress/e2e/wip/P2P/editPaymentMethods.cy.js
@@ -1,6 +1,6 @@
 import '@testing-library/cypress/add-commands'
 
-let paymentMethod = null
+let paymentMethod = null;
 
 function navigateToDerivP2P() {
     cy.get('#dt_mobile_drawer_toggle').should('be.visible').click()
@@ -112,3 +112,67 @@ describe("QATEST-2831 - My Profile page - Edit Payment Method", () => {
         })
     })
 })
+
+function editPaymentMethod(paymentMethod){
+    //Get Payment Method Type:
+    
+    if(paymentMethod == "Bank Transfers"){
+
+    }
+}
+
+function navigateToDerivP2P() {
+    cy.get('#dt_mobile_drawer_toggle')
+        .should('be.visible')
+        .click()
+
+        cy.contains('div.dc-mobile-drawer__submenu-toggle h3', "Cashier")
+        .should('be.visible')
+        .contains('Cashier')
+        .click()
+
+        cy.contains('h3', "Deriv P2P")
+        .should('be.visible')
+        .contains('Deriv P2P')
+        .click()
+
+    cy.get('.dc-modal-header__title').should('be.visible').then(($title) => {
+        if ($title.is(':visible')) {
+            cy.get('.dc-checkbox__box')
+                .should('be.visible')
+                .click()
+        } else {
+            cy.log('Title is not visible, breaking from the else');
+        }
+    })
+    cy.get('[data-testid="dt_modal_footer"] > .dc-btn')
+        .should('be.visible')
+        .click()
+}
+
+function closeNotificationHeader() {
+    cy.document().then(doc => {
+        let notification = doc.querySelector('.notification__header')
+        if (notification && notification != undefined && notification != null) {
+            cy.log('Notification header appeared')
+            cy.get('.notification__text-body').invoke('text').then((text) => {
+                cy.log(text);
+            });
+            cy.get('.notification__close-button')
+                .should('be.visible')
+                .click()
+            cy.get('.notification__header')
+                .should('not.exist')
+            notification = null;
+            cy.wait(2000)
+            closeNotificationHeader()
+        }
+        else {
+            cy.log('Notification header did not appear')
+        }
+    })
+}
+
+function navigateToTab(tabName) {
+    cy.contains('div[data-testid="dt_themed_scrollbars"] li', tabName).click({force:true})
+}
