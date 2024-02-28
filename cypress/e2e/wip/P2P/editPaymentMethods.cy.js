99924e7f907d1d0b804d0380730b25acb17c3319
Usman Zubair <152943428+usman-deriv@users.noreply.github.com>
2024-02-28 11:04:50 +0800

    usman/Edit Payment Methods in P2P - Responsive (#106)
    * created new spec file for P2P Edit Payment Method module
    
    * added functions for navigation, closepopup, navigatedtoeditpaymentmethods
    
    * added functions and test cases for edit payment in P2P
    
    * removed an extra fixture line in beforeEach block
    
    * Updated line and text spacing
    
    * test
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * edit payment methods in P2P responsives
    
    * edit payment methods in P2P responsives
    
    * Master e2e
    
    * feat: resolved conflicts
    
    * feat: resolved conflicts
    
    * Resolved merge conflict in cypress/support/e2e.js
    
    * add_dependencies (#100)
    
    * add_dependencies
    
    * update
    
    ---------
    
    Co-authored-by: Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
    
    * Update email verification command (#101)
    
    * Add run name (#98)
    
    * Update run name
    
    * changed command location
    
    * update command
    
    * Updated command
    
    * run name
    
    * Remove unecessary wait on code (#85)
    
    * Remove unecessary wait on code
    
    * add back wallet test
    
    * Afsaneh/add mt5 account (#92)
    
    * add_mt5_bvi
    
    * temp
    
    * temp
    
    * add_mt5_bvi
    
    * remove_extra_file
    
    * update_url
    
    * launch_modal
    
    * remove_col
    
    * remove_wait
    
    ---------
    
    Co-authored-by: Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
    
    * added functions for navigation, closepopup, navigatedtoeditpaymentmethods
    
    * test
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * editing payment methods for p2p responsives
    
    * edit payment methods in P2P responsives
    
    * edit payment methods in P2P responsives
    
    * Changed to using Testing Library and added setText, savePaymentDetailsAndVerify functions to increase code readability and organization
    
    * new changes
    
    * removed wait, added setText function to increase code readability and organization, integrated the use of testing library
    
    * increased code readability and resolved for abuzars comment
    
    * Coding standards followed for it descriptions
    
    * updating code to use datatest-id in a scenario
    
    * removed semi colons as per coding standards
    
    * removed semi colons as per coding standard
    
    ---------
    
    Co-authored-by: Usman Zubair <usman@Usman-Zubairs-MacBook-Pro-QM6W0HQ00W-Standard.local>
    Co-authored-by: Afsaneh Rezaeinejad <101572229+afsaneh-rezaeinejad-deriv@users.noreply.github.com>
    Co-authored-by: Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
    Co-authored-by: Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
    Co-authored-by: thivya-deriv <82857131+thivya-deriv@users.noreply.github.com>

---
+++ cypress/e2e/wip/P2P/editPaymentMethods.cy.js
@@ -0,0 +1,112 @@
+import '@testing-library/cypress/add-commands'
+
+let paymentMethod = null
+
+function navigateToDerivP2P() {
+    cy.get('#dt_mobile_drawer_toggle').should('be.visible').click()
+    cy.findByRole('heading', { name: 'Cashier' }).should('be.visible').click()
+    cy.findByRole('link', { name: 'Deriv P2P' }).should('be.visible').click()
+    cy.findByRole('heading', { name: 'For your safety:' }).should('be.visible').then(($title) => {
+        if ($title.is(':visible')) {
+            cy.get('.dc-checkbox__box').should('be.visible').click()
+        }
+    })
+    cy.findByRole('button', { name: 'Confirm' }).should('be.visible').click()
+}
+
+function closeNotificationHeader() {
+    cy.document().then(doc => {
+        let notification = doc.querySelector('.notification__header')
+        if (notification) {
+            cy.log('Notification header appeared')
+            cy.get('.notification__text-body').invoke('text').then((text) => {
+                cy.log(text)
+            })
+            cy.findByRole('button', { name: 'Close' }).should('be.visible').click().and('not.exist')
+            notification = null
+            cy.then(() => {closeNotificationHeader()})
+        }
+        else {
+            cy.log('Notification header did not appear')
+        }
+    })
+}
+
+function savePaymentDetailsAndVerify(accountNumberString) {
+    cy.findByRole('button', { name: 'Save changes' }).should('not.be.disabled').click()
+    cy.findByText('Payment methods').should('be.visible')
+    cy.findByText(accountNumberString).should('be.visible')
+}
+
+function navigateToTab(tabName) {
+    cy.findByText(tabName).click()
+}
+
+function setText(fieldName, fieldText) {
+    cy.findByRole('textbox', { name: fieldName }).clear().type(fieldText).should('have.value', fieldText)
+}
+
+function generateAccountNumberString(length) {
+    let result = ''
+    const characters = '0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
+
+function editPaymentMethod(paymentMethod) {
+    if (paymentMethod == "Bank Transfers") {
+        cy.log("Bank Transfer Block")
+        const accountNumberString = generateAccountNumberString(12)
+        setText('Account Number', accountNumberString)
+        setText('SWIFT or IFSC code', '9087')
+        setText('Bank Name', 'Bank Alfalah TG')
+        setText('Branch', 'Branch number 42')
+        savePaymentDetailsAndVerify(accountNumberString)
+
+    } else if (paymentMethod == "E-wallets") {
+        cy.log("Wallets Block")
+        cy.get('input[name="choose_payment_method"]').invoke('val').then((text) => {
+            cy.log('Payment Method: ' + text)
+        })
+        const accountNumberString = generateAccountNumberString(12)
+        cy.get('input[name="account"]').clear().type(accountNumberString).should('have.value', accountNumberString)
+        savePaymentDetailsAndVerify(accountNumberString)
+
+    } else if (paymentMethod == "Others") {
+        cy.log("Others Block")
+        const accountNumberString = generateAccountNumberString(12)
+        setText('Account ID / phone number / email', accountNumberString)
+        setText('Payment method name', 'Xenos Power')
+        savePaymentDetailsAndVerify(accountNumberString)
+    }
+}
+
+describe("QATEST-2831 - My Profile page - Edit Payment Method", () => {
+    beforeEach(() => {
+        cy.c_login()
+        cy.c_visitResponsive('/appstore/traders-hub', 'small')
+    })
+
+    it('Should be able to edit the existing payment method in responsive mode.', () => {
+        navigateToDerivP2P() //Navigation to P2P Handler
+        cy.findByText('Deriv P2P').should('exist')
+        closeNotificationHeader()
+        navigateToTab('My profile')
+        cy.findByText('Available Deriv P2P balance').should('be.visible') //verifes from a page text after navigating to my profile tab
+        cy.findByText('Payment methods').should('be.visible').click()
+        cy.findByText('Payment methods').should('be.visible') //verifies that Payment methods heading page is visible after clicking on Payment Methods
+        // Get first payment type available on the screen
+        cy.get('span.payment-methods-list__list-header').first().invoke('text').then((value) => {
+            paymentMethod = value.trim() //Will only get either of the three: Bank Transfers, E-wallets, Others
+            cy.log("Payment type available: " + paymentMethod)
+            cy.contains('div.payment-methods-list__list-container span', paymentMethod).next().within(() => {
+                cy.findByTestId('dt_dropdown_display').first().click()
+            })
+            cy.get('#edit').should('be.visible').click()
+            editPaymentMethod(paymentMethod)
+        })
+    })
+})
