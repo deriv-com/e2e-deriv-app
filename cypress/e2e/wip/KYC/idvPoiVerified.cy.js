1a24d3b7ab516ef8b20be8263d310aa4b48fe6b7
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-02-29 13:37:30 +0200

    refactored the tests

--- cypress/e2e/wip/KYC/idvPoiVerified.cy.js
+++ cypress/e2e/wip/KYC/idvPoiVerified.cy.js
@@ -3,31 +3,20 @@ import '@testing-library/cypress/add-commands'
 describe('QATEST-22037 IDV verified by Smile Identity provider', () => {
     beforeEach(()=> {
         cy.c_login();
-        cy.c_visitResponsive('/account/proof-of-identity','small')
-        // cy.navigate_to_poi('Republic of QA');
+        cy.navigate_to_poi_responsive('Republic of QA');
 
     })
     
     it('Should Return IDV Verified', () => {
-        cy.get('input[name="document_type"]').click()
-        cy.contains("Passport").click()
-        cy.get('input[name="document_number"]').type("12760978")
-        cy.get('input[name="first_name"').clear().type("Verified")
-        cy.get('input[name="last_name"').clear().type("Namees")
-        cy.get('input[name="date_of_birth"').click()
-        cy.get('span[data-year="2000"]').click()
-        cy.get('span[data-month="8"]').click()
-       
-        cy.get('span[data-date="2000-09-20"]').click()
-        cy.contains('button', 'Verify').should('be.disabled')
-
-        cy.get('.dc-checkbox__box').click()
-        cy.contains('button', 'Verify').should('be.enabled')
-        cy.contains('button', 'Verify').click()
-        cy.wait(5000)
-        cy.reload()
-
+        cy.get('select[name="document_type"]').select('Passport');
+        cy.findByLabelText('Enter your document number').type('12376678');
+        cy.findByTestId('first_name').clear().type('Verifiedd');
+        cy.findByTestId('last_name').clear().type('Name');
+        cy.findByTestId('date_of_birth').type('2000-01-01');
+        cy.get('.dc-checkbox__box').click();
+        cy.findByRole('button', { name: 'Verify' }).click();
         cy.contains('ID verification passed').should('be.visible')
         cy.contains('a','Continue trading').should('be.visible')
+        
     }); 
 });
