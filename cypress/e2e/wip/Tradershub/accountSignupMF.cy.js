f3581dbc2bab6dd6159664aeaa23b01cd9ad1044
Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
2024-02-29 16:22:24 +0800

    Refactor "c_personalDetails"

--- cypress/e2e/wip/Tradershub/accountSignupMF.cy.js
+++ cypress/e2e/wip/Tradershub/accountSignupMF.cy.js
@@ -7,6 +7,10 @@ function generate_epoch() {
 describe("QATEST-5569: Verify MF Signup flow", () => {
   const epoch = generate_epoch()
   const sign_up_mail = `sanity${epoch}MF@deriv.com`
+  let country = Cypress.env("countries").ES
+  let nationalIDNum = Cypress.env("nationalIDNum").ES
+  let taxIDNum = Cypress.env("taxIDNum").ES
+  let currency = Cypress.env("accountCurrency").GBP
 
   beforeEach(() => {
     localStorage.setItem("config.server_url", Cypress.env("stdConfigServer"))
@@ -18,7 +22,7 @@ describe("QATEST-5569: Verify MF Signup flow", () => {
   it("Verify I can signup for an MF demo and real account", () => {
     cy.c_emailVerificationSignUp(epoch)
     cy.then(() => {
-      cy.c_visitResponsive("/endpoint", "desktop").then(() => {
+      cy.c_visitResponsive(Cypress.env("signUpUrl"), "desktop").then(() => {
         cy.window().then((win) => {
           win.localStorage.setItem(
             "config.server_url",
@@ -31,14 +35,21 @@ describe("QATEST-5569: Verify MF Signup flow", () => {
         })
       })
 
-      cy.c_visitResponsive(Cypress.env("signUpUrl"), "desktop")
+      // cy.c_visitResponsive(Cypress.env("signUpUrl"), "desktop")
       cy.get("h1").contains("Select your country and").should("be.visible")
-      cy.c_selectCountryOfResidence(Cypress.env("CoRMF"))
-      cy.c_selectCitizenship(Cypress.env("citizenshipMF"))
+      cy.c_selectCountryOfResidence(country)
+      cy.c_selectCitizenship(country)
       cy.c_enterPassword()
     })
     cy.c_generateRandomName().then((firstName) => {
-      cy.c_personalDetails(firstName, "MF", Cypress.env("CoRMF"))
+      cy.c_personalDetails(
+        firstName,
+        "MF",
+        country,
+        nationalIDNum,
+        taxIDNum,
+        currency
+      )
     })
     cy.c_addressDetails()
     cy.c_completeTradingAssessment()
@@ -47,6 +58,6 @@ describe("QATEST-5569: Verify MF Signup flow", () => {
     cy.get("#traders-hub").scrollIntoView({ position: "top" })
     cy.findByText("Total assets").should("be.visible")
     cy.findByText("0.00").should("be.visible")
-    cy.c_manageAccountsetting(Cypress.env("CoRMF"))
+    cy.c_manageAccountsetting(country)
   })
 })
