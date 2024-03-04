a92efcaeb95d3357c17acf460da5de66d0b3ebeb
usman-deriv <usman@deriv.com>
2024-03-04 18:03:19 +0800

    added functionality for adding a payment method before deleting

--- cypress/e2e/wip/P2P/deletePaymentMethods.cy.js
+++ cypress/e2e/wip/P2P/deletePaymentMethods.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 import '@testing-library/cypress/add-commands'
 
 let paymentMethod = null
@@ -29,9 +30,33 @@ function deletePaymentMethod() {
     })
 }
 
-function addNewPaymentMethod() {
-    
+function generateAccountNumberString(length) {
+    let result = ''
+    const characters = '0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
 
+function setText(fieldName, fieldText) {
+    cy.findByRole('textbox', { name: fieldName }).clear().type(fieldText).should('have.value', fieldText)
+}
+
+function addPaymentMethod() {
+    const accountNumberString = generateAccountNumberString(12)
+    cy.findByRole('button').should('be.visible').and('contain.text', 'Add').click()
+    setText('Payment method', 'Bank Transfer')
+    cy.findByText('Bank Transfer').click()
+    setText('Account Number', accountNumberString)
+    setText('SWIFT or IFSC code', '9087')
+    setText('Bank Name', 'Bank Alfalah TG')
+    setText('Branch', 'Branch number 42')
+    cy.get('textarea[name="instructions"]').type('Follow instructions.').should('have.value', 'Follow instructions.')
+    cy.findByRole('button', { name: 'Add' }).should('not.be.disabled').click()
+    cy.findByText('Payment methods').should('be.visible')
+    cy.findByText(accountNumberString).should('be.visible')
 }
 
 describe("QATEST-2839 - My Profile page - Delete Payment Method", () => {
@@ -48,9 +73,9 @@ describe("QATEST-2839 - My Profile page - Delete Payment Method", () => {
         cy.findByText('Available Deriv P2P balance').should('be.visible')
         cy.findByText('Payment methods').should('be.visible').click()
         cy.findByText('Payment methods').should('be.visible')
-        addNewPaymentMethod()
+        addPaymentMethod()
         cy.get('span.payment-methods-list__list-header').first().invoke('text').then((value) => {
-            paymentMethod = value.trim() //Will only get either of the three: Bank Transfers, E-wallets, Others
+            paymentMethod = value.trim()
             cy.log("Payment type available: " + paymentMethod)
             cy.contains('div.payment-methods-list__list-container span', paymentMethod).next().within(() => {
                 deletePaymentMethod()
