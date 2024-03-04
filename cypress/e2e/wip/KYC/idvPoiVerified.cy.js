13d6316124353a6940ac798a25826d91f37552ce
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-03-04 12:36:34 +0200

    addressed review comment

--- cypress/e2e/wip/KYC/idvPoiVerified.cy.js
+++ cypress/e2e/wip/KYC/idvPoiVerified.cy.js
@@ -10,8 +10,8 @@ describe('QATEST-22037 IDV verified by Smile Identity provider', () => {
     it('Should Return IDV Verified', () => {
         cy.get('select[name="document_type"]').select('Passport');
         cy.findByLabelText('Enter your document number').type('12376678');
-        cy.findByTestId('first_name').clear().type('Verifiedd');
-        cy.findByTestId('last_name').clear().type('Name');
+        cy.findByTestId('first_name').clear().type('Verifiede');
+        cy.findByTestId('last_name').clear().type('Namee');
         cy.findByTestId('date_of_birth').type('2000-01-01');
         cy.get('.dc-checkbox__box').click();
         cy.findByRole('button', { name: 'Verify' }).click();
