72953ac2fa1b1f3654cd850302dc979288cb40a9
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-03-04 15:54:53 +0200

    Removed semicolon

--- cypress/e2e/wip/KYC/idvPoiVerified.cy.js
+++ cypress/e2e/wip/KYC/idvPoiVerified.cy.js
@@ -8,13 +8,13 @@ describe('QATEST-22037 IDV verified by Smile Identity provider', () => {
     })
     
     it('Should Return IDV Verified', () => {
-        cy.get('select[name="document_type"]').select('Passport');
-        cy.findByLabelText('Enter your document number').type('12376678');
-        cy.findByTestId('first_name').clear().type('Verifiede');
-        cy.findByTestId('last_name').clear().type('Namee');
-        cy.findByTestId('date_of_birth').type('2000-01-01');
-        cy.get('.dc-checkbox__box').click();
-        cy.findByRole('button', { name: 'Verify' }).click();
+        cy.get('select[name="document_type"]').select('Passport')
+        cy.findByLabelText('Enter your document number').type('12376678')
+        cy.findByTestId('first_name').clear().type('Verifiede')
+        cy.findByTestId('last_name').clear().type('Namee')
+        cy.findByTestId('date_of_birth').type('2000-01-01')
+        cy.get('.dc-checkbox__box').click()
+        cy.findByRole('button', { name: 'Verify' }).click()
         cy.contains('ID verification passed').should('be.visible')
         cy.contains('a','Continue trading').should('be.visible')
         
