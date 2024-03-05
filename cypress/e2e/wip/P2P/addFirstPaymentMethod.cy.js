f7bc42bf5aba2808ee19a63bd6e9111e9428145f
usman-deriv <usman@deriv.com>
2024-03-06 01:26:19 +0800

    cypress test for adding first payment method in p2p

--- cypress/e2e/wip/P2P/addFirstPaymentMethod.cy.js
+++ cypress/e2e/wip/P2P/addFirstPaymentMethod.cy.js
@@ -1,45 +1,8 @@
 import '@testing-library/cypress/add-commands'
 
 let paymentMethod = 'Bank Transfer'
-let paymentName = null
-let paymentID = null
-
-function navigateToDerivP2P() {
-    cy.get('#dt_mobile_drawer_toggle').should('be.visible').click()
-    cy.findByRole('heading', { name: 'Cashier' }).should('be.visible').click()
-    cy.findByRole('link', { name: 'Deriv P2P' }).should('be.visible').click()
-    cy.findByRole('heading', { name: 'For your safety:' }).should('be.visible').then(($title) => {
-        if ($title.is(':visible')) {
-            cy.get('.dc-checkbox__box').should('be.visible').click()
-        }
-    })
-    cy.findByRole('button', { name: 'Confirm' }).should('be.visible').click()
-}
-
-function closeNotificationHeader() {
-    cy.document().then(doc => {
-        let notification = doc.querySelector('.notification__header')
-        if (notification) {
-            cy.log('Notification header appeared')
-            cy.get('.notification__text-body').invoke('text').then((text) => {
-                cy.log(text)
-            })
-            cy.findByRole('button', { name: 'Close' }).should('be.visible').click().and('not.exist')
-            notification = null
-            cy.then(() => { closeNotificationHeader() })
-        }
-        else {
-            cy.log('Notification header did not appear')
-        }
-    })
-}
-
-function savePaymentDetailsAndVerify(accountNumberString, accountBankName) {
-    cy.findByRole('button', { name: 'Add' }).should('not.be.disabled').click()
-    cy.findByText('Payment methods').should('be.visible')
-    cy.findByText(accountNumberString).should('be.visible')
-    cy.findByText(accountBankName).should('be.visible')
-}
+let paymentName = 'Bank Alfalah TG'
+let paymentID = generateAccountNumberString(12)
 
 function navigateToTab(tabName) {
     cy.findByText(tabName).click()
@@ -49,34 +12,35 @@ function setText(fieldName, fieldText) {
     cy.findByRole('textbox', { name: fieldName }).clear().type(fieldText).should('have.value', fieldText)
 }
 
-function addFirstPaymentMethod() {
-    setText('Account Number', '627438573856395')
+function generateAccountNumberString(length) {
+    let result = ''
+    const characters = '0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
+
+function deletePaymentMethod() {
+    cy.findByText(paymentID).should('exist').parent().prev().find('.dc-dropdown-container').and('exist').click()
+    cy.get('#delete').should('be.visible').click()
+    cy.findByText(`Delete ${paymentName}?`).should('be.visible')
+    cy.findByRole('button', { name: 'Yes, remove' }).should('be.visible').click()
+    cy.findByText(paymentID).should('not.exist')
+}
+
+function addPaymentMethod() {
+    setText('Payment method', 'Bank')
+    cy.findByText('Bank Transfer').click()
+    setText('Account Number', paymentID)
     setText('SWIFT or IFSC code', '9087')
     setText('Bank Name', 'Bank Alfalah TG')
     setText('Branch', 'Branch number 42')
     cy.get('textarea[name="instructions"]').type('Follow instructions.').should('have.value', 'Follow instructions.')
-    savePaymentDetailsAndVerify('627438573856395', 'Bank Alfalah TG')
-}
-
-function deletePaymentMethod() {
-    cy.get('div.payment-method-card__body span').each(($span, index) => {
-        const value = $span.text().trim()
-        if ((index === 0 || index === 1) && value !== '' && !paymentName) {
-            paymentName = value
-            return
-        }
-        if (value !== '') {
-            paymentID = $span.text().trim()
-            if (paymentName && paymentID) {
-                cy.findAllByTestId('dt_dropdown_display').first().click()
-                cy.get('#delete').should('be.visible').click()
-                cy.get('div[class="dc-modal-header"]', { withinSubject: null }).should('be.visible').and('exist').contains(`Delete ${paymentName}?`)
-                cy.get('div.dc-modal-footer button', { withinSubject: null }).first().should('be.visible').and('have.text', 'Yes, remove').click()
-                cy.get(`span:contains(${paymentID})`, { withinSubject: null }).should('not.exist')
-            }
-            return false
-        }
-    })
+    cy.findByRole('button', { name: 'Add' }).should('not.be.disabled').click()
+    cy.findByText('Payment methods').should('be.visible')
+    cy.findByText(paymentID).should('be.visible')
 }
 
 describe("QATEST-2821 My Profile page : User add their first payment method", () => {
@@ -85,24 +49,17 @@ describe("QATEST-2821 My Profile page : User add their first payment method", ()
         cy.c_visitResponsive('/appstore/traders-hub', 'small')
     })
     it('Should be able to add first payment method in responsive mode.', () => {
-        navigateToDerivP2P()
+        cy.c_navigateToDerivP2P()
         cy.findByText('Deriv P2P').should('exist')
-        closeNotificationHeader()
+        cy.c_closeNotificationHeader()
         navigateToTab('My profile')
         cy.findByText('Available Deriv P2P balance').should('be.visible')
         cy.findByText('Payment methods').should('be.visible').click()
         cy.findByText('Payment methods').should('be.visible')
-        cy.findByText('You haven’t added any payment methods yet').should('be.visible').and('exist')
+        cy.findByText('You haven’t added any payment methods yet').should('be.visible')
         cy.findByRole('button', { name: 'Add payment methods' }).should('be.visible').click()
-        cy.findByText('Add payment method').should('be.visible')
-        setText('Payment method', paymentMethod)
-        cy.findByText(paymentMethod).click()
-        addFirstPaymentMethod()
-        cy.get('span.payment-methods-list__list-header').first().invoke('text').then((paymentMethod) => {
-            cy.contains('div.payment-methods-list__list-container span', paymentMethod).next().within(() => {
-                deletePaymentMethod()
-            })
-        })
-        cy.findByText('You haven’t added any payment methods yet').should('be.visible').and('exist')
+        addPaymentMethod()
+        deletePaymentMethod()
+        cy.findByText('You haven’t added any payment methods yet').should('be.visible')
     })
 })
