48a8e09c9139751dcc8aa0472550248ec0c6cf02
usman-deriv <usman@deriv.com>
2024-03-06 15:15:04 +0800

    blocking and unblocking user

--- cypress/e2e/wip/P2P/blockAdvertFromProfilePage.cy.js
+++ cypress/e2e/wip/P2P/blockAdvertFromProfilePage.cy.js
@@ -1,5 +1,28 @@
 import '@testing-library/cypress/add-commands'
 
+function getNickname() {
+    return cy.get('.buy-sell-row__advertiser-name--text').first().invoke('text')
+}
+
+function blockProfile(userNickname) {
+    cy.findAllByText(userNickname).first().should('be.visible').click()
+    cy.findByText("Advertiser's page").should('exist')
+    cy.findByText(userNickname).should('be.visible')
+    cy.findByTestId('dt_page_return').next().click()
+    cy.get('.advertiser-page-dropdown-menu').should('be.visible').click()
+    cy.findByText(`Block ${userNickname}?`).should('be.visible')
+    cy.findByRole('button', { name: 'Block' }).should('be.visible').click()
+    cy.findByText(`You have blocked ${userNickname}.`).should('be.visible')
+}
+
+function unblockProfile(userNickname) {
+    cy.findByRole('button', { name: 'Unblock' }).should('be.visible').click()
+    cy.findByText(`Unblock ${userNickname}?`).should('be.visible')
+    cy.findByRole('button', { name: 'Unblock' }).should('be.visible').and('exist').click()
+    cy.findByText("Advertiser's page").should('exist')
+    cy.findByText(userNickname).should('be.visible')
+}
+
 describe("QATEST-2871 - Block and unblock user from advertisers Profile pag", () => {
     beforeEach(() => {
         cy.c_login()
@@ -11,8 +34,9 @@ describe("QATEST-2871 - Block and unblock user from advertisers Profile pag", ()
         cy.findByText('Deriv P2P').should('exist')
         cy.c_closeNotificationHeader()
         cy.findByText('No ads for this currency at the moment 😞').should('not.exist')
-        cy.get('div[class="buy-sell__cell--container__row"]').first().click()
-        cy.findByText("Advertiser's page").should('exist')
-        cy.findByTestId('dt_page_return').next().click()
+        getNickname().then((userNickname) => {
+            blockProfile(userNickname)
+            unblockProfile(userNickname)
+        })
     })
 })
