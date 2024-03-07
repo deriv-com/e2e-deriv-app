7564626b8a0fe200564f65db6c6bcb03d4c71446
usman-deriv <usman@deriv.com>
2024-03-07 15:45:05 +0800

    function to close safety notification when navigated to p2p

--- cypress/e2e/wip/P2P/deletePaymentMethods.cy.js
+++ cypress/e2e/wip/P2P/deletePaymentMethods.cy.js
@@ -12,6 +12,7 @@ describe("QATEST-2839 - My Profile page - Delete Payment Method", () => {
 
     it('Should be able to delete the existing payment method in responsive mode.', () => {
         cy.c_navigateToDerivP2P()
+        cy.c_closeSafetyInstructions()
         cy.findByText('Deriv P2P').should('exist')
         cy.c_closeNotificationHeader()
         cy.findByText('My profile').click()
