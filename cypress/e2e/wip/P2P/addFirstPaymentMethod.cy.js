0c725adf864cef17f7f0b3f11825093e906487b9
usman-deriv <usman@deriv.com>
2024-03-07 14:47:19 +0800

    resolved for Meeyuns comments

--- cypress/e2e/wip/P2P/addFirstPaymentMethod.cy.js
+++ cypress/e2e/wip/P2P/addFirstPaymentMethod.cy.js
@@ -12,16 +12,16 @@ describe("QATEST-2821 - My Profile page : User add their first payment method", 
 
     it('Should be able to add first payment method in responsive mode.', () => {
         cy.c_navigateToDerivP2P()
+        cy.c_closeSafetyInstructions()
         cy.findByText('Deriv P2P').should('exist')
         cy.c_closeNotificationHeader()
         cy.findByText('My profile').click()
         cy.findByText('Available Deriv P2P balance').should('be.visible')
         cy.findByText('Payment methods').should('be.visible').click()
         cy.findByText('Payment methods').should('be.visible')
+        cy.c_deleteAllPM()
         cy.findByText('You haven’t added any payment methods yet').should('be.visible')
         cy.findByRole('button', { name: 'Add payment methods' }).should('be.visible').click()
         cy.c_addPaymentMethod(paymentID, paymentName)
-        cy.c_deletePaymentMethod(paymentID, paymentName)
-        cy.findByText('You haven’t added any payment methods yet').should('be.visible')
     })
 })
--- cypress/support/p2p.js
+++ cypress/support/p2p.js
@@ -2,6 +2,25 @@ Cypress.Commands.add('c_navigateToDerivP2P', () => {
     cy.get('#dt_mobile_drawer_toggle').should('be.visible').click()
     cy.findByRole('heading', { name: 'Cashier' }).should('be.visible').click()
     cy.findByRole('link', { name: 'Deriv P2P' }).should('be.visible').click()
+})
+
+Cypress.Commands.add('c_deleteAllPM', () => {
+    cy.document().then(doc => {
+        let notification = doc.querySelector('.dc-dropdown__container')
+        if (notification) {
+            cy.get('.dc-dropdown__container').first().click()
+            cy.get('#delete').should('be.visible').click()
+            cy.findByRole('button', { name: 'Yes, remove' }).should('be.visible').click().and('not.exist')
+            notification = null
+            cy.then(() => { cy.c_deleteAllPM() })
+        }
+        else {
+            cy.log('No PMs available')
+        }
+    })
+})
+
+Cypress.Commands.add('c_closeSafetyInstructions', () => {
     cy.findByRole('heading', { name: 'For your safety:' }).should('be.visible').then(($title) => {
         if ($title.is(':visible')) {
             cy.get('.dc-checkbox__box').should('be.visible').click()
