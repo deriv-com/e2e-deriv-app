b32bb6b748d4d584e94f7bfd8f2b429c3389a0df
usman-deriv <usman@deriv.com>
2024-03-11 14:16:33 +0800

    file name changed

--- cypress/e2e/wip/P2P/blockUnblockAdvertiser.cy.js
+++ cypress/e2e/wip/P2P/blockUnblockAdvertiser.cy.js
@@ -1,40 +1,36 @@
 import '@testing-library/cypress/add-commands'
 
-function getNickname() {
-    return cy.get('.buy-sell-row__advertiser-name--text').first().invoke('text')
-}
-
 function blockProfile(userNickname) {
     cy.findAllByText(userNickname).first().should('be.visible').click()
-    cy.findByText("Advertiser's page").should('exist')
+    cy.findByText('Advertiser\'s page').should('exist')
     cy.findByText(userNickname).should('be.visible')
     cy.findByTestId('dt_page_return').next().click()
     cy.get('.advertiser-page-dropdown-menu').should('be.visible').click()
     cy.findByText(`Block ${userNickname}?`).should('be.visible')
-    cy.findByRole('button', { name: 'Block' }).should('be.visible').click()
+    cy.findByRole('button', { name: 'Block' }).should('exist').click()
     cy.findByText(`You have blocked ${userNickname}.`).should('be.visible')
 }
 
 function unblockProfile(userNickname) {
-    cy.findByRole('button', { name: 'Unblock' }).should('be.visible').click()
+    cy.findByRole('button', { name: 'Unblock' }).should('exist').click()
     cy.findByText(`Unblock ${userNickname}?`).should('be.visible')
-    cy.findByRole('button', { name: 'Unblock' }).should('be.visible').and('exist').click()
-    cy.findByText("Advertiser's page").should('exist')
+    cy.findByTestId('dt_modal_footer').findByRole('button', { name: 'Unblock' }).should('exist').click()
+    cy.findByText('Advertiser\'s page').should('exist')
     cy.findByText(userNickname).should('be.visible')
 }
 
 describe("QATEST-2871 - Block and unblock user from advertisers Profile pag", () => {
     beforeEach(() => {
         cy.c_login()
-        cy.c_visitResponsive('/appstore/traders-hub', 'small')
+        cy.c_visitResponsive('/cashier/p2p', 'small')
     })
 
     it('Should be able to block the advertiser from profile page.', () => {
-        cy.c_navigateToDerivP2P()
+        cy.c_closeSafetyInstructions()
         cy.findByText('Deriv P2P').should('exist')
         cy.c_closeNotificationHeader()
         cy.findByText('No ads for this currency at the moment 😞').should('not.exist')
-        getNickname().then((userNickname) => {
+        cy.get('.buy-sell-row__advertiser-name--text').first().invoke('text').then((userNickname) => {
             blockProfile(userNickname)
             unblockProfile(userNickname)
         })
