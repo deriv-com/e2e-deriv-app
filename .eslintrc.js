79fbeb78276f843214fc5303da2d1c7f773ec07e
Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
2024-03-11 12:41:45 +0800

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "extends": [
+        // "eslint:recommended",
+         
+        // "prettier",
+        "plugin:prettier/recommended"
+    ]
+    
+  }
