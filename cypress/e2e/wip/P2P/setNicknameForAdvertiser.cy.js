d143f4ccc5d3331022da0ff0e26bc5df0915e51a
usman-deriv <usman@deriv.com>
2024-03-12 12:53:57 +0800

    nickname script name update

---
+++ cypress/e2e/wip/P2P/setNicknameForAdvertiser.cy.js
@@ -0,0 +1,24 @@
+import '@testing-library/cypress/add-commands'
+import { generateAccountNumberString } from '../../../support/p2p'
+
+let nickname = `nickWithAcc${generateAccountNumberString(5)}`
+
+describe("QATEST-2292 - Register a new client as an Advertiser in Deriv P2P - Nickname checks", () => {
+    beforeEach(() => {
+        cy.c_login()
+        cy.c_visitResponsive('/cashier/p2p', 'small')
+    })
+
+    it('Should be able to create a nickname for P2P and redirect to my ads screen in responsive mode.', () => {
+        cy.c_closeSafetyInstructions()
+        cy.findByText('Deriv P2P').should('exist')
+        cy.c_closeNotificationHeader()
+        cy.findByText('My profile').click()
+        cy.findByRole('heading', { name: 'What’s your nickname?' }).should('be.visible')
+        cy.findByText('Others will see this on your profile, ads, and chats.').should('be.visible')
+        cy.findByText('Your nickname cannot be changed later.').should('be.visible')
+        cy.findByRole('textbox', { name: 'Your nickname' }).should('exist').type(nickname)
+        cy.findByRole('button', { name: 'Confirm' }).should('be.enabled').click()
+        cy.findByText('Nickname added successfully!').should('be.visible').click().should('not.be.visible')
+    })
+})
