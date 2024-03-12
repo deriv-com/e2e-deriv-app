75ee59a6de3468a26d12bc3894ff3464b8f90151
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-03-12 09:08:43 +0800

    check for all currency create account

--- cypress/e2e/wip/Crypto/createcryptoaccount.cy.js
+++ cypress/e2e/wip/Crypto/createcryptoaccount.cy.js
@@ -18,27 +18,23 @@ describe('QATEST-707 - Create crypto account', () => {
     cy.c_login()
     cy.c_visitResponsive('/appstore/traders-hub', 'large')
   })
-  const validatecyrpto = () => {
-    cy.close_notification_banner()
-    cyrpto.elements.currency_switcher().should('be.visible').click()
-    cyrpto.elements.manage_account().should('be.visible').click()
-  }
-  const validatecyrpto1 = () => {
-    cy.close_notification_banner()
-    cyrpto.elements.crypto_add_account().should('be.visible').click()
-    cyrpto.elements.maybe_later().should('be.visible').click()
-    cyrpto.elements.currency_switcher().should('be.visible').click()
-  }
-
-
-
-  it('should be able to create crypto account from Traders Hub.', () => {
-  const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"];
-  cryptocurrencies.forEach(crypto => {
-    validatecyrpto()
+  const validatecrypto = (crypto) => {
+    cy.close_notification_banner();
+    cyrpto.elements.currency_switcher().should('be.visible').click();
+    cyrpto.elements.manage_account().should('be.visible').click();
     cy.findByText(crypto).click();
-    validatecyrpto1()
-    cy.findByText("0.00000000 BTC").should("be.visible")
+    cyrpto.elements.crypto_add_account().should('be.visible').click();
+    cyrpto.elements.maybe_later().should('be.visible').click();
+    return crypto;
+  };
+  it('should be able to create crypto account from Traders Hub.', () => {
+    const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"];
+    cryptocurrencies.forEach(crypto => {
+      validatecrypto(crypto);
+      // Now you can use `selectedCrypto` in further assertions or operations
+      // For example:
+      // cy.findByText(selectedCrypto).should('be.visible');
+      // cy.get('#modal_root').click();
+    });
   });
   })
-})
