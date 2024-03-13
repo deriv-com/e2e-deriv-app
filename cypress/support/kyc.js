15cbf1dd8d57b919f73f07ddfd716304e853b81d
bertrand <bertrand@regentmarkets.com>
2024-03-13 10:12:23 +0200

    Improve custom commands

--- cypress/support/kyc.js
+++ cypress/support/kyc.js
@@ -1,16 +1,16 @@
-Cypress.Commands.add('navigateToPoi', (country) => {
-    cy.get('a[href="/account/personal-details"]').click()
-    cy.get('a[href="/account/proof-of-identity"]').click()
-    cy.get('input[name="country_input"]').click()
-    cy.get('input[name="country_input"]').type(country)
-    cy.contains(country).click()
-    cy.contains('button', 'Next').click()
-  })
-  
-  Cypress.Commands.add('c_navigateToPoiResponsive', (country) => {
-    cy.c_visitResponsive("/account/proof-of-identity", "small")
-    cy.findByText("Pending action required").should('exist')
-    cy.c_closeNotificationHeader()
-    cy.get('select[name="country_input"]').select(country)
-    cy.contains("button", "Next").click()
-  })
+Cypress.Commands.add('c_navigateToPoi', (country) => {
+  cy.get('a[href="/account/personal-details"]').click()
+  cy.findByRole('link', { name: 'Proof of identity' }).click()
+  cy.findByLabel('Country').click()
+  cy.findByText(country).click()
+  cy.contains(country).click()
+  cy.contains('button', 'Next').click()
+})
+
+Cypress.Commands.add('c_navigateToPoiResponsive', (country) => {
+  cy.c_visitResponsive('/account/proof-of-identity', 'small')
+  cy.findByText('Pending action required').should('exist')
+  cy.c_closeNotificationHeader()
+  cy.get('select[name="country_input"]').select(country)
+  cy.contains('button', 'Next').click()
+})
