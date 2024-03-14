ed727ae81fd1be651376e3210b78020b924415b4
maliheh <maliheh@regentmarkets.com>
2024-03-14 16:57:49 +0800

    verify production bug

--- cypress/e2e/full/redirection.cy.js
+++ cypress/e2e/full/redirection.cy.js
@@ -1,19 +1,23 @@
 import '@testing-library/cypress/add-commands'
-
 describe('WALL-3591 -production bug WALL-3588', () => {
   beforeEach(() => {
     cy.c_login()
   })
 
   it('should remain logged in after redirection to another platform', () => {
-    cy.c_visitResponsive("/appstore/traders-hub", "large")
-    cy.findAllByRole("button", { name: "Open" }).eq(1).click({ force: true })
-    cy.findByText("Get started on Deriv Bot", { timeout: 15000 }).should(
-      "be.visible"
-    )
-    if (Cypress.config().baseUrl.includes("staging"))
-      cy.url().should("eql", dBotStagingUrl)
-    else cy.url().should("eql", dBotProdUrl)
+    cy.findByTestId('dt_platform_switcher').click()
+    cy.get('a[href="/bot"]').click()
+    cy.get('.dc-dialog__footer > .dc-btn--secondary').click()
+    cy.get('#id-charts').click()
+    cy.get('.cq-top-ui-widgets > .ciq-menu > .cq-menu-btn').click()
+    cy.get('.sc-mcd__item--1HZ10V > .sc-mcd__item__name').click()
+    cy.findByTestId('dt_platform_switcher').click()
+    cy.get('a[href="/"]').click()
+    // Verify the chart loaded 
+    cy.xpath('//*[contains(@class,"smartcharts") and contains(@class, "loading")]').should('exist')
+    // Verify account remained logged in
+    cy.get('.dc-btn').should('exist')
+     
   })
 
 })
