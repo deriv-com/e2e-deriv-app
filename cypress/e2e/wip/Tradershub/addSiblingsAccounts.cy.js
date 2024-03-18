15465ca53d0a539cccac9116e74ed0b0b0bfd2e0
Indulekha <indulekha@regentmarkets.com>
2024-03-18 11:42:22 +0400

    Addsiblingsaccount

--- cypress/e2e/wip/Tradershub/addSiblingsAccounts.cy.js
+++ cypress/e2e/wip/Tradershub/addSiblingsAccounts.cy.js
@@ -1,10 +1,10 @@
 import "@testing-library/cypress/add-commands"
 import {generateEpoch} from '../../../support/tradersHub'
 
-function createSiblingacct(i){
+function createSiblingAcct(acctCount){
     cy.findByTestId('dt_currency-switcher__arrow').click()
     cy.findByRole('button', { name: 'Add or manage account' }).click()
-    cy.get('.currency-list__item').eq(i).click()
+    cy.get('.currency-list__item').eq(acctCount).click()
     cy.findByRole('button', { name: 'Add account' }).click()
     cy.findByRole('heading', { name: 'Success!' }).should('be.visible')
     cy.findByRole('button', { name: 'Deposit now' }).should('be.enabled')
@@ -18,17 +18,13 @@ function getCurrencyList() {
 }
 describe("QATEST-5797, QATEST-5820 - Add siblings accounts", () => {
     const epoch = generateEpoch()
-    const sign_up_mail = `sanity${epoch}@deriv.com`
+    const signUpMail = `sanity${epoch}@deriv.com`
     let country = Cypress.env("countries").CO
     let nationalIDNum = Cypress.env("nationalIDNum").CO
     let taxIDNum = Cypress.env("taxIDNum").CO
     let currency = Cypress.env("accountCurrency").USD
     beforeEach(() => {
-      localStorage.setItem("config.server_url", Cypress.env("stdConfigServer"))
-      localStorage.setItem("config.app_id", Cypress.env("stdConfigAppId"))
-      cy.c_visitResponsive("/endpoint", "desktop")
-      cy.findByRole("button", { name: "Sign up" }).should("not.be.disabled")
-      cy.c_enterValidEmail(sign_up_mail)
+     cy.c_setEndpoint(signUpMail)
     })
 
 it("Create siblings accounts from USD account ", () =>{
@@ -56,8 +52,8 @@ it("Create siblings accounts from USD account ", () =>{
   cy.findByRole('button', { name: 'Add or manage account' }).click()
   getCurrencyList().then((labelCount) => {
   cy.get('.dc-modal-header__close').click()
-    for (let i = 0; i < labelCount; i++) {
-    createSiblingacct(i)
+    for (let acctCount = 0; acctCount < labelCount; acctCount++) {
+    createSiblingAcct(acctCount)
     }
   })
   })
