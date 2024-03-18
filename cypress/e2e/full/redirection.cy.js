1424872b44c368e35d502daf87f89b9142c51b0a
maliheh <maliheh@regentmarkets.com>
2024-03-18 12:15:21 +0800

    update locators

--- cypress/e2e/full/redirection.cy.js
+++ cypress/e2e/full/redirection.cy.js
@@ -1,5 +1,5 @@
 import '@testing-library/cypress/add-commands'
-describe('WALL-3591 -production bug WALL-3588', () => {
+describe('WALL-3591 - Verify changing platforms and markets in Dtrader', () => {
   beforeEach(() => {
     cy.c_login()
   })
@@ -7,17 +7,16 @@ describe('WALL-3591 -production bug WALL-3588', () => {
   it('should remain logged in after redirection to another platform', () => {
     cy.findByTestId('dt_platform_switcher').click()
     cy.get('a[href="/bot"]').click()
-    cy.get('.dc-dialog__footer > .dc-btn--secondary').click()
-    cy.get('#id-charts').click()
-    cy.get('.cq-top-ui-widgets > .ciq-menu > .cq-menu-btn').click()
-    cy.get('.sc-mcd__item--1HZ10V > .sc-mcd__item__name').click()
+    cy.findByRole('button', { name: 'Skip' }).click()
+    cy.findByText('Charts').click()
+    cy.get('.cq-symbol-select-btn').click()
+    cy.get('#dbot').findByText('Volatility 10 (1s) Index').click()
     cy.findByTestId('dt_platform_switcher').click()
     cy.get('a[href="/"]').click()
     // Verify the chart loaded 
-    cy.xpath('//*[contains(@class,"smartcharts") and contains(@class, "loading")]').should('exist')
+    cy.get('.chart-container__loader').should('not.exist', { timeout: 10000 })
     // Verify account remained logged in
-    cy.get('.dc-btn').should('exist')
-     
+    cy.findByRole('button', { name: 'Deposit' }).should('exist')
   })
 
 })
