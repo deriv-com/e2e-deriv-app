537d9dbabc2de1711844711e8f3cbcda0f6f281a
Bertrand Mutangana <133766175+bertrand-deriv@users.noreply.github.com>
2024-03-18 05:22:25 +0200

    IDV POI Name Mismatch Test cases (#116)
    * Create idvPOI file and write common steps
    
    * Made assertions on the proof of identity page
    
    * reverting login statements
    
    * Add steps to reach POI page
    
    * Use the login command
    
    * Add steps for name mismatch scenario
    
    * remove node modules
    
    * remove node
    
    * ommit node modules
    
    * Add Custom tag
    
    * Commit a  verify button
    
    * Remove duplicate command
    
    * Refactor to use qa provider
    
    * update the issuing country
    
    * Wrote tests for Name mismatch scenario and add custom commands
    
    * refactor to align with mobile view
    
    * Refactor tests
    
    * remove duplicate
    
    * use strings quotes
    
    * rename and improvements
    
    * Implement requested changes
    
    * check close notification
    
    * improvements on codes - remove unnecesarry commands
    
    * move kyc commands in a separate file
    
    * use single quotes
    
    * Improve custom commands

---
+++ cypress/e2e/wip/KYC/idvPoiNameMismatch.cy.js
@@ -0,0 +1,27 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-23015 - IDV POI Name Mismatch - Mobile view', () => {
+    beforeEach(()=> {
+        cy.c_login()
+        cy.c_navigateToPoiResponsive('Republic of QA')
+    })
+    
+    it('Should return Name mismatch', () => {
+        cy.get('select[name="document_type"]').select('Passport')
+        cy.findByLabelText('Enter your document number').type('12345678')
+        cy.findByTestId('first_name').clear().type('Refuted')
+        cy.findByTestId('last_name').clear().type('Name')
+        cy.findByTestId('date_of_birth').type('2000-09-20')
+
+        cy.findByRole('button', { name: 'Verify' }).should('be.disabled')
+        cy.get('.dc-checkbox__box').click()
+        cy.findByRole('button', { name: 'Verify' }).should('be.enabled').click()
+        cy.contains("Your documents were submitted successfully").should('be.visible')
+        cy.findByText("Proof of address required").should('exist')
+        cy.reload()
+
+        cy.contains("Your identity verification failed").should('be.visible')
+        cy.contains("The name on your identity document doesn't match your profile.").should('be.visible')
+
+    })
+})
--- cypress/e2e/wip/Tradershub/addUSDAccount.cy.js
+++ cypress/e2e/wip/Tradershub/addUSDAccount.cy.js
@@ -1,33 +1,32 @@
-import "@testing-library/cypress/add-commands"
-import {generateEpoch} from '../../../support/tradersHub'
+import '@testing-library/cypress/add-commands'
+import { generateEpoch } from '../../../support/tradersHub'
 
-  describe("QATEST 5813 - Add USD account for existing BTC account", () => {
-    const epoch = generateEpoch()
-    const signUpMail = `sanity${epoch}crypto@deriv.com`
-    let country = Cypress.env("countries").CO
-    let nationalIDNum = Cypress.env("nationalIDNum").CO
-    let taxIDNum = Cypress.env("taxIDNum").CO
-    let currency = Cypress.env("accountCurrency").BTC
-  
-    beforeEach(() => {
-      cy.c_setEndpoint(signUpMail)
+describe('QATEST 5813 - Add USD account for existing BTC account', () => {
+  const epoch = generateEpoch()
+  const signUpMail = `sanity${epoch}crypto@deriv.com`
+  let country = Cypress.env('countries').CO
+  let nationalIDNum = Cypress.env('nationalIDNum').CO
+  let taxIDNum = Cypress.env('taxIDNum').CO
+  let currency = Cypress.env('accountCurrency').BTC
+
+  beforeEach(() => {
+    cy.c_setEndpoint(signUpMail)
+  })
+  it('Create a new crypto account and add USD account', () => {
+    cy.c_demoAccountSignup(epoch, country)
+    cy.c_switchToReal()
+    cy.c_completeTradersHubTour()
+    cy.findByRole('button', { name: 'Get a Deriv account' }).click()
+    cy.c_generateRandomName().then((firstName) => {
+      cy.c_personalDetails(
+        firstName,
+        'Onfido',
+        country,
+        nationalIDNum,
+        taxIDNum,
+        currency
+      )
     })
-    it("Create a new crypto account and add USD account", () => {
-      
-      cy.c_demoAccountSignup(epoch, country)
-      cy.c_switchToReal()
-      cy.c_completeTradersHubTour()
-      cy.findByRole("button", { name: "Get a Deriv account" }).click()
-      cy.c_generateRandomName().then((firstName) => {
-        cy.c_personalDetails(
-          firstName,
-          "Onfido",
-          country,
-          nationalIDNum,
-          taxIDNum,
-          currency
-        )
-      })
     cy.c_addressDetails()
     cy.c_completeFatcaDeclarationAgreement()
     cy.c_addAccount()
@@ -38,8 +37,10 @@ import {generateEpoch} from '../../../support/tradersHub'
     cy.findByText('Fiat currencies').click()
     cy.findByText('US Dollar').click()
     cy.findByRole('button', { name: 'Add account' }).click()
-    cy.findByRole('heading', { name: 'Success!' }).should('be.visible', { timeout: 30000 }) 
-    cy.findByText('You have added a USD account.').should("be.visible")
-    cy.findByRole('button', { name: 'Maybe later' }).click()
+    cy.findByRole('heading', { name: 'Success!' }).should('be.visible', {
+      timeout: 30000,
     })
+    cy.findByText('You have added a USD account.').should('be.visible')
+    cy.findByRole('button', { name: 'Maybe later' }).click()
   })
+})
