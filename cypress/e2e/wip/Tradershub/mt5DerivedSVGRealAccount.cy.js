40ca7e528acb832e08b63b9a7d6fad57d9d21e35
Indulekha <indulekha@regentmarkets.com>
2024-03-20 20:13:01 +0400

    gh tradershub

--- cypress/e2e/wip/Tradershub/mt5DerivedSVGRealAccount.cy.js
+++ cypress/e2e/wip/Tradershub/mt5DerivedSVGRealAccount.cy.js
@@ -6,11 +6,16 @@ describe("QATEST-5972: Create a Derived SVG account", () => {
   let country = Cypress.env("countries").CO
   let nationalIDNum = Cypress.env("nationalIDNum").CO
   let taxIDNum = Cypress.env("taxIDNum").CO
+  const cashierURL = Cypress.env("cashierURL")
+  const derivAppProdUrl = Cypress.env("prodURL")
+  const derivAppStagingUrl = Cypress.env("stagingUrl")
 
   beforeEach(() => {
     cy.c_setEndpoint(signUpEmail)
   })
   it("Verify I can signup for a real derived SVG CFD account", () => {
+    cy.log('cashier URL1'+Cypress.env("cashierURL") )
+    cy.log('cashier page constant' + cashierURL)
     cy.c_demoAccountSignup(country, signUpEmail)
     cy.c_checkTradersHubHomePage()
     cy.findByTestId('dt_dropdown_display').click()
@@ -54,9 +59,10 @@ describe("QATEST-5972: Create a Derived SVG account", () => {
     cy.findByRole('button', { name: 'Transfer now' }).should('exist')
     cy.findByRole('button', { name: 'Maybe later' }).click()
     cy.findByText('0.00 USD').should('be.visible')
-    cy.findByRole('button', { name: 'Transfer' }).should('exist')
-    cy.get('button:nth-child(2)').click()
-    cy.get('#modal_root').findByText('Derived SVG').should('be.visible')
-    cy.get('#modal_root').findByText('Deriv (SVG) LLC').should('be.visible')
+    cy.findByRole('button', { name: 'Transfer' }).should('exist').click()
+    cy.findByRole('button', { name: 'Deposit now' }).click()
+    if (Cypress.config().baseUrl.includes("staging"))
+    cy.url().should("eql", derivAppStagingUrl + cashierURL)
+    else cy.url().should("eql", derivAppProdUrl + cashierURL)
   })
 })
