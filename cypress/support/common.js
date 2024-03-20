cc741389c15b9b7ebc5dba1e5569a69cbf561276
bertrand <bertrand@regentmarkets.com>
2024-03-20 16:57:46 +0200

    getLogingToken should match master

--- cypress/support/common.js
+++ cypress/support/common.js
@@ -1,41 +1,64 @@
 export function getLoginToken(callback) {
   cy.request({
     method: 'POST',
-    url: 'https://' + Cypress.env('configServer') + '/oauth2/api/v1/authorize',
+    url: 'https://' + Cypress.env('configServer') + '/oauth2/api/v1/verify',
     headers: {
       Origin: 'https://oauth.deriv.com',
       'Content-Type': 'application/json',
     },
     body: {
       app_id: Cypress.env('configAppId'),
-      expire: expire,
-      solution: solution,
     },
   }).then((response) => {
-    const bearerToken = response.body.token
-    cy.log('<bearer token>' + bearerToken)
-    expect(response.status).to.eq(200)
+    const challenge = response.body.challenge
+    const expire = response.body.expire
+    var crypto = require('crypto')
+    const solution = crypto
+      .createHmac('sha256', Cypress.env('HMACKey'))
+      .update(challenge)
+      .digest('hex')
+
+    cy.log('<solution>' + solution)
 
     cy.request({
       method: 'POST',
-      url: 'https://' + Cypress.env('configServer') + '/oauth2/api/v1/login',
+      url:
+        'https://' + Cypress.env('configServer') + '/oauth2/api/v1/authorize',
       headers: {
-        Authorization: 'Bearer ' + bearerToken,
+        Origin: 'https://oauth.deriv.com',
         'Content-Type': 'application/json',
       },
       body: {
         app_id: Cypress.env('configAppId'),
-        type: 'system',
-        email: Cypress.env('loginEmail'),
-        password: Cypress.env('loginPassword'),
+        expire: expire,
+        solution: solution,
       },
     }).then((response) => {
-      const token = response.body.tokens[0].token
-      cy.log('<login token>' + token)
-
-      callback(token)
-
+      const bearerToken = response.body.token
+      cy.log('<bearer token>' + bearerToken)
       expect(response.status).to.eq(200)
+
+      cy.request({
+        method: 'POST',
+        url: 'https://' + Cypress.env('configServer') + '/oauth2/api/v1/login',
+        headers: {
+          Authorization: 'Bearer ' + bearerToken,
+          'Content-Type': 'application/json',
+        },
+        body: {
+          app_id: Cypress.env('configAppId'),
+          type: 'system',
+          email: Cypress.env('loginEmail'),
+          password: Cypress.env('loginPassword'),
+        },
+      }).then((response) => {
+        const token = response.body.tokens[0].token
+        cy.log('<login token>' + token)
+
+        callback(token)
+
+        expect(response.status).to.eq(200)
+      })
     })
   })
 }
