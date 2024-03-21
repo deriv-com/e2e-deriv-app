a019fe3390cc56f285a71866728785d39c5ccefb
maliheh <maliheh@regentmarkets.com>
2024-03-21 12:39:09 +0800

    updating selector

--- cypress/e2e/full/redirection.cy.js
+++ cypress/e2e/full/redirection.cy.js
@@ -6,13 +6,13 @@ describe('WALL-3591 - Verify changing platforms and markets in Dtrader', () => {
 
   it('should remain logged in after redirection to another platform', () => {
     cy.findByTestId('dt_platform_switcher').click()
-    cy.get('a[href="/bot"]').click()
+    cy.findByText('Automated trading at your fingertips. No coding needed.').click()
     cy.findByRole('button', { name: 'Skip' }).click()
     cy.findByText('Charts').click()
     cy.get('.cq-symbol-select-btn').click()
     cy.get('#dbot').findByText('Volatility 10 (1s) Index').click()
     cy.findByTestId('dt_platform_switcher').click()
-    cy.get('a[href="/"]').click()
+    cy.findByText('A whole new trading experience on a powerful yet easy to use platform.').click()
     // Verify the chart loaded 
     cy.get('.chart-container__loader').should('not.exist', { timeout: 10000 })
     // Verify account remained logged in
