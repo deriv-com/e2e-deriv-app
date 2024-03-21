14c9e2587dd6073c9d34850093202c2466b90b7c
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-03-21 08:41:28 +0800

    moving_out_function

--- cypress/e2e/wip/Crypto/crypto_account_check.cy.js
+++ cypress/e2e/wip/Crypto/crypto_account_check.cy.js
@@ -1,42 +1,42 @@
-import '@testing-library/cypress/add-commands';
-import cryptoconfig from '/Users/vimalanrajakumar/e2e-deriv-app/cypress/e2e/wip/Crypto/Pageobject/common.js';
+import '@testing-library/cypress/add-commands'
+import cryptoconfig from '/Users/vimalanrajakumar/e2e-deriv-app/cypress/e2e/wip/Crypto/Pageobject/common.js'
 
-
+const addcryptoaccount = (crypto, code) => {
+  cryptoconfig.elements.currencyswitcher().should('be.visible').click()
+  cryptoconfig.elements.manageaccount().should('be.visible').click()
+  cy.get('.add-crypto-currency').then(($parent) => {
+    const $input = $parent.find(`input[name="currency"][id="${code}"][disabled]`)
+    if ($input.length > 0) {
+      // Element is disabled
+      cy.log(`${code} input element is disabled.`)
+      cy.get('.dc-modal-header__close').click()
+      cy.c_closeNotificationHeader()
+    } else {
+      // Element is not disabled
+      cy.c_closeNotificationHeader()
+      cy.findByText(crypto).click()
+      cryptoconfig.elements.cryptoaddaccount().should('be.visible').click()
+      cy.findByText('Success!')
+      cy.findByText('Make a deposit now to start trading.')
+      cryptoconfig.elements.maybelater().should('be.visible').click()
+    }})
+  return crypto
+};
 
 describe('QATEST-707 - Create crypto account', () => {
   beforeEach(() => {
     cy.c_login()
     cy.c_visitResponsive('/appstore/traders-hub', 'large')
   })
-  const addcryptoaccount = (crypto, code) => {
-    cryptoconfig.elements.currencyswitcher().should('be.visible').click();
-    cryptoconfig.elements.manageaccount().should('be.visible').click();
-    cy.get('.add-crypto-currency').then(($parent) => {
-      const $input = $parent.find(`input[name="currency"][id="${code}"][disabled]`);
-      if ($input.length > 0) {
-        // Element is disabled
-        cy.log(`${code} input element is disabled.`);
-        cy.get('.dc-modal-header__close').click();
-        cy.c_closeNotificationHeader()
-      } else {
-        // Element is not disabled
-        cy.c_closeNotificationHeader()
-        cy.findByText(crypto).click();
-        cryptoconfig.elements.cryptoaddaccount().should('be.visible').click();
-        cy.findByText('Success!')
-        cy.findByText('Make a deposit now to start trading.')
-        cryptoconfig.elements.maybelater().should('be.visible').click();
-      }})
-    return crypto;
-  };
   it('should be able to create crypto account from Traders Hub.', () => {
+    cy.wait(1000)
     cy.c_closeNotificationHeader()
-    const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"];
-    const currency_code = ["BTC", "ETH", "LTC", "tUSDT", "USDC"];
+    const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"]
+    const currency_code = ["BTC", "ETH", "LTC", "tUSDT", "USDC"]
     // loop to make sure it check for all available currency
     cryptocurrencies.forEach((crypto, index) => {
-        const code = currency_code[index];
-        addcryptoaccount(crypto, code);
+        const code = currency_code[index]
+        addcryptoaccount(crypto, code)
     });
     // to check for the account balance after creation
       cryptoconfig.elements.currencyswitcher().should('be.visible').click();
