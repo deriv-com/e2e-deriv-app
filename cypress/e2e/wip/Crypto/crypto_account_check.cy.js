20b5ba8bf8491225e645676c24f61117072db677
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-03-21 09:25:34 +0800

    updated_code

---
+++ cypress/e2e/wip/Crypto/crypto_account_check.cy.js
@@ -0,0 +1,48 @@
+import '@testing-library/cypress/add-commands'
+import cryptoconfig from './pageobjects/common.js'
+
+const addcryptoaccount = (crypto, code) => {
+  cy.c_closeNotificationHeader()
+  cryptoconfig.elements.currencyswitcher().should('be.visible').click()
+  cryptoconfig.elements.manageaccount().should('be.visible').click()
+  cy.get('.add-crypto-currency').then(($parent) => {
+    const $input = $parent.find(`input[name="currency"][id="${code}"][disabled]`)
+    if ($input.length > 0) {
+      // Element is disabled
+      cy.log(`${code} input element is disabled.`)
+      cy.get('.dc-modal-header__close').click()
+      cy.c_closeNotificationHeader()
+    } else {
+      // Element is not disabled
+      cy.c_closeNotificationHeader()
+      cy.findByText(crypto).click()
+      cryptoconfig.elements.cryptoaddaccount().should('be.visible').click()
+      cy.findByText('Success!')
+      cy.findByText('Make a deposit now to start trading.')
+      cryptoconfig.elements.maybelater().should('be.visible').click()
+    }})
+  return crypto
+};
+
+describe('QATEST-707 - Create crypto account', () => {
+  beforeEach(() => {
+    cy.c_login()
+    cy.c_visitResponsive('/appstore/traders-hub', 'large')
+  })
+  it('should be able to create crypto account from Traders Hub.', () => {
+    const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"]
+    const currency_code = ["BTC", "ETH", "LTC", "tUSDT", "USDC"]
+    // loop to make sure it check for all available currency
+    cryptocurrencies.forEach((crypto, index) => {
+        const code = currency_code[index]
+        addcryptoaccount(crypto, code)
+    });
+    // to check for the account balance after creation
+      cryptoconfig.elements.currencyswitcher().should('be.visible').click();
+      cy.findByText("0.00000000 BTC").should("be.visible")
+      cy.findByText("0.00000000 ETH").should("be.visible")
+      cy.findByText("0.00000000 LTC").should("be.visible")
+      cy.findByText("0.00 tUSDT").should("be.visible")
+      cy.findByText("0.00 USDC").should("be.visible") 
+});
+  })
---
+++ cypress/e2e/wip/Crypto/pageobjects/common.js
@@ -0,0 +1,13 @@
+class cryptoconfig {
+    elements = {
+        currencyswitcher: () => cy.findByTestId('dt_currency-switcher__arrow'),
+        manageaccount: () => cy.get('[class*="dc-btn dc-btn--secondary dc-btn__large block-button"]'),
+        closemanageaccount: () => cy.get('[class*="dc-modal-header__close"]'),
+        cryptoaddaccount: () => cy.get('[class*="dc-btn dc-btn__effect dc-btn--primary dc-btn__large"]'),
+        maybelater: () => cy.findByRole('button', { name: 'Maybe later' }),
+        closenotification: () => cy.get('.notification__close-button')
+ }
+ }
+ 
+ 
+ module.exports = new cryptoconfig()
