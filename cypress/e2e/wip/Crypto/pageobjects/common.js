06f10d1c208cffda772cb3b0c793bd86cf51bffb
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-03-21 10:05:54 +0800

    Update the variable

--- cypress/e2e/wip/Crypto/pageobjects/common.js
+++ cypress/e2e/wip/Crypto/pageobjects/common.js
@@ -1,11 +1,12 @@
 class cryptoconfig {
     elements = {
         currencyswitcher: () => cy.findByTestId('dt_currency-switcher__arrow'),
-        manageaccount: () => cy.get('[class*="dc-btn dc-btn--secondary dc-btn__large block-button"]'),
-        closemanageaccount: () => cy.get('[class*="dc-modal-header__close"]'),
-        cryptoaddaccount: () => cy.get('[class*="dc-btn dc-btn__effect dc-btn--primary dc-btn__large"]'),
+        manageaccount: () => cy.findByRole('button', { name: 'Add or manage account' }),
+        //closemanageaccount: () => cy.get('[class*="dc-modal-header__close"]'),
+        closemanageaccount: () => cy.findByTestId('dt_modal_close_icon'),
+        cryptoaddaccount: () => cy.findByRole('button', { name: 'Add account' }),
+        //cryptoaddaccount: () => cy.get('[class*="dc-btn dc-btn__effect dc-btn--primary dc-btn__large"]'),
         maybelater: () => cy.findByRole('button', { name: 'Maybe later' }),
-        closenotification: () => cy.get('.notification__close-button')
     }
  }
  
