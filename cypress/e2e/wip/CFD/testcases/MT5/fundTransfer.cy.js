885b8eef1fff1349570ec8fa56c46a3e9ea57b0d
Bilal Bin Majid <155507124+bilalmajid-deriv@users.noreply.github.com>
2024-03-21 18:47:24 +0800

    apply prettier formating. (#156)

--- cypress/e2e/wip/CFD/testcases/MT5/fundTransfer.cy.js
+++ cypress/e2e/wip/CFD/testcases/MT5/fundTransfer.cy.js
@@ -2,22 +2,21 @@ import '@testing-library/cypress/add-commands'
 import mt5_tradershub from '../../Pages/mt5PageObjects'
 import fundTransferPageObjects from '../../Pages/fundTransferPageObjects'
 
-describe("CFDS-2826 - Fund transfer of MT5 USD to USD", () => {
-    beforeEach(() => {
-        cy.clearCookies()
-        cy.clearLocalStorage()
-        cy.c_login()
-        cy.c_visitResponsive('/appstore/traders-hub', 'large')
-    })
-    
-    it('MT5 account deposit, USD to Derived SVG',() => {
-        mt5_tradershub.selectRealAccount()
-        fundTransferPageObjects.mt5Deposit()
-    })
+describe('QATEST-37180 - MT5 Deposit & withdrawal for same fiat currency (USD-USD)', () => {
+  beforeEach(() => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.c_login()
+    cy.c_visitResponsive('/appstore/traders-hub', 'large')
+  })
 
-    it('MT5 account withdrawal, Derived SVG to USD',() => {
-        mt5_tradershub.selectRealAccount()
-        fundTransferPageObjects.mt5Withdrawal()
-    })
+  it('MT5 account deposit, USD to Derived SVG', () => {
+    mt5_tradershub.selectRealAccount()
+    fundTransferPageObjects.mt5Deposit()
+  })
 
+  it('MT5 account withdrawal, Derived SVG to USD', () => {
+    mt5_tradershub.selectRealAccount()
+    fundTransferPageObjects.mt5Withdrawal()
+  })
 })
