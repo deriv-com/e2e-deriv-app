ff1b51d5816c0b04fe3012e137c02554594df570
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-03-22 13:30:25 +0800

    to apply prettier

--- cypress/e2e/wip/Crypto/crypto_account_check.cy.js
+++ cypress/e2e/wip/Crypto/crypto_account_check.cy.js
@@ -2,12 +2,13 @@ import '@testing-library/cypress/add-commands'
 import cryptoconfig from './pageobjects/common.js'
 import { generateEpoch } from '../../../support/tradersHub'
 
-
 const addcryptoaccount = (crypto, code) => {
   cryptoconfig.elements.currencyswitcher().should('be.visible').click()
   cryptoconfig.elements.manageaccount().should('be.visible').click()
   cy.get('.add-crypto-currency').then(($parent) => {
-    const $input = $parent.find(`input[name="currency"][id="${code}"][disabled]`)
+    const $input = $parent.find(
+      `input[name="currency"][id="${code}"][disabled]`
+    )
     if ($input.length > 0) {
       // Element is disabled
       cy.log(`${code} input element is disabled.`)
@@ -21,16 +22,17 @@ const addcryptoaccount = (crypto, code) => {
       cy.findByText('Success!')
       cy.findByText('Make a deposit now to start trading.')
       cryptoconfig.elements.maybelater().should('be.visible').click()
-    }})
+    }
+  })
   return crypto
-};
+}
 
 describe('QATEST-707 - Create crypto account', () => {
   const signUpEmail = `sanity${generateEpoch()}crypto@deriv.com`
-  let country = Cypress.env("countries").CO
-  let nationalIDNum = Cypress.env("nationalIDNum").CO
-  let taxIDNum = Cypress.env("taxIDNum").CO
-  let currency = Cypress.env("accountCurrency").USD
+  let country = Cypress.env('countries').CO
+  let nationalIDNum = Cypress.env('nationalIDNum').CO
+  let taxIDNum = Cypress.env('taxIDNum').CO
+  let currency = Cypress.env('accountCurrency').USD
   beforeEach(() => {
     cy.c_setEndpoint(signUpEmail)
   })
@@ -38,35 +40,41 @@ describe('QATEST-707 - Create crypto account', () => {
     cy.c_demoAccountSignup(country, signUpEmail)
     cy.c_switchToReal()
     cy.c_completeTradersHubTour()
-    cy.findByRole("button", { name: "Get a Deriv account" }).click()
+    cy.findByRole('button', { name: 'Get a Deriv account' }).click()
     cy.c_generateRandomName().then((firstName) => {
-    cy.c_personalDetails(
-          firstName,
-          "Onfido",
-          country,
-          nationalIDNum,
-          taxIDNum,
-          currency
-        )
-      })
-   cy.c_addressDetails()
-   cy .c_completeFatcaDeclarationAgreement()
-   cy.c_addAccount()
-   cy.c_checkTradersHubHomePage()
-   cy.c_closeNotificationHeader()
-    const cryptocurrencies = ["Bitcoin", "Ethereum", "Litecoin", "Tether TRC20", "USD Coin"]
-    const currency_code = ["BTC", "ETH", "LTC", "tUSDT", "USDC"]
+      cy.c_personalDetails(
+        firstName,
+        'Onfido',
+        country,
+        nationalIDNum,
+        taxIDNum,
+        currency
+      )
+    })
+    cy.c_addressDetails()
+    cy.c_completeFatcaDeclarationAgreement()
+    cy.c_addAccount()
+    cy.c_checkTradersHubHomePage()
+    cy.c_closeNotificationHeader()
+    const cryptocurrencies = [
+      'Bitcoin',
+      'Ethereum',
+      'Litecoin',
+      'Tether TRC20',
+      'USD Coin',
+    ]
+    const currency_code = ['BTC', 'ETH', 'LTC', 'tUSDT', 'USDC']
     // loop to make sure it check for all available currency
     cryptocurrencies.forEach((crypto, index) => {
-        const code = currency_code[index]
-        addcryptoaccount(crypto, code)
-    });
+      const code = currency_code[index]
+      addcryptoaccount(crypto, code)
+    })
     // to check for the account balance after creation
-      cryptoconfig.elements.currencyswitcher().should('be.visible').click();
-      cy.findByText("0.00000000 BTC").should("be.visible")
-      cy.findByText("0.00000000 ETH").should("be.visible")
-      cy.findByText("0.00000000 LTC").should("be.visible")
-      cy.findByText("0.00 tUSDT").should("be.visible")
-      cy.findByText("0.00 USDC").should("be.visible") 
-});
+    cryptoconfig.elements.currencyswitcher().should('be.visible').click()
+    cy.findByText('0.00000000 BTC').should('be.visible')
+    cy.findByText('0.00000000 ETH').should('be.visible')
+    cy.findByText('0.00000000 LTC').should('be.visible')
+    cy.findByText('0.00 tUSDT').should('be.visible')
+    cy.findByText('0.00 USDC').should('be.visible')
   })
+})
