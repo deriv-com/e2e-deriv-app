64c2c434021fa9767fe8e4d17d792793ba879049
lubega-deriv <lubega@deriv.com>
2024-03-22 14:54:33 +0800

    fix: update styling

--- cypress/e2e/wipWallet/demoResetBalance.cy.js
+++ cypress/e2e/wipWallet/demoResetBalance.cy.js
@@ -3,8 +3,10 @@ import '@testing-library/cypress/add-commands'
 function reset_balance_demo() {
   cy.get('.wallets-dropdown__button').click()
   cy.findByText('USD Demo Wallet').scrollIntoView()
-  cy.get('.wallets-list-card-dropdown__item-content').contains('USD Demo Wallet').click()
-  cy.get('.wallets-list-details__content').within(()=>{
+  cy.get('.wallets-list-card-dropdown__item-content')
+    .contains('USD Demo Wallet')
+    .click()
+  cy.get('.wallets-list-details__content').within(() => {
     cy.contains('USD').should('be.visible')
   })
   cy.contains('Reset balance').should('be.visible')
