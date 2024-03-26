086a2c7327c7828b72e5560e07245acd521a9a97
bertrand <bertrand@regentmarkets.com>
2024-03-26 16:53:04 +0200

    Create smoke createAccount test

---
+++ cypress/e2e/smoke/apiCreateAccountAndLogin.cy.js
@@ -0,0 +1,14 @@
+// import '@testing-library/cypress/add-commands'
+// const { getOAuthLogin } = require('../../support/common')
+
+describe('Get oAuthUrl', () => {
+  beforeEach(() => {
+    cy.log('<E2EOAuthUrl - beforeEach>' + Cypress.env('oAuthUrl'))
+    cy.c_createRealAccount()
+    cy.c_login()
+  })
+
+  it('should get the oauth url from API', () => {
+    cy.log('<E2EOAuthUrl - Test 1>' + Cypress.env('oAuthUrl'))
+  })
+})
