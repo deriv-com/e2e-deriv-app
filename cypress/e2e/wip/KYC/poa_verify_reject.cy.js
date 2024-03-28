3dc56de63a6fa515ca3aa76330542449398a3a6c
bertrand <bertrand@regentmarkets.com>
2024-03-28 13:13:02 +0200

    Create test file for POA

---
+++ cypress/e2e/wip/KYC/poa_verify_reject.cy.js
@@ -0,0 +1,9 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-4835 - Testing Verify/reject POA - Mobile view', () => {
+  beforeEach(() => {
+    cy.c_login()
+  })
+
+  it('Should show verified page when the POA is verified', () => {})
+})
