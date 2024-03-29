24ba576513b40974ce98200fb7573743c5afa79f
Bertrand Mutangana <133766175+bertrand-deriv@users.noreply.github.com>
2024-03-29 11:24:57 +0200

    API account creation command (#183)
    * develop account creation function
    
    * Account creation and added cypress task
    
    * refactor and Adding createVirtualAccountTask
    
    * Create a command for account creation
    
    * Create smoke createAccount test
    
    * Use crypto module which is more secure
    
    * Refactor to use secure crypto module
    
    * Remove unnecessary comments
    
    * Remove unnecessary import from common.js
    
    * Using already configurable env. var
    
    * Remove unneccessary comment
    
    * Rename file
    
    * Move utility functions in the new file

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require("cypress")
 require("dotenv").config()
+const { defineConfig } = require("cypress")
+const {createAccountReal, createAccountVirtual} = require('./cypress/support/helper/accountCreationUtility');
 
 //const gViewPortSize = {small: 'phone-xr', large: 'macbook-16'} //TODO Use enum
  
@@ -12,6 +13,30 @@ module.exports = defineConfig({
     supportFile: "cypress/support/e2e.js",
     experimentalWebKitSupport: true,
     chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      on('task', {
+        async createRealAccountTask() {
+          try {
+            const realAccountDetails = await createAccountReal();
+            return realAccountDetails;
+          } catch (error) {
+            console.error('Error creating account:', error);
+            throw error;
+          }
+        },
+        async createVirtualAccountTask() {
+          try {
+              const virtualAccountDetails = await createAccountVirtual();
+              return virtualAccountDetails;
+          } catch (error) {
+              console.error('Error creating virtual account:', error);
+              throw error;
+          }
+      },
+      });
+
+      return config;  // Return the config object is important for custom configurations to take effect
+    },
   },
   env: {
     stagingUrl: "https://staging-app.deriv.com/",
