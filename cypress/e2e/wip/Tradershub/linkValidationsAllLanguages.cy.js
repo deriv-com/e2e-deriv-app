e0ee756ca2ba9de917ebaf75e5f3d53bf4a71c7f
infanta-deriv <infanta@regentmarkets.com>
2024-03-30 17:24:05 +0400

    validatealllang

--- cypress/e2e/wip/Tradershub/linkValidationsAllLanguages.cy.js
+++ cypress/e2e/wip/Tradershub/linkValidationsAllLanguages.cy.js
@@ -5,27 +5,8 @@ describe('TRAH-2997 Verify the hyperlinks on Traders Hub', () => {
     cy.c_login()
   })
 
-  it('Should validate the hyperlinks in tradershub for english and spanish', () => {
+  it('Should validate the hyperlinks in tradershub in all languages', () => {
     cy.c_visitResponsive('/appstore/traders-hub', 'large')
-    /*cy.checkHyperLinks('EN')
-    cy.findAllByTestId('dt_icon').eq(0).click()
-    cy.findByText('Select Language').should('be.visible')
-    cy.findAllByTestId('dt_settings_language_button').eq(0).click()
-    cy.c_rateLimit()
-    cy.findByText('Cajero').should('be.visible')
-    cy.checkHyperLinks('ES')
-    cy.findAllByTestId('dt_icon').eq(1).click()
-    cy.findByText('Select Language').should('be.visible')
-    cy.findAllByTestId('dt_settings_language_button').eq(0).click()
-    cy.c_rateLimit()
-    cy.findByText('abcdf').should('be.visible')
-    cy.checkHyperLinks('DE')
-    cy.findAllByTestId('dt_icon').eq(2).click()
-    cy.findByText('Select Language').should('be.visible')
-    cy.findAllByTestId('dt_settings_language_button').eq(0).click()
-    cy.c_rateLimit()
-    cy.findByText('fsdfs').should('be.visible')
-    cy.checkHyperLinks('BO')*/
     cy.checkAllLanguages()
   })
 })
