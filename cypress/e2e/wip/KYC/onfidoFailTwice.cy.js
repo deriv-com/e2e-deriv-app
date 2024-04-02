ee14d55e33e44aa84c957534a655678c67a2149c
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-04-02 16:59:47 +0200

    updated my code

--- cypress/e2e/wip/KYC/onfidoFailTwice.cy.js
+++ cypress/e2e/wip/KYC/onfidoFailTwice.cy.js
@@ -13,26 +13,10 @@ describe('QATEST-22853 Onfido (2 attempts) failed clients are redirected to manu
     cy.findByText('Submit passport photo pages').should('be.visible')
     cy.findByText('or upload photo – no scans or photocopies').click()
     cy.findByText('Upload passport photo page').should('be.visible')
-    cy.findByText('Upload photo').click()
-    cy.findByRole('button', { name: 'Save and submit' }).should('be.disabled')
-    cy.fixture('kyc/testDriversLicense.jpeg').then((fileContent) => {
-      cy.findByTestId('dt_file_upload_input').selectFile({
-        contents: fileContent,
-        fileName: 'testDriversLicense.jpeg',
-      })
-    })
-    /**cy.findByRole('button', { name: 'or upload photo – no scans or' }).click()
-    cy.findByRole('button', { name: 'Continue' }).click()
-    cy.findByRole('button', { name: 'Continue' }).setInputFiles('uk-driving-licence.jpeg')
-    cy.findByRole('button', { name: 'Upload' }).click()
-    cy.findByRole('button', { name: 'Continue' }).click()
-    cy.findByRole('heading', { name: 'Allow camera access' }).click()
-    cy.findByRole('button', { name: 'Enable camera' }).click()
-    cy.findByLabel('Take a photo').click()
-    cy.findByRole('button', { name: 'Upload' }).click()
-
-    // try again button
-    //cy.findByRole('button', { name: 'Verify again' }).click()
-    **/
+    cy.get('input[type=file]').selectFile(
+      'cypress/fixtures/kyc/testDriversLicense.jpeg',
+      { force: true }
+    )
+    cy.findByText('Confirm').click()
   })
 })
