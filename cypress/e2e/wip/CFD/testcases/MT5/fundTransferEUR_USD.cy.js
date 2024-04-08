c2968da688d7c0ef4932f08e9b879c1471cc6b5f
ravali-deriv <106730390+ravali-deriv@users.noreply.github.com>
2024-04-08 08:10:42 +0000

    MT5 fundtransfer for all fiat currencies (#112)
    * [ci skip] new fundtransfer scripts of mt5
    
    * [ci skip] new fundtransfer scripts of mt5
    
    * fundtransfer in page object model
    
    * modified waits
    
    * added spaces
    
    * deleted fundTransfer.cy.js
    
    * apply prettier formating. (#156)
    
    * fundtransfer in page object model
    
    * fundtransfer modified code
    
    * reverting to previous version
    
    * fixed duplication
    
    * apply prettier formating. (#156)
    
    * [ci skip] new fundtransfer scripts of mt5
    
    * fundtransfer in page object model
    
    * added spaces
    
    * apply prettier formating. (#156)
    
    * added prettier ignore back and removed fundTransfer.cy.js
    
    * reolved merging issue
    
    * reolved merging issues
    
    * reolved merging issues
    
    * reolved merging issues
    
    ---------
    
    Co-authored-by: Bilal Bin Majid <155507124+bilalmajid-deriv@users.noreply.github.com>
    Co-authored-by: bilalmajid-deriv <bilal.majid@regentmarkets.com>

---
+++ cypress/e2e/wip/CFD/testcases/MT5/fundTransferEUR_USD.cy.js
@@ -0,0 +1,22 @@
+import '@testing-library/cypress/add-commands'
+import mt5_tradershub from '../../Pages/mt5PageObjects'
+import fundTransferPageObjects from '../../Pages/fundTransferPageObjects'
+
+describe('QATEST-37204 - MT5 Deposit & withdrawal for different fiat currencies (EUR-USD)', () => {
+  beforeEach(() => {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.c_login()
+    cy.c_visitResponsive('/appstore/traders-hub', 'large')
+  })
+
+  it('MT5 account deposit, EUR to Derived SVG', () => {
+    mt5_tradershub.selectRealAccount()
+    fundTransferPageObjects.mt5Deposit('Euro', 'Derived SVG')
+  })
+
+  it('MT5 account withdrawal, Derived SVG to EUR', () => {
+    mt5_tradershub.selectRealAccount()
+    fundTransferPageObjects.mt5Withdrawal('Euro', 'Derived SVG')
+  })
+})
--- cypress/e2e/wip/CFD/testcases/MT5/fundTransferGBP_USD.cy.js
+++ cypress/e2e/wip/CFD/testcases/MT5/fundTransferGBP_USD.cy.js
@@ -2,7 +2,7 @@ import '@testing-library/cypress/add-commands'
 import mt5_tradershub from '../../Pages/mt5PageObjects'
 import fundTransferPageObjects from '../../Pages/fundTransferPageObjects'
 
-describe('QATEST-37180 - MT5 Deposit & withdrawal for same fiat currency (USD-USD)', () => {
+describe('QATEST-37216 - MT5 Deposit & withdrawal for different fiat currencies (GBP-USD)', () => {
   beforeEach(() => {
     cy.clearCookies()
     cy.clearLocalStorage()
@@ -10,13 +10,13 @@ describe('QATEST-37180 - MT5 Deposit & withdrawal for same fiat currency (USD-US
     cy.c_visitResponsive('/appstore/traders-hub', 'large')
   })
 
-  it('MT5 account deposit, USD to Derived SVG', () => {
+  it('MT5 account deposit, GBP to Derived SVG', () => {
     mt5_tradershub.selectRealAccount()
-    fundTransferPageObjects.mt5Deposit()
+    fundTransferPageObjects.mt5Deposit('Pound Sterling', 'Derived SVG')
   })
 
-  it('MT5 account withdrawal, Derived SVG to USD', () => {
+  it('MT5 account withdrawal, Derived SVG to GBP', () => {
     mt5_tradershub.selectRealAccount()
-    fundTransferPageObjects.mt5Withdrawal()
+    fundTransferPageObjects.mt5Withdrawal('Pound Sterling', 'Derived SVG')
   })
 })
