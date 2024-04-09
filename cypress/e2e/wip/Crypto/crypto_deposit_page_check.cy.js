fa96de09492185ba3d6dccd6c8cfee1ea706eaca
vimalan-deriv <vimalan.rajakumar@regentmarkets.com>
2024-04-09 09:18:09 +0800

    crypto_deposit

---
+++ cypress/e2e/wip/Crypto/crypto_deposit_page_check.cy.js
@@ -0,0 +1,39 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-707 - Create crypto account', () => {
+  beforeEach(() => {
+    cy.c_login()
+    cy.c_visitResponsive('/appstore/traders-hub', 'large')
+  })
+
+  it('should be able to create crypto account from Traders Hub.', () => {
+    cy.c_closeNotificationHeader()
+    cy.findByTestId('dt_currency-switcher__arrow').should('be.visible').click()
+    cy.findByText('Bitcoin').click()
+    cy.c_closeNotificationHeader()
+    cy.findByRole('button', { name: 'Deposit' }).click()
+    cy.xpath('//canvas[contains(@class,"qrcode")]').should('exist') // assert that the QR code element exists
+    cy.get('.deposit-crypto-wallet-address__hash-container').should('exist') // Assert that the container exists
+    cy.get('.deposit-crypto-wallet-address__action-container').click()
+    cy.get('.deposit-crypto-wallet-address__hash-container')
+      .invoke('text')
+      .then((expectedValue) => {
+        cy.get('.deposit-crypto-wallet-address__action-container')
+          .click()
+          .then(() => {
+            // After clicking, retrieve the value from the clipboard
+            cy.window().then((win) => {
+              cy.wrap(win.navigator.clipboard.readText()).then(
+                (copiedValue) => {
+                  // Assert that the copied value matches the expected value
+                  expect(copiedValue.trim()).to.equal(expectedValue.trim())
+                }
+              )
+            })
+          })
+      })
+    cy.findAllByText('Transaction status').should('be.visible')
+    cy.get('.transactions-crypto-transaction-status-side-note').should('exist')
+    cy.get('.side-note').should('exist')
+  })
+})
