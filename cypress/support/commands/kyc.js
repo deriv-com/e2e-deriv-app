fb02bf72186cbf220bf8178b9333990d850d5971
bertrand <bertrand@regentmarkets.com>
2024-04-09 15:01:14 +0200

    Add custom command

--- cypress/support/commands/kyc.js
+++ cypress/support/commands/kyc.js
@@ -14,3 +14,13 @@ Cypress.Commands.add('c_navigateToPoiResponsive', (country) => {
   cy.get('select[name="country_input"]').select(country)
   cy.contains('button', 'Next').click()
 })
+
+Cypress.Commands.add('c_submitIdv', () => {
+  cy.get('select[name="document_type"]').select('Passport')
+  cy.findByLabelText('Enter your document number').type('12345678')
+  cy.findByTestId('first_name').clear().type('Refuted')
+  cy.findByTestId('last_name').clear().type('Name')
+  cy.findByTestId('date_of_birth').type('2000-09-20')
+  cy.get('.dc-checkbox__box').click()
+  cy.findByRole('button', { name: 'Verify' }).click()
+})
