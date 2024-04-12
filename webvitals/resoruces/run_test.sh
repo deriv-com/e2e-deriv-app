f801057a048ed5edb7f2db3bb5c38fcabcb3fc7c
mohammed.luqmaan-deriv <mohammed.luqmaan@deriv.com>
2024-04-12 11:01:10 +0800

    [ci skip]

--- webvitals/resoruces/run_test.sh
+++ webvitals/resoruces/run_test.sh
@@ -3,4 +3,4 @@
 # #     Inputs expected from GH workflow.   # #
 
 STAGING_AUTH_TOKEN=`python3 login.py $1 $2 $3`
-node stage_wpt.js $STAGING_AUTH_TOKEN $4
+node core_web_vitals.js $STAGING_AUTH_TOKEN $4
