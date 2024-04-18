9858c831aba3b87e74107523051c5dcf04851d7b
bertrand <bertrand@regentmarkets.com>
2024-04-18 14:01:38 +0200

    Create Random name generator

--- cypress/e2e/wip/KYC/idvPoiSwitching.cy.js
+++ cypress/e2e/wip/KYC/idvPoiSwitching.cy.js
@@ -2,6 +2,7 @@ import '@testing-library/cypress/add-commands'
 
 describe('QATEST-123731 - IDV (2 attempts) and Onfido (1 attempt) failed clients are redirected to manual upload', () => {
   beforeEach(() => {
+    cy.c_createRealAccount()
     cy.c_login()
     cy.c_navigateToPoiResponsive('Republic of QA')
   })
--- cypress/support/commands/kyc.js
+++ cypress/support/commands/kyc.js
@@ -1,3 +1,5 @@
+import { generateRandomName } from '../helper/loginUtility'
+
 Cypress.Commands.add('c_navigateToPoi', (country) => {
   cy.get('a[href="/account/personal-details"]').click()
   cy.findByRole('link', { name: 'Proof of identity' }).click()
@@ -9,8 +11,6 @@ Cypress.Commands.add('c_navigateToPoi', (country) => {
 
 Cypress.Commands.add('c_navigateToPoiResponsive', (country) => {
   cy.c_visitResponsive('/account/proof-of-identity', 'small')
-  cy.findByText('Pending action required').should('exist')
-  cy.c_closeNotificationHeader()
   cy.get('select[name="country_input"]').select(country)
   cy.contains('button', 'Next').click()
 })
@@ -19,7 +19,9 @@ Cypress.Commands.add('c_submitIdv', () => {
   cy.get('select[name="document_type"]').select('Passport')
   cy.findByLabelText('Enter your document number').type('12345678')
   cy.findByTestId('first_name').clear().type('Refuted')
-  cy.findByTestId('last_name').clear().type('Rejected')
+  cy.findByTestId('last_name')
+    .clear()
+    .type(`${generateRandomName()}` + ' ' + 'Rejected')
   cy.findByTestId('date_of_birth').type('2000-09-20')
   cy.get('.dc-checkbox__box').click()
   cy.findByRole('button', { name: 'Verify' }).click()
