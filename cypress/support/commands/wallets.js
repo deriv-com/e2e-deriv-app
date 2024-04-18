2e5caca54fa9cb373d1e3c54a44ad4f78783786f
lubega-deriv <lubega@deriv.com>
2024-04-18 10:39:04 +0800

    fix: added rate limit command to wallet switch logic

--- cypress/support/commands/wallets.js
+++ cypress/support/commands/wallets.js
@@ -3,4 +3,5 @@ Cypress.Commands.add('c_switchWalletsAccount', (account) => {
   cy.get('.wallets-list-card-dropdown__item-content')
     .contains(`${account} Wallet`)
     .click()
+  cy.c_rateLimit({ waitTimeAfterError: 15000, maxRetries: 5 })
 })
