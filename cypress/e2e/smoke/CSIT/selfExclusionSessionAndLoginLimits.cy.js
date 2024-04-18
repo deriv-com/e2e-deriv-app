d6eb078a0f28e91e19af37b7268e18c1a0097b4d
Alena-deriv <alena.sachylovich@regentmarkets.com>
2024-04-18 15:24:58 +0300

    feat: :sparkles: add test for selfexclusion

--- cypress/e2e/smoke/CSIT/selfExclusionSessionAndLoginLimits.cy.js
+++ cypress/e2e/smoke/CSIT/selfExclusionSessionAndLoginLimits.cy.js
@@ -1,14 +1,36 @@
 import '@testing-library/cypress/add-commands'
 
-describe('Get oAuthUrl', () => {
-  beforeEach(() => {
-    cy.c_visitResponsive('/')
-    cy.log('<E2EOAuthUrl - beforeEach>' + Cypress.env('oAuthUrl'))
-    cy.c_createRealAccount()
-    cy.c_login()
-  })
+function createClientAndLogin() {
+  cy.log('<E2EOAuthUrl - beforeEach>' + Cypress.env('oAuthUrl'))
+  cy.c_createRealAccount()
+  cy.c_login()
+}
 
-  it('should get the oauth url from API', () => {
-    cy.log('<E2EOAuthUrl - Test 1>' + Cypress.env('oAuthUrl'))
+function setSessionAndLoginLimitExclusion() {
+  cy.c_visitResponsive('/account/self-exclusion', 'large')
+  cy.get('input[name="timeout_until"]').click()
+  cy.get('[data-duration="13 Days"]').click()
+  cy.findByRole('button', { name: 'Next' }).click()
+  cy.findByRole('button', { name: 'Accept' }).click()
+  cy.findByRole('button', { name: 'Yes, log me out immediately' }).click()
+}
+
+function checkSelfExclusionIsSet() {
+  cy.c_login()
+  cy.c_visitResponsive('/appstore/traders-hub', 'large')
+  cy.wait(2000)
+  cy.findByTestId('dt_dropdown_display').click()
+  cy.wait(1000)
+  cy.get('#real').click()
+  cy.wait(2000)
+  cy.c_visitResponsive('/account/self-exclusion', 'large')
+  cy.get('input[name="timeout_until"]').should('not.be.empty')
+}
+
+describe('QATEST-116798 Self Exclusion Session and login limits on desktop', () => {
+  it('should login, set self exclusion and verify it applied', () => {
+    createClientAndLogin()
+    setSessionAndLoginLimitExclusion()
+    checkSelfExclusionIsSet()
   })
 })
