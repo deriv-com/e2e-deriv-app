16a3ee2c81a279d481850d99120ed5adb7c58a67
usman-deriv <usman@deriv.com>
2024-04-23 14:58:14 +0800

    init commit

--- cypress/e2e/wip/P2P/setNicknameForAdvertiser.cy.js
+++ cypress/e2e/wip/P2P/setNicknameForAdvertiser.cy.js
@@ -3,22 +3,29 @@ import { generateAccountNumberString } from '../../../support/p2p'
 
 let nickname = `NickWithAcc${generateAccountNumberString(5)}`
 
-describe("QATEST-2292 - Register a new client as an Advertiser in Deriv P2P - Nickname checks", () => {
-    beforeEach(() => {
-        cy.c_login()
-        cy.c_visitResponsive('/cashier/p2p', 'small')
-    })
+describe('QATEST-2292 - Register a new client as an Advertiser in Deriv P2P - Nickname checks', () => {
+  beforeEach(() => {
+    cy.c_login()
+    cy.c_visitResponsive('/cashier/p2p', 'small')
+  })
 
-    it('Should be able to set a nickname for P2P in responsive mode.', () => {
-        cy.c_closeSafetyInstructions()
-        cy.findByText('Deriv P2P').should('exist')
-        cy.c_closeNotificationHeader()
-        cy.findByText('My profile').click()
-        cy.findByRole('heading', { name: 'What’s your nickname?' }).should('be.visible')
-        cy.findByText('Others will see this on your profile, ads, and chats.').should('be.visible')
-        cy.findByText('Your nickname cannot be changed later.').should('be.visible')
-        cy.findByRole('textbox', { name: 'Your nickname' }).type(nickname).should('have.value', nickname).and('have.length.below', 25)
-        cy.findByRole('button', { name: 'Confirm' }).should('be.enabled').click()
-        cy.findByText(nickname).should('be.visible')
-    })
+  it('Should be able to set a nickname for P2P in responsive mode.', () => {
+    cy.c_closeSafetyInstructions()
+    cy.findByText('Deriv P2P').should('exist')
+    cy.c_closeNotificationHeader()
+    cy.findByText('My profile').click()
+    cy.findByRole('heading', { name: 'What’s your nickname?' }).should(
+      'be.visible'
+    )
+    cy.findByText(
+      'Others will see this on your profile, ads, and chats.'
+    ).should('be.visible')
+    cy.findByText('Your nickname cannot be changed later.').should('be.visible')
+    cy.findByRole('textbox', { name: 'Your nickname' })
+      .type(nickname)
+      .should('have.value', nickname)
+      .and('have.length.below', 25)
+    cy.findByRole('button', { name: 'Confirm' }).should('be.enabled').click()
+    cy.findByText(nickname).should('be.visible')
+  })
 })
