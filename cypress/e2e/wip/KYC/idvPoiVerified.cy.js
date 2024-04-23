6a9d37d3367c6a5bfc5573c523d576eb9e4b8865
bertrand <bertrand@regentmarkets.com>
2024-04-23 17:17:57 +0200

    Improve verify

--- cypress/e2e/wip/KYC/idvPoiVerified.cy.js
+++ cypress/e2e/wip/KYC/idvPoiVerified.cy.js
@@ -2,19 +2,19 @@ import '@testing-library/cypress/add-commands'
 
 describe('QATEST-22037 IDV verified by Smile Identity provider', () => {
   beforeEach(() => {
+    cy.c_createRealAccount()
     cy.c_login()
-    cy.c_navigateToPOIResponsive('Ghana')
+    cy.c_navigateToPoiResponsive('South Africa')
   })
 
   it('Should Return IDV Verified', () => {
-    cy.get('select[name="document_type"]').select('Passport')
-    cy.findByLabelText('Enter your document number').type('G2380523')
-    cy.findByTestId('first_name').clear().type('PRINCE')
-    cy.findByTestId('last_name').clear().type('NORBI')
-    cy.findByTestId('date_of_birth').type('1985-03-27')
+    cy.get('select[name="document_type"]').select('National ID Number')
+    cy.findByLabelText('Enter your document number').type('0000000000000')
+    cy.findByTestId('first_name').clear().type('Joe')
+    cy.findByTestId('last_name').clear().type('Leo')
+    cy.findByTestId('date_of_birth').type('2000-09-20')
     cy.get('.dc-checkbox__box').click()
     cy.findByRole('button', { name: 'Verify' }).click()
-    cy.reload()
     cy.contains('ID verification passed').should('be.visible')
     cy.contains('a', 'Continue trading').should('be.visible')
   })
