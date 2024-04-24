f9aa3634032b9d7922c598c1560023fe45b6688b
thivya-deriv <82857131+thivya-deriv@users.noreply.github.com>
2024-04-24 12:09:40 +0800

    Merge branch 'master' into thivya/rateLimit

--- cypress/e2e/wip/dbot/sanity-tests/FeedVerification.cy.js
+++ cypress/e2e/wip/dbot/sanity-tests/FeedVerification.cy.js
@@ -2,19 +2,22 @@ import charts from '../pageobjects/charts'
 import '@testing-library/cypress/add-commands'
 import TradersHub from '../pageobjects/traders_hub'
 import Common from '../pageobjects/common'
+import { createAccountVirtual } from '../../../../support/helper/accountCreationUtility'
 
 describe('QATEST-99340: Verify feed is loading on charts tab', () => {
   const tradersHub = new TradersHub()
   const common = new Common()
 
   beforeEach(() => {
-    cy.c_login({ user: 'p2pFloating', rateLimit: 'check' })
-    cy.c_visitResponsive('/bot', 'large', 'check')
+    cy.c_visitResponsive('/')
+    cy.c_createRealAccount()
+    cy.c_login()
+    cy.c_visitResponsive('/bot', 'large')
     cy.c_loadingCheck()
     common.skipTour()
   })
 
-  it('Verify feed for real account', () => {
+  it('Verify feed for real and demo account', () => {
     charts.openChartsTab()
     charts.selectSymbolOnCharts('Volatility 10 (1s) Index')
     charts.verifyTickChange(5000)
@@ -22,22 +25,8 @@ describe('QATEST-99340: Verify feed is loading on charts tab', () => {
     charts.selectSymbolOnCharts('Gold Basket')
     charts.verifyTickChange(5000)
 
-    charts.selectSymbolOnCharts('AUD/JPY')
-    charts.verifyTickChange(5000)
-
-    charts.selectSymbolOnCharts('Gold/USD')
-    charts.verifyTickChange(5000)
-  })
-
-  it('Verify feed for demo account', () => {
     common.switchToDemo()
     charts.openChartsTab()
-
-    charts.selectSymbolOnCharts('Volatility 10 (1s) Index')
-    charts.verifyTickChange(5000)
-
-    charts.selectSymbolOnCharts('Gold Basket')
-    charts.verifyTickChange(5000)
 
     charts.selectSymbolOnCharts('AUD/JPY')
     charts.verifyTickChange(5000)
