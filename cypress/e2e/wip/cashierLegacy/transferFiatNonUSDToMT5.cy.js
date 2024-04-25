a3008bfc4075014e81f00dc857cbbd9736e4f527
bilalmajid-deriv <bilal.majid@regentmarkets.com>
2024-04-25 15:46:27 +0800

    first commit

--- cypress/e2e/wip/cashierLegacy/transferFiatNonUSDToMT5.cy.js
+++ cypress/e2e/wip/cashierLegacy/transferFiatNonUSDToMT5.cy.js
@@ -1,23 +1,28 @@
 import '@testing-library/cypress/add-commands'
 import { derivApp } from '../../../support/locators'
 
-const toCurrency = {
-  name: 'Bitcoin',
-  code: 'BTC',
+const toAccount = {
+  type: 'Deriv MT5',
+  subType: 'Derived',
+  name: `${type} ${subType}`,
+  code: 'SVG',
   delta: 0.001, // needed for approximately equal to
-  accurateDelta: 0.0000001, // this for BTc to match exact exchangerate
+  accurateDelta: 0.0000001, // this for BTC to match exact exchangerate
 }
-const fromCurrency = {
-  name: 'US Dollar',
-  code: 'USD',
+const fromAccount = {
+  type: 'Currency',
+  subType: 'Fiat currencies',
+  name: 'Euro',
+  code: 'EUR',
   delta: 0.5, // needed for approximately equal to
+  accurateDelta: 0.1,
 }
 const amountToTransfer = 10.0
 
 const screenSizes = ['large', 'small']
 
 screenSizes.forEach((screenSize) => {
-  describe(`QATEST-20036 - Transfer: Enter USD amount when Transfer Fiat to Crypto in screen size: ${screenSize}`, () => {
+  describe(`QATEST-20036 - Transfer: Perform Transfer from Fiat non-USD to MT5 in screen size: ${screenSize}`, () => {
     beforeEach(() => {
       cy.clearAllSessionStorage()
       cy.c_login({ user: 'cashierLegacy', rateLimit: 'check' })
@@ -32,30 +37,30 @@ screenSizes.forEach((screenSize) => {
         cy.findByText('Options & Multipliers').should('be.visible')
       }
       cy.c_closeNotificationHeader()
-      cy.c_verifyActiveCurrencyAccount(fromCurrency, { closeModalAtEnd: false })
-      cy.c_checkCurrencyAccountExists(toCurrency, {
-        modalAlreadyOpened: true,
-        closeModalAtEnd: false,
-      })
+      cy.c_verifyActiveCurrencyAccount(fromAccount, { closeModalAtEnd: false })
+      // cy.c_checkCurrencyAccountExists(toAccount, {
+      //   modalAlreadyOpened: true,
+      //   closeModalAtEnd: false,
+      // })
       cy.then(() => {
         if (
-          sessionStorage.getItem(`c_is${toCurrency.code}AccountCreated`) ==
+          sessionStorage.getItem(`c_is${toAccount.code}AccountCreated`) ==
           'false'
         ) {
-          cy.c_createNewCurrencyAccount(toCurrency, { size: screenSize })
+          cy.c_createNewCurrencyAccount(toAccount, { size: screenSize })
         } else if (
-          sessionStorage.getItem(`c_is${toCurrency.code}AccountCreated`) ==
+          sessionStorage.getItem(`c_is${toAccount.code}AccountCreated`) ==
           'true'
         ) {
           cy.c_closeModal()
         }
       })
-      cy.c_getCurrencyBalance(fromCurrency, { closeModalAtEnd: false })
-      cy.c_getCurrencyBalance(toCurrency, {
+      cy.c_getCurrencyBalance(fromAccount, { closeModalAtEnd: false })
+      cy.c_getCurrencyBalance(toAccount, {
         modalAlreadyOpened: true,
         closeModalAtEnd: false,
       })
-      cy.c_selectCurrency(fromCurrency, { modalAlreadyOpened: true })
+      cy.c_selectCurrency(fromAccount, { modalAlreadyOpened: true })
       cy.c_getCurrentCurrencyBalance()
     })
     it(`should transfer amount from fiat to crypto account.`, () => {
@@ -66,10 +71,10 @@ screenSizes.forEach((screenSize) => {
       cy.findByRole('heading', {
         name: 'Transfer between your accounts in Deriv',
       }).should('exist')
-      cy.c_getCurrentExchangeRate(fromCurrency.code, toCurrency.code)
+      cy.c_getCurrentExchangeRate(fromAccount.code, toAccount.code)
       cy.c_TransferBetweenAccounts({
-        fromAccount: fromCurrency,
-        toAccount: toCurrency,
+        fromAccount: fromAccount,
+        toAccount: toAccount,
         withExtraVerifications: true,
         transferAmount: amountToTransfer,
         size: screenSize,
@@ -89,8 +94,8 @@ screenSizes.forEach((screenSize) => {
       cy.get('.currency-switcher-container').within(() => {
         cy.findByTestId('dt_balance_text_container').should('be.visible')
       })
-      cy.c_getCurrencyBalance(fromCurrency, { closeModalAtEnd: false })
-      cy.c_getCurrencyBalance(toCurrency, {
+      cy.c_getCurrencyBalance(fromAccount, { closeModalAtEnd: false })
+      cy.c_getCurrencyBalance(toAccount, {
         modalAlreadyOpened: true,
         closeModalAtEnd: false,
       })
@@ -104,12 +109,12 @@ screenSizes.forEach((screenSize) => {
         )
         const currentBalanceFromAccount = parseFloat(
           sessionStorage
-            .getItem(`c_balance${fromCurrency.code}`)
+            .getItem(`c_balance${fromAccount.code}`)
             .replace(/[^\d.]/g, '')
         )
         const currentBalanceToAccount = parseFloat(
           sessionStorage
-            .getItem(`c_balance${toCurrency.code}`)
+            .getItem(`c_balance${toAccount.code}`)
             .replace(/[^\d.]/g, '')
         )
         const estimatedFromAccountBalance = parseFloat(
@@ -122,15 +127,15 @@ screenSizes.forEach((screenSize) => {
         expect(
           currentCurrencyBalance,
           "Current selected Currency Account's balance"
-        ).to.be.closeTo(estimatedFromAccountBalance, fromCurrency.delta)
+        ).to.be.closeTo(estimatedFromAccountBalance, fromAccount.delta)
         expect(
           currentBalanceFromAccount,
           "From Currency Account's balance"
-        ).to.be.closeTo(estimatedFromAccountBalance, fromCurrency.delta)
+        ).to.be.closeTo(estimatedFromAccountBalance, fromAccount.delta)
         expect(
           currentBalanceToAccount,
           "To Currency Account's balance"
-        ).to.be.closeTo(estimatedToAccountBalance, toCurrency.delta)
+        ).to.be.closeTo(estimatedToAccountBalance, toAccount.delta)
       })
     })
   })
