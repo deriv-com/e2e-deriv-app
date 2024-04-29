9c4a04bed500cf5f4ef4a0e206684322a7e2197b
rajendra-deriv <140414984+rajendra-deriv@users.noreply.github.com>
2024-04-29 15:05:09 +0400

    Adding Testcase ID to checkBalance.cy.js file

--- cypress/e2e/api/checkBalance.cy.js
+++ cypress/e2e/api/checkBalance.cy.js
@@ -1,6 +1,6 @@
 import '@testing-library/cypress/add-commands'
 
-describe('Checking the Balance', () => {
+describe('QATEST - 145407 : Checking the Balance', () => {
   it('should show balance call', () => {
     cy.c_login()
     cy.log('<E2EOAuthUrl - Test >' + Cypress.env('oAuthUrl'))
