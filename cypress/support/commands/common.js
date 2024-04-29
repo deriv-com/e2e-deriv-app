1d5477cdc69369d4dd6678f31500163a125342b1
bertrand <bertrand@regentmarkets.com>
2024-04-29 16:02:20 +0200

    Improve api create account function

--- cypress/support/commands/common.js
+++ cypress/support/commands/common.js
@@ -452,25 +452,35 @@ Cypress.Commands.add('c_loadingCheck', () => {
   cy.findByTestId('dt_initial_loader').should('not.exist')
 })
 
-Cypress.Commands.add('c_createRealAccount', () => {
-  // Call Verify Email and then set the Verification code in env
-  try {
-    cy.task('wsConnect')
-    cy.task('verifyEmailTask').then((accountEmail) => {
-      cy.c_emailVerificationV2('account_opening_new.html', accountEmail)
-      cy.task('createRealAccountTask').then(() => {
-        // Updating Cypress environment variables with the new email
-        const currentCredentials = Cypress.env('credentials')
-        currentCredentials.test.masterUser.ID = accountEmail
-        Cypress.env('credentials', currentCredentials)
+/*
+  Usage cy.c_createRealAccount('co', 'EUR') or you may not pass in anything to go with default.
+  Currently works for CR countries as well as DIEL - non-EU account.
+*/
+Cypress.Commands.add(
+  'c_createRealAccount',
+  (country_code = 'id', currency = 'USD') => {
+    // Call Verify Email and then set the Verification code in env
+    try {
+      cy.task('wsConnect')
+      cy.task('verifyEmailTask').then((accountEmail) => {
+        cy.c_emailVerificationV2('account_opening_new.html', accountEmail)
+        cy.task('createVirtualAccountTask', {
+          country_code: country_code,
+          currency: currency,
+        }).then(() => {
+          // Updating Cypress environment variables with the new email
+          const currentCredentials = Cypress.env('credentials')
+          currentCredentials.test.masterUser.ID = accountEmail
+          Cypress.env('credentials', currentCredentials)
+        })
       })
-    })
-  } catch (e) {
-    console.error('An error occurred during the account creation process:', e)
-  } finally {
-    cy.task('wsDisconnect')
+    } catch (e) {
+      console.error('An error occurred during the account creation process:', e)
+    } finally {
+      cy.task('wsDisconnect')
+    }
   }
-})
+)
 
 Cypress.Commands.add('c_closeModal', () => {
   cy.log('Closing the modal')
