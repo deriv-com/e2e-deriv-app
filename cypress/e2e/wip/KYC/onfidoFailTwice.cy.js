6512c5d07102ed52768d20e12ce208965e28c498
deborah-deriv <111048322+deborah-deriv@users.noreply.github.com>
2024-04-30 11:33:59 +0200

    Onfido x 2 >> Manual upload (#217)
    * initial commit
    
    * updated my code
    
    * updated my code
    
    * updated my code
    
    * created c_onfidoSecondRun command
    
    * updated the message on date of birth mismatch
    
    * new changes
    
    * Removed passport photo
    
    * added comment line to show where second onfido run starts
    
    * added the changes to create an account on the go
    
    * added retry script
    
    * made some changes
    
    * added account creation on the go
    
    * added small change
    
    * added minor change
    
    * Removed vitals folder

---
+++ cypress/e2e/wip/KYC/onfidoFailTwice.cy.js
@@ -0,0 +1,57 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-22853 Onfido (2 attempts) failed clients are redirected to manual upload', () => {
+  beforeEach(() => {
+    cy.c_visitResponsive('/')
+    cy.c_createRealAccount('co')
+    cy.c_login()
+    cy.c_navigateToPoiResponsive('Colombia')
+  })
+
+  it('Onfido should fail twice and then manual upload screen should be displayed', () => {
+    cy.findByText('Choose document').should('be.visible')
+    cy.get('.dc-checkbox__box').click()
+    cy.findByText('Passport').click()
+    cy.findByText('Submit passport photo pages').should('be.visible')
+    cy.findByText('or upload photo – no scans or photocopies').click()
+    cy.findByText('Upload passport photo page').should('be.visible')
+    cy.get('input[type=file]').selectFile(
+      'cypress/fixtures/kyc/testDriversLicense.jpeg',
+      { force: true }
+    )
+    cy.findByText('Confirm').click()
+    cy.findByText('Continue').click()
+    cy.findByText('Take a selfie').should('be.visible')
+    cy.get('.onfido-sdk-ui-Camera-btn').click()
+    cy.findByText('Confirm').click()
+    cy.findByText('Account verification required').should('be.visible')
+
+    cy.reload()
+    cy.c_closeNotificationHeader()
+    cy.c_waitUntilElementIsFound({
+      cyLocator: () =>
+        cy.findByText('Your proof of identity submission failed because:'),
+      timeout: 1000,
+      maxRetries: 5,
+    })
+    cy.findByText('Your proof of identity submission failed because:')
+    cy.get('.dc-btn').click()
+
+    // Second onfido attempt
+    cy.c_onfidoSecondRun()
+    cy.findByText('Your documents were submitted successfully').should(
+      'be.visible'
+    )
+
+    cy.c_waitUntilElementIsFound({
+      cyLocator: () =>
+        cy.findByText('Please upload one of the following documents:'),
+      timeout: 1000,
+      maxRetries: 2,
+    })
+    cy.reload()
+    cy.findByText('Please upload one of the following documents:').should(
+      'be.visible'
+    )
+  })
+})
