f85b5480d5fe28baa474290bc1f48ab030bdb065
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-05-01 12:39:19 +0800

    demo reset balance

--- cypress/support/commands/wallets.js
+++ cypress/support/commands/wallets.js
@@ -6,6 +6,8 @@ Cypress.Commands.add('c_switchWalletsAccount', (account) => {
   cy.c_rateLimit({ waitTimeAfterError: 15000, maxRetries: 5 })
 })
 Cypress.Commands.add('c_switchWalletsAccountResponsive', (account) => {
+  let currentIndex = 0 // Initialize index counter
+
   const checkForWallet = () => {
     return new Cypress.Promise((resolve) => {
       const elementsWithText = Cypress.$(`:contains("${account}")`)
@@ -16,18 +18,23 @@ Cypress.Commands.add('c_switchWalletsAccountResponsive', (account) => {
         cy.log('no scroll')
         resolve(true)
       } else {
-        cy.log(' scroll')
+        cy.log('scroll')
         resolve(false)
       }
     })
   }
+
   const clickNext = () => {
     return cy
       .get('div.wallets-progress-bar')
       .find('div.wallets-progress-bar-inactive')
-      .first()
+      .eq(currentIndex) // Click on element based on currentIndex
       .click()
+      .then(() => {
+        currentIndex++ // Increment currentIndex
+      })
   }
+
   const keepClickingNext = () => {
     clickNext().then(() => {
       checkForWallet().then((isTextVisible) => {
@@ -37,6 +44,14 @@ Cypress.Commands.add('c_switchWalletsAccountResponsive', (account) => {
       })
     })
   }
+
   keepClickingNext()
   cy.c_rateLimit({ waitTimeAfterError: 15000, maxRetries: 5 })
+})
+
+Cypress.Commands.add('c_switchWalletsAccountDemo', () => {
+  cy.get('div.wallets-progress-bar')
+    .find('div.wallets-progress-bar-inactive')
+    .eq(3)
+    .click()
 })
