953d7ed115e5673790c195272cd977231668a7ad
Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
2024-05-02 18:04:56 +0800

    Added responsive tests for QATEST-5724 and QATEST-5699

--- cypress/e2e/wip/Tradershub/euDisclaimer.cy.js
+++ cypress/e2e/wip/Tradershub/euDisclaimer.cy.js
@@ -1,28 +1,27 @@
 import '@testing-library/cypress/add-commands'
-const size = ['small', 'desktop']
+const size = ['desktop', 'small']
 
-describe(`QATEST-42150 Validate the EU statutory disclaimer in footer for EU users on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+describe('QATEST-42150 Validate the EU statutory disclaimer in footer for EU users', () => {
   beforeEach(() => {
     cy.c_login({ user: 'eu' })
   })
   size.forEach((size) => {
-    it(`Should validate the EU statutory disclaimer in footer for EU users`, () => {
+    it(`Should validate the EU statutory disclaimer in footer for EU users on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
       cy.c_visitResponsive('/appstore/traders-hub', size)
       cy.c_validateEUDisclaimer()
     })
   })
 })
 
-describe(`QATEST-37723 Validate the EU statutory disclaimer in footer of EU account for DIEL users ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+describe('QATEST-37723 Validate the EU statutory disclaimer in footer of EU account for DIEL users', () => {
   beforeEach(() => {
     cy.c_login({ user: 'diel' })
   })
 
-  size.forEach(() => {
-    it('Should validate the EU statutory disclaimer in footer of EU account for DIEL users', () => {
+  size.forEach((size) => {
+    it(`Should validate the EU statutory disclaimer in footer of EU account for DIEL users on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
       cy.c_visitResponsive('/appstore/traders-hub', size)
-      cy.findByText('EU', { exact: true }).click()
-      cy.c_closeNotificationHeader()
+      cy.findByText('EU', { exact: true }).click({ force: true })
       cy.c_validateEUDisclaimer()
     })
   })
