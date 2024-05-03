148cf314a2416a057fff51f0e33c7a860b5534b3
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-05-03 11:12:53 +0800

    update

--- cypress/e2e/wip/Wallets/fiatDeposit.cy.js
+++ cypress/e2e/wip/Wallets/fiatDeposit.cy.js
@@ -22,7 +22,10 @@ describe('QATEST-98805 - Fiat deposit iframe access', () => {
     cy.c_visitResponsive('/wallets', 'small')
     cy.contains('Wallet', { timeout: 10000 }).should('exist')
     cy.findAllByText('Financial', { timeout: 10000 }).should('exist')
-    cy.findByText('Deposit').parent().scrollIntoView().click()
+    cy.findByText('Deposit')
+      .prev('button')
+      .scrollIntoView()
+      .click({ force: true })
     cy.get('iframe[class=wallets-deposit-fiat__iframe]').should('be.visible')
     cy.enter('iframe[class=wallets-deposit-fiat__iframe]').then((getBody) => {
       getBody().find('#pmfilter').should('be.visible')
