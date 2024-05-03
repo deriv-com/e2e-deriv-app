b85575358b0743210f0b54ad10167743260e5c2d
rajendra-deriv <140414984+rajendra-deriv@users.noreply.github.com>
2024-05-03 18:37:30 +0400

    Adding Cypress.env('configAppId', newAppId)

--- cypress/e2e/api/createApplicationRegister.cy.js
+++ cypress/e2e/api/createApplicationRegister.cy.js
@@ -2,11 +2,15 @@ import '@testing-library/cypress/add-commands'
 
 describe('Register a New Application / App ID', () => {
   it('Creation of New App ID should be successful. ', () => {
-    cy.c_login()
-    cy.c_wsConnect()
+    if (Cypress.env('E2E_NEW_OAUTH_APPID').to.be.null()) {
+      cy.c_wsConnect()
+      cy.c_login()
 
-    const newAppId = cy.c_registerNewApplicationID()
-    console.log('The New Application Id is : ', newAppId)
+      const newAppId = cy.c_registerNewApplicationID()
+      console.log('The New Application Id is : ', newAppId)
+      Cypress.env('configAppId', newAppId)
+    }
+    cy.c_login()
 
     cy.c_wsDisconnect()
   })
