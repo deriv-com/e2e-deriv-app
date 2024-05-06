f8f5f44a0ae5dcc950d3356bc7493dd619f7cde0
rajendra-deriv <140414984+rajendra-deriv@users.noreply.github.com>
2024-05-06 09:16:51 +0400

    Adding a comment for cy.c_login() for why login method is needed

--- cypress/e2e/api/createApplicationRegister.cy.js
+++ cypress/e2e/api/createApplicationRegister.cy.js
@@ -4,7 +4,7 @@ describe('Register a New Application / App ID', () => {
   it('Creation of New App ID should be successful. ', () => {
     if (Cypress.env('E2E_NEW_OAUTH_APPID').to.be.null()) {
       cy.c_wsConnect()
-      cy.c_login()
+      cy.c_login() // Here Login is required as we need Auth ID for running RegisterApplication API. We are updating the 'configAppId' with 'newAppId'
 
       const newAppId = cy.c_registerNewApplicationID()
       console.log('The New Application Id is : ', newAppId)
