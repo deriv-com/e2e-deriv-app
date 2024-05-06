88c5eae573f4f99714c1c7be9b9975d7dbed96c5
thivya-deriv <82857131+thivya-deriv@users.noreply.github.com>
2024-05-06 12:38:39 +0800

    Add check for live demo

--- cypress/e2e/wip/dbot/regression-tests/LogInFromDerivCom.cy.js
+++ cypress/e2e/wip/dbot/regression-tests/LogInFromDerivCom.cy.js
@@ -2,6 +2,7 @@ import '@testing-library/cypress/add-commands'
 
 describe('QATEST-4126: Log in Deriv Bot platform page from deriv.com', () => {
   beforeEach(() => {
+    //set the server and app id based on baseUrl
     if (Cypress.config().baseUrl != Cypress.env('prodURL')) {
       cy.on('window:before:load', (win) => {
         win.localStorage.setItem(
@@ -14,12 +15,11 @@ describe('QATEST-4126: Log in Deriv Bot platform page from deriv.com', () => {
     cy.visit('https://deriv.com')
     cy.findByRole('link', { name: 'Learn more About Deriv Bot' }).click()
     cy.findByRole('img', { name: 'Deriv Bot' }).should('be.visible')
-    setTimeout(() => {
-      cy.findByRole('button', { name: 'Log in' }).click({ force: true })
-    }, 5000)
   })
 
   it('Login from deriv.com and redirect to dbot on app.deriv.com', () => {
+    cy.wait(5000)
+    cy.findByRole('button', { name: 'Log in' }).click({ force: true })
     if (Cypress.config().baseUrl === Cypress.env('prodURL')) {
       cy.get('input[id="txtEmail"]')
         .click()
@@ -36,6 +36,11 @@ describe('QATEST-4126: Log in Deriv Bot platform page from deriv.com', () => {
         .type(Cypress.env('credentials').test.dBot.PSWD)
     }
     cy.findByRole('button', { name: 'Log in' }).click({ force: true })
-    cy.wait(12000)
+    cy.wait(7000)
+  })
+
+  it('Go to dbot by choosing go to live demo', () => {
+    cy.findAllByText('Go to live demo').click()
+    cy.c_skipTour()
   })
 })
