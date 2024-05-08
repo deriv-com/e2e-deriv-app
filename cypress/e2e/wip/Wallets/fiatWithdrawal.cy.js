47fab37dbabd6b0354397121eb9b8e80c1113534
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-05-08 11:46:44 +0800

    update

--- cypress/e2e/wip/Wallets/fiatWithdrawal.cy.js
+++ cypress/e2e/wip/Wallets/fiatWithdrawal.cy.js
@@ -59,6 +59,7 @@ describe('QATEST-98812 - Fiat withdrawal access iframe from email verification l
   it('should be able to access doughflow iframe', () => {
     cy.log('Access Fiat Withdrawal Iframe Through Email Link')
     cy.c_visitResponsive('/wallets', 'large')
+    cy.c_skipPasskeysV2()
     cy.contains('Wallet', { timeout: 10000 }).should('exist')
     cy.c_rateLimit({ waitTimeAfterError: 15000, maxRetries: 5 })
     performFiatWithdraw()
