34af9df5a845a80c934313ae18c7f940525f1517
mohammed.luqmaan-deriv <mohammed.luqmaan@deriv.com>
2024-05-10 10:44:55 +0800

    [ci skip]

--- cypress/e2e/wip/Wallets/switchWallets.cy.js
+++ cypress/e2e/wip/Wallets/switchWallets.cy.js
@@ -1,55 +1,37 @@
 import '@testing-library/cypress/add-commands'
 function switchWallets() {
-  let walletsTexts = [];
-  cy.get('.wallets-carousel-content__container').first().find('.wallets-text wallets-text__size--md wallets-text__weight--normal wallets-text__align--left wallets-text__color--general wallets-text__line-height--undefined wallets-text__font-style--normal').each(($textElement) => {
-    const text = $textElement.text();
-    walletsTexts.push(text);
-  });
-  cy.log(`Wallets available are: ${walletsTexts}`)
-  cy.get('.wallets-carousel-content__progress-bar')
-    .find('button')
-    .then((buttons) => {
-      const buttoncount = buttons.filter(
-        (index, button) => Cypress.$(button).text().trim() === 'progress'
-      ).length // To get the exact match of the text
-      cy.log(`Number of buttons": ${buttoncount}`)
-      if (buttoncount > 0) {
-        cy.log('Button with text "progress" found')
-        for (let i = buttoncount; i > 0; i--) {
-          cy.get('wallets-progress-bar').click()
-          const inactiveProgressBars = cy.get('.wallets-progress-bar-inactive wallets-progress-bar-transition')
-            .then(() => {
-                inactiveProgressBars.each(($progressBar) => {
-                    cy.wrap($progressBar).click();
-                });
-              cy.findByText(`${walletname}`).should('exist')
-              cy.findByTestId('dt-wallets-add-more').scrollIntoView()
-              cy.get('[class*="wallets-add-more__content"]')
-                .contains(walletname)
-                .parent()
-                .parent()
-                .find('button')
-                .then((button) => {
-                  expect(button).to.contain('Added')
-                })
-            })
-        }
-      } else {
-        cy.log('All wallets successfully switched and viewed')
-      }
-    })
-}
+  cy.get('.wallets-carousel-content')
+    .find('.wallets-carousel-content__progress-bar')
+    .find('.wallets-progress-bar')
+    .find('.wallets-progress-bar-inactive.wallets-progress-bar-transition')
+    .each(($element, index, $list) => {
+        console.log(`Number of elements found: ${$list.length}`);
+        cy.get('.wallets-carousel-content__container')
+        .find('[data-testid="dt_wallet_gradient_background"]')
+        .find('.wallets-card__carousel-content-details')
+        .find('.wallets-card__details-bottom').should('contain', 'Wallet')
 
+        // View deposit, withdraw and transfer.
+
+        const actions = ['deposit', 'withdraw', 'transfer'];
+        actions.forEach((action) => {
+        cy.get('.wallets-mobile-actions__container')
+        .find('.wallets-mobile-actions')
+        .find(`button[aria-label="${action}"]`).click()
+        .then(() => {
+        cy.get('.wallets-cashier-header')
+        .find('.wallets-cashier-header__close-icon').click()
+        })
+        })
+      })
+    }
+       
 describe('QATEST-139905 - Mobile wallet card redirection', () => {
   beforeEach(() => {
     cy.c_login({ app: 'wallets' })
   })
 
-  it('should be able to switch to all of the available wallets in Responsive', () => {
-    cy.c_visitResponsive('/wallets', 'small')
-    switchWallets()
-  })
-  it('should be able to open correct deposit, withdraw and transfer page for all wallets in Responsive', () => {
+  it('should be able to switch and open correct deposit, withdraw and transfer in all the available wallets in Responsive', () => {
     cy.c_visitResponsive('/wallets', 'small')
     switchWallets()
   })
