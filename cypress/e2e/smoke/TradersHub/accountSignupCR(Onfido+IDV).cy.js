6e690b66bff9c19224a8127bc70f92361903c1a4
Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
2024-05-10 18:04:34 +0800

    Separate stable tests

---
+++ cypress/e2e/smoke/TradersHub/accountSignupCR(Onfido+IDV).cy.js
@@ -0,0 +1,74 @@
+import '@testing-library/cypress/add-commands'
+import { generateEpoch } from '../../../support/helper/utility'
+
+describe('QATEST-24427,5533,5827 - Cypress test for ROW account sign up', () => {
+  const size = ['small', 'desktop']
+  let countryIDV = Cypress.env('countries').KE
+  let nationalIDNumIDV = Cypress.env('nationalIDNum').KE
+  let taxIDNumIDV = Cypress.env('taxIDNum').KE
+  let countryOnfido = Cypress.env('countries').CO
+  let nationalIDNumOnfido = Cypress.env('nationalIDNum').CO
+  let taxIDNumOnfido = Cypress.env('taxIDNum').CO
+  let currency = Cypress.env('accountCurrency').USD
+
+  size.forEach((size) => {
+    it(`New account sign up ROW - Onfido supported country on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size == 'small' ? true : false
+      const signUpEmail = `sanity${generateEpoch()}onfido@deriv.com`
+      cy.c_setEndpoint(signUpEmail, size)
+      cy.c_demoAccountSignup(countryOnfido, signUpEmail, size)
+      cy.c_switchToReal()
+      cy.findByTestId('dt_trading-app-card_real_deriv-account')
+        .findByRole('button', { name: 'Get' })
+        .click()
+      cy.c_generateRandomName().then((firstName) => {
+        cy.c_personalDetails(
+          firstName,
+          'Onfido',
+          countryOnfido,
+          nationalIDNumOnfido,
+          taxIDNumOnfido,
+          currency,
+          { isMobile: isMobile }
+        )
+      })
+      cy.c_addressDetails()
+      cy.c_completeFatcaDeclarationAgreement()
+      cy.c_addAccount()
+      cy.c_checkTradersHubHomePage(isMobile)
+      cy.c_closeNotificationHeader()
+      cy.c_manageAccountsetting(countryOnfido, {
+        isMobile: isMobile,
+      })
+    })
+    it(`New account sign up ROW - IDV supported country on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size == 'small' ? true : false
+      const signUpEmail = `sanity${generateEpoch()}idv@deriv.com`
+      cy.c_setEndpoint(signUpEmail, size)
+      cy.c_demoAccountSignup(countryIDV, signUpEmail, size)
+      cy.c_switchToReal()
+      cy.findByTestId('dt_trading-app-card_real_deriv-account')
+        .findByRole('button', { name: 'Get' })
+        .click()
+      cy.c_generateRandomName().then((firstName) => {
+        cy.c_personalDetails(
+          firstName,
+          'IDV',
+          countryIDV,
+          nationalIDNumIDV,
+          taxIDNumIDV,
+          currency,
+          { isMobile: isMobile }
+        )
+      })
+      cy.c_addressDetails()
+      cy.c_completeFatcaDeclarationAgreement()
+      cy.c_addAccount()
+      cy.c_checkTradersHubHomePage(isMobile)
+      cy.c_closeNotificationHeader()
+      cy.c_manageAccountsetting(countryIDV, {
+        isMobile: isMobile,
+      })
+    })
+  })
+})
---
+++ cypress/e2e/smoke/TradersHub/addSiblingsAccounts.cy.js
@@ -0,0 +1,60 @@
+import '@testing-library/cypress/add-commands'
+import { generateEpoch } from '../../../support/helper/utility'
+
+function createSiblingAcct(acctCount) {
+  cy.findByTestId('dt_currency-switcher__arrow').click()
+  cy.findByRole('button', { name: 'Add or manage account' }).click()
+  cy.get('.currency-list__item').eq(acctCount).click()
+  cy.findByRole('button', { name: 'Add account' }).click()
+  cy.findByRole('heading', { name: 'Success!' }).should('be.visible')
+  cy.findByRole('button', { name: 'Deposit now' }).should('be.enabled')
+  cy.findByRole('button', { name: 'Maybe later' }).click()
+}
+function getCurrencyList() {
+  return cy
+    .get('div.currency-list__items')
+    .find('label')
+    .then(($labels) => {
+      const labelCount = $labels.length
+      return labelCount
+    })
+}
+describe('QATEST-5797, QATEST-5820 - Add siblings accounts', () => {
+  const signUpEmail = `sanity${generateEpoch()}sibacct@deriv.com`
+  let country = Cypress.env('countries').CO
+  let nationalIDNum = Cypress.env('nationalIDNum').CO
+  let taxIDNum = Cypress.env('taxIDNum').CO
+  let currency = Cypress.env('accountCurrency').USD
+  beforeEach(() => {
+    cy.c_setEndpoint(signUpEmail)
+  })
+
+  it('Create siblings accounts from USD account ', () => {
+    cy.c_demoAccountSignup(country, signUpEmail)
+    cy.c_switchToReal()
+    cy.findByRole('button', { name: 'Get a Deriv account' }).click()
+    cy.c_generateRandomName().then((firstName) => {
+      cy.c_personalDetails(
+        firstName,
+        'Onfido',
+        country,
+        nationalIDNum,
+        taxIDNum,
+        currency
+      )
+    })
+    cy.c_addressDetails()
+    cy.c_completeFatcaDeclarationAgreement()
+    cy.c_addAccount()
+    cy.c_checkTradersHubHomePage()
+    cy.c_closeNotificationHeader()
+    cy.findByTestId('dt_currency-switcher__arrow').click()
+    cy.findByRole('button', { name: 'Add or manage account' }).click()
+    getCurrencyList().then((labelCount) => {
+      cy.findByTestId('dt_modal_close_icon').click()
+      for (let acctCount = 0; acctCount < labelCount; acctCount++) {
+        createSiblingAcct(acctCount)
+      }
+    })
+  })
+})
