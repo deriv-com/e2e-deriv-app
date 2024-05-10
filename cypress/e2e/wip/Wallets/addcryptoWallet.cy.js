4c5fe0fad8c67df314b2ddbebc0f6c41f066bdeb
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-05-10 10:02:10 +0800

    update

--- cypress/e2e/wip/Wallets/addcryptoWallet.cy.js
+++ cypress/e2e/wip/Wallets/addcryptoWallet.cy.js
@@ -39,7 +39,7 @@ function addcryptowallet() {
     })
 }
 
-describe('QATEST-98773 - Add crypto wallet account', () => {
+describe('QATEST-98773 - Add crypto wallet account and QATEST-124514 wallet account switcher', () => {
   beforeEach(() => {
     cy.c_login({ app: 'wallets' })
   })
