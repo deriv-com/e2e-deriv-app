ed11e080e4764f4eb8e5d9fa148829df20c83319
usman-deriv <usman@deriv.com>
2024-05-16 15:34:02 +0800

    final draft

--- cypress/e2e/smoke/P2P/sortFunctionalityExchangeRate.cy.js
+++ cypress/e2e/smoke/P2P/sortFunctionalityExchangeRate.cy.js
@@ -5,30 +5,34 @@ let ratesArrayAfterExchangeRateSort = []
 
 describe('QATEST-2718 - Sort Functionality (by Exchange Rate)', () => {
   beforeEach(() => {
+    cy.clearAllLocalStorage()
+    cy.clearAllSessionStorage()
     cy.c_login({ user: 'p2pSortFunctionality' })
     cy.c_visitResponsive('/appstore/traders-hub', 'small')
   })
 
   it('Should be able to sort Buy and Sell ads by Exchange Rate.', () => {
     cy.c_navigateToDerivP2P()
-    cy.c_closeSafetyInstructions()
+    cy.c_skipPasskey()
     cy.findByText('Deriv P2P').should('exist')
     cy.c_closeNotificationHeader()
-    cy.c_getExchngeRatesFromScreen('Buy').then((returnedRatesArray) => {
+    cy.c_getExchangeRatesFromScreen('Buy').then((returnedRatesArray) => {
       ratesArray = returnedRatesArray
       cy.c_sortAdBy('Exchange rate')
-      cy.c_getExchngeRatesFromScreen('Buy').then((sortedReturnedRatesArray) => {
-        ratesArrayAfterExchangeRateSort = sortedReturnedRatesArray
-        cy.wrap(ratesArray).should(
-          'deep.equal',
-          ratesArrayAfterExchangeRateSort
-        )
-      })
+      cy.c_getExchangeRatesFromScreen('Buy').then(
+        (sortedReturnedRatesArray) => {
+          ratesArrayAfterExchangeRateSort = sortedReturnedRatesArray
+          cy.wrap(ratesArray).should(
+            'deep.equal',
+            ratesArrayAfterExchangeRateSort
+          )
+        }
+      )
     })
-    cy.c_getExchngeRatesFromScreen('Sell').then((returnedRatesArray) => {
+    cy.c_getExchangeRatesFromScreen('Sell').then((returnedRatesArray) => {
       ratesArray = returnedRatesArray
       cy.c_sortAdBy('Exchange rate')
-      cy.c_getExchngeRatesFromScreen('Sell').then(
+      cy.c_getExchangeRatesFromScreen('Sell').then(
         (sortedReturnedRatesArray) => {
           ratesArrayAfterExchangeRateSort = sortedReturnedRatesArray
           cy.wrap(ratesArray).should(
