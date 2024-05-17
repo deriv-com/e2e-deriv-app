ca8af00fdbdd8a9f9fa3b650ee5f9ec3cf604875
Shah Abuzar Alam <132635598+abuzar-deriv@users.noreply.github.com>
2024-05-17 16:38:14 +0800

    Fix cron failures

--- cypress/e2e/full/TradersHub/linksTradersHub.cy.js
+++ cypress/e2e/full/TradersHub/linksTradersHub.cy.js
@@ -1,76 +1,45 @@
 import '@testing-library/cypress/add-commands'
-
-const linkValidations = [
-  {
-    linkName: 'options',
-    expectedUrl: '/trade-types/options/digital-options/up-and-down/',
-    contentCheck: 'Digital options available on Deriv',
-  },
-  {
-    linkName: 'multipliers',
-    expectedUrl: '/trade-types/multiplier/',
-    contentCheck: 'Multipliers',
-  },
-  {
-    linkName: 'Learn more',
-    expectedUrl: '/trade-types/cfds/',
-    contentCheck: 'CFD trading',
-  },
-]
-
-const validateLink = (linkName, expectedUrl, contentCheck) => {
-  cy.findByRole('link', { name: linkName }).then(($link) => {
-    const url = $link.prop('href')
-    cy.visit(url, {
-      onBeforeLoad: (win) => {
-        cy.stub(win, 'open').as('windowOpen')
-      },
-    })
-    cy.url().should('contain', expectedUrl)
-    cy.findByRole('heading', { name: `${contentCheck}` })
-  })
-  cy.go('back')
-}
-
-function checkHyperLinks(deviceType) {
-  linkValidations.forEach(({ linkName, expectedUrl, contentCheck }) => {
-    if (!(deviceType === 'mobile' && linkName === 'Learn more')) {
-      validateLink(linkName, expectedUrl, contentCheck)
-    } else {
-      cy.findByRole('button', { name: 'CFDs' }).click()
-      validateLink(linkName, expectedUrl, contentCheck)
-    }
-  })
-
-  if (deviceType === 'mobile') {
-    cy.findByRole('button', { name: 'CFDs' }).click()
-  }
-
-  cy.findByText('Compare accounts').click()
-  cy.url().should('contain', 'appstore/cfd-compare-acccounts')
-  cy.findByText('Compare CFDs accounts').should('be.visible')
-  cy.go('back')
-}
+const size = ['small', 'desktop']
 
 describe("QATEST-5930 - Validate the hyperlinks on Trader's hub", () => {
   beforeEach(() => {
     cy.c_createRealAccount()
     cy.c_login()
   })
-
-  it('Should navigate to all links in traders hub home page and validate its redirection in mobile', () => {
-    cy.c_visitResponsive('/appstore/traders-hub', 'small')
-    cy.c_skipPasskeysV2()
-    //Wait for page to load
-    cy.findByTestId('dt_trading-app-card_real_deriv-trader')
-      .findByText('Deriv Trader')
-      .should('be.visible')
-    cy.c_closeNotificationHeader()
-    checkHyperLinks('mobile')
-  })
-
-  it('Should navigate to all links in traders hub home page and validate its redirection in desktop', () => {
-    cy.c_visitResponsive('/appstore/traders-hub', 'large')
-    checkHyperLinks('desktop')
+  size.forEach((size) => {
+    it(`Should navigate to all links in traders hub home page and validate its redirection in ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size == 'small' ? true : false
+      cy.c_visitResponsive('/appstore/traders-hub', size)
+      //Wait for page to load
+      cy.findByTestId('dt_trading-app-card_real_deriv-trader')
+        .findByText('Deriv Trader')
+        .should('be.visible')
+      if (isMobile) cy.c_skipPasskeysV2()
+      cy.c_closeNotificationHeader()
+      cy.findAllByRole('link', { name: 'Learn more' })
+        .first()
+        .c_clickToOpenInSamePage()
+      cy.findByRole('heading', { name: 'Digital options available on Deriv' })
+      cy.url().should(
+        'contain',
+        '/trade-types/options/digital-options/up-and-down/'
+      )
+      cy.go('back')
+      if (isMobile) {
+        cy.findByRole('button', { name: 'CFDs' }).click()
+      }
+      cy.findAllByRole('link', { name: 'Learn more' })
+        .last()
+        .c_clickToOpenInSamePage()
+      cy.findByRole('heading', { name: 'CFD trading' })
+      cy.url().should('contain', '/trade-types/cfds/')
+      cy.go('back')
+      if (isMobile) {
+        cy.findByRole('button', { name: 'CFDs' }).click()
+      }
+      cy.findByText('Compare accounts').click()
+      cy.url().should('contain', 'appstore/cfd-compare-acccounts')
+      cy.findByText('Compare CFDs accounts').should('be.visible')
+    })
   })
 })
