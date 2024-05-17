8ee5eeaa1f1385272f9ee1404b32e3db60509106
usman-deriv <usman@deriv.com>
2024-05-17 13:18:29 +0800

    moved const outside describe

--- cypress/e2e/smoke/P2P/sortFunctionalityExchangeRate.cy.js
+++ cypress/e2e/smoke/P2P/sortFunctionalityExchangeRate.cy.js
@@ -1,5 +1,22 @@
 import '@testing-library/cypress/add-commands'
 
+const verifySortFunctionality = (adType) => {
+  cy.c_getExchangeRatesFromScreen(adType, { sortArray: true }).then(
+    (initialRatesArray) => {
+      cy.c_sortAdBy('Exchange rate')
+      cy.c_getExchangeRatesFromScreen(adType, { sortArray: false }).then(
+        (sortedRatesArray) => {
+          cy.wrap(initialRatesArray).should(
+            'deep.equal',
+            sortedRatesArray,
+            `${adType} ads should be sorted by Exchange Rate`
+          )
+        }
+      )
+    }
+  )
+}
+
 describe('QATEST-2718 - Verify sorting of Buy and Sell ads by Exchange Rate', () => {
   beforeEach(() => {
     cy.clearAllLocalStorage()
@@ -7,23 +24,6 @@ describe('QATEST-2718 - Verify sorting of Buy and Sell ads by Exchange Rate', ()
     cy.c_login({ user: 'p2pSortFunctionality' })
     cy.c_visitResponsive('/appstore/traders-hub', 'small')
   })
-
-  const verifySortFunctionality = (adType) => {
-    cy.c_getExchangeRatesFromScreen(adType, { sortArray: true }).then(
-      (initialRatesArray) => {
-        cy.c_sortAdBy('Exchange rate')
-        cy.c_getExchangeRatesFromScreen(adType, { sortArray: false }).then(
-          (sortedRatesArray) => {
-            cy.wrap(initialRatesArray).should(
-              'deep.equal',
-              sortedRatesArray,
-              `${adType} ads should be sorted by Exchange Rate`
-            )
-          }
-        )
-      }
-    )
-  }
 
   it('Should be able to sort Buy and Sell ads by Exchange Rate.', () => {
     cy.c_navigateToDerivP2P()
