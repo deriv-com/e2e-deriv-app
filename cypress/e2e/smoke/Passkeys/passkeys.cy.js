74b703ef03fc377f6089811f5ba4899af460af37
rajendra-deriv <140414984+rajendra-deriv@users.noreply.github.com>
2024-05-24 13:45:16 +0400

    Removing cy.visit() method and using c_visitResponsive directly

--- cypress/e2e/smoke/Passkeys/passkeys.cy.js
+++ cypress/e2e/smoke/Passkeys/passkeys.cy.js
@@ -6,9 +6,9 @@ describe('QATEST - 142511, QATEST - 125330 - Passkeys Login Page Button Check', 
   size.forEach((size) => {
     it('Should contain Passkeys Button in Mobile Responsive and Should not contain on Desktop Version', () => {
       const isMobile = size == 'small' ? true : false
-      cy.c_visitResponsive('/', size)
-      cy.visit(
-        Cypress.env('passkeyUrl') + '?app_id=' + Cypress.env('passkeyAppId')
+      cy.c_visitResponsive(
+        Cypress.env('passkeyUrl') + '?app_id=' + Cypress.env('passkeyAppId'),
+        size
       )
       if (isMobile) {
         cy.findByText('Passkey').should('be.visible')
