e8721bf45fd1c4716c066019c9ec7662d1e4fa97
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2024-05-24 10:07:05 +0100

    appReg API fixes5

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -9,7 +9,7 @@ Cypress.jsQR = jsQR
 before(() => {
   // Code to run once before all tests
   cy.log('setupComplete = ' + Cypress.env('setupComplete'))
-  if (!Cypress.env('setupComplete')) {
+  if (Cypress.env('setupComplete') == 'false') {
     cy.log('runFromPR = ' + Cypress.env('runFromPR'))
     if (Cypress.env('runFromPR') == 'true') {
       cy.c_createApplicationId()
