9b7149b7cf0e9e64dfb735367dafdd1e060dd46f
mohammed.luqmaan-deriv <mohammed.luqmaan@deriv.com>
2024-05-24 16:54:41 +0800

    [ci skip]

--- cypress/e2e/wip/Wallets/switchWallets.cy.js
+++ cypress/e2e/wip/Wallets/switchWallets.cy.js
@@ -1,31 +1,49 @@
 import '@testing-library/cypress/add-commands'
 function switchWallets() {
-  cy.get('.wallets-carousel-content')
-    .find('.wallets-carousel-content__progress-bar')
-    .find('.wallets-progress-bar')
-    .find('.wallets-progress-bar-inactive.wallets-progress-bar-transition')
-    .each(($element, index, $list) => {
-        console.log(`Number of elements found: ${$list.length}`);
-        cy.get('.wallets-carousel-content__container')
-        .find('[data-testid="dt_wallet_gradient_background"]')
-        .find('.wallets-card__carousel-content-details')
-        .find('.wallets-card__details-bottom').should('contain', 'Wallet')
-
-        // View deposit, withdraw and transfer.
-
-        const actions = ['deposit', 'withdraw', 'transfer'];
-        actions.forEach((action) => {
-        cy.get('.wallets-mobile-actions__container')
-        .find('.wallets-mobile-actions')
-        .find(`button[aria-label="${action}"]`).click()
-        .then(() => {
-        cy.get('.wallets-cashier-header')
-        .find('.wallets-cashier-header__close-icon').click()
-        })
-        })
-      })
+  const performActionsOnElement = (currentIndex, totalElements) => {
+    if (currentIndex >= totalElements) {
+      return;
     }
-       
+      cy.get('.wallets-progress-bar')
+      .find('.wallets-progress-bar-active.wallets-progress-bar-transition, .wallets-progress-bar-inactive.wallets-progress-bar-transition')
+      .eq(currentIndex)
+      .click()
+      .then(() => {
+        cy.get('.wallets-card__carousel-content-details')
+        .eq(currentIndex)
+        .find('.wallets-card__details-bottom').invoke('text')
+        
+        const actions = ['deposit', 'withdraw', 'transfer'];
+        let actionChain = Cypress.Promise.resolve();
+        actions.forEach((action) => {
+          actionChain = actionChain
+            .then(() => {
+              return cy.get('.wallets-mobile-actions__container')
+              .scrollIntoView() // Scroll the container into view
+              .find('.wallets-mobile-actions')
+                .find(`button[aria-label="${action}"]`)
+                .click({ force: true }); // Click on the action button
+            })
+            .then(() => {
+              return cy.get('.wallets-cashier-header')
+                .find('.wallets-cashier-header__close-icon')
+                .click(); // Close the modal after the action is clicked
+            });
+        });
+
+        actionChain.then(() => {
+          performActionsOnElement(currentIndex + 1, totalElements);
+        });
+      });
+  };
+
+  cy.get('.wallets-progress-bar-active.wallets-progress-bar-transition, .wallets-progress-bar-inactive.wallets-progress-bar-transition')
+    .then(($elements) => {
+      performActionsOnElement(0, $elements.length);
+    });
+}
+
+
 describe('QATEST-139905 - Mobile wallet card redirection', () => {
   beforeEach(() => {
     cy.c_login({ app: 'wallets' })
