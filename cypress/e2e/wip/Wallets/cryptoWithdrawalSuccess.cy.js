9bb01f33ec32b72efdd49e7516db6a5321f902b3
rostislav-deriv <rostislav@deriv.com>
2024-05-24 12:42:20 +0300

    boilerplate

---
+++ cypress/e2e/wip/Wallets/cryptoWithdrawalSuccess.cy.js
@@ -0,0 +1,9 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-98698 - Crypto withdrawal success', () => {
+  beforeEach(() => {
+    cy.c_login({ user: 'wallets', backEndProd: true })
+  })
+
+  it('', () => {})
+})
