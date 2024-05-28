1977fc9b8365e3ebc1ff2bf2c20b8756a66268ae
bertrand <bertrand@regentmarkets.com>
2024-05-28 13:41:21 +0200

    file created

---
+++ cypress/e2e/wip/KYC/poaVerify.cy.js
@@ -0,0 +1,12 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-4835 POA Verify', () => {
+  beforeEach(() => {
+    cy.c_visitResponsive('/')
+    cy.c_createRealAccount('gh')
+    cy.c_login()
+    cy.c_navigateToPoiResponsive('Ghana')
+  })
+
+  it('Should return Date of birth mismatch', () => {})
+})
