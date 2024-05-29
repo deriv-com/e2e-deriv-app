602536b2cec6b8be34fcb1eb3096b4182aaf0119
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2024-05-29 09:54:04 +0100

    Fix to appReg test

--- cypress/e2e/api/createApplicationRegister.cy.js
+++ cypress/e2e/api/createApplicationRegister.cy.js
@@ -1,24 +1,33 @@
 import '@testing-library/cypress/add-commands'
+let oldAppId = 0
 
 describe('QATEST - 148419 - Register a New Application / App ID', () => {
-  beforeEach(() => {
-    cy.log('<prevAppId - beforeEach> ' + Cypress.env('setupComplete'))
-    if (!Cypress.env('setupComplete')) {
-      cy.log('runFromPR = ' + Cypress.env('runFromPR'))
-      if (Cypress.env('runFromPR') == 'true') {
-        cy.c_createApplicationId()
-      }
-
-      Cypress.env('setupComplete', true) // Flag to indicate setup is done
+  it('configAppId should be different to original env var.', () => {
+    oldAppId = Cypress.env('configAppId')
+    cy.log('oldAppId = ' + Cypress.env('configAppId'))
+    if (Cypress.env('runFromPR') != 'true') {
+      cy.c_createApplicationId()
+      cy.log('newAppId = ' + Cypress.env('updatedAppId'))
     }
+
     cy.c_login()
-  })
 
-  it('updateAppId should be incremented. ', () => {
-    cy.log('<Test 1> ' + Cypress.env('setupComplete'))
-  })
-
-  it('updateAppId should NOT be incremented. ', () => {
-    cy.log('<Test 2> ' + Cypress.env('setupComplete'))
+    if (Cypress.env('runFromPR') != 'true') {
+      cy.then(() => {
+        const newConfigAppId = Cypress.env('configAppId')
+        expect(
+          oldAppId,
+          'Old app ID should not be equal to the new app ID'
+        ).to.not.equal(newConfigAppId)
+      })
+    } else {
+      cy.then(() => {
+        const newConfigAppId = Cypress.env('configAppId')
+        expect(
+          oldAppId,
+          'Old app ID should be equal to the new app ID'
+        ).to.equal(newConfigAppId)
+      })
+    }
   })
 })
