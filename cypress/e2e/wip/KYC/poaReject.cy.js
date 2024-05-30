41b44d5b2d93af762567798cc5d553c4afeff639
bertrand <bertrand@regentmarkets.com>
2024-05-30 15:54:11 +0200

    Add Verified tests

--- cypress/e2e/wip/KYC/poaReject.cy.js
+++ cypress/e2e/wip/KYC/poaReject.cy.js
@@ -6,26 +6,30 @@ const CURRENT_DATE = getCurrentDate()
 
 describe('QATEST-4835 POA Reject', () => {
   beforeEach(() => {
-    // cy.c_visitResponsive('/')
-    // cy.c_createRealAccount()
-    // cy.c_login()
-    // cy.c_navigateToPoaResponsive()
+    cy.c_visitResponsive('/')
+    cy.c_createRealAccount()
+    cy.c_login()
+    cy.c_navigateToPoaResponsive()
   })
 
-  it('Should show POA page when POA is rejected', () => {
-    // cy.findByRole('button', { name: 'Save and submit' }).should('not.be.enabled')
+  it('Should show Failure message when POA is rejected', () => {
+    /* Submit POA */
+    cy.findByRole('button', { name: 'Save and submit' }).should(
+      'not.be.enabled'
+    )
+    cy.get('input[type=file]').selectFile(
+      'cypress/fixtures/kyc/testDocument.jpg',
+      { force: true }
+    )
+    cy.findByRole('button', { name: 'Save and submit' })
+      .should('be.enabled')
+      .click()
+    cy.findByText('Your documents were submitted successfully').should(
+      'be.visible'
+    )
+    cy.c_closeNotificationHeader()
 
-    // cy.get('input[type=file]').selectFile(
-    //   'cypress/fixtures/kyc/testDocument.jpg',
-    //   { force: true }
-    // )
-
-    // cy.findByRole('button', { name: 'Save and submit' }).should('be.enabled').click()
-
-    // cy.findByText('Your documents were submitted successfully').should('be.visible')
-    // cy.c_closeNotificationHeader()
-
-    // Visit BO
+    /* Visit BO */
     cy.c_visitResponsive('/', 'large')
     cy.visit(BO_URL)
     cy.findByText('Please login.').click()
@@ -37,16 +41,24 @@ describe('QATEST-4835 POA Reject', () => {
     cy.findByRole('button', { name: /View \/ Edit/i }).click()
     cy.get('.link').eq(1).should('be.visible').click()
 
-    // rejecting POA from BO
-    cy.get('#documents_wrapper table tbody tr td input[type="text"]')
+    /* Rejecting POA from BO */
+    cy.get(
+      '#documents_wrapper table tbody tr td input[name^="issue_date_"]'
+    ).type(CURRENT_DATE)
     cy.get('#documents_wrapper table tbody tr td input[type="checkbox"]')
       .should('exist')
       .check()
-    cy.findByRole('button', { name: /reject_checked_documents/i }).click()
+    cy.findByRole('button', { name: /Reject Checked Files/i }).click()
     cy.get('select[name="client_authentication"]')
       .select('Needs Action')
       .type('{enter}')
 
-    // cy.c_visitResponsive('/account/proof-of-address', 'small')
+    /* Back to Deriv to make final assertions */
+    cy.c_visitResponsive('/account/proof-of-address', 'small')
+    cy.contains('We could not verify your proof of address').should(
+      'be.visible'
+    )
+    cy.findByRole('button', { name: 'Resubmit' }).should('be.visible')
+    cy.c_closeNotificationHeader()
   })
 })
