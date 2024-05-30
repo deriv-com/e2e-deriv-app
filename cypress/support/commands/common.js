c753b311e21df866e3d18e349cc06cf8a99e00da
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard.local>
2024-05-30 11:05:21 +0800

    update

--- cypress/support/commands/common.js
+++ cypress/support/commands/common.js
@@ -135,16 +135,6 @@ Cypress.Commands.add('c_login', (options = {}) => {
       loginEmail,
       loginPassword
     )
-    // } else if (
-    //   (Cypress.env('oAuthUrl') == '<empty>' && app == 'wallets') ||
-    //   app == 'doughflow'
-    // ) {
-    //   getWalletOAuthUrl((oAuthUrl) => {
-    //     cy.log('came inside wallet getOauth')
-    //     Cypress.env('oAuthUrl', oAuthUrl)
-    //     cy.c_doOAuthLogin(app, { rateLimitCheck: rateLimitCheck })
-    //   })
-    // }
   } else {
     cy.c_doOAuthLogin(app, { rateLimitCheck: rateLimitCheck })
   }
