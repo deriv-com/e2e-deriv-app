a525e7a4ed393a6a193a968484d5b72fb9e20079
maliheh-deriv <55732602+maliheh-deriv@users.noreply.github.com>
2024-05-31 15:49:27 +0800

    Mali/qatest 121392 (#330)
    * verify filter
    
    * add order with payment method
    
    * add sell and buy order with pm
    
    * update based on new design
    
    * wip
    
    * add the second account
    
    * update filters
    
    * move functions to common folder
    
    * update verification on payment method
    
    * ad accounts
    
    * check toggle
    
    * wip
    
    * update loading check
    
    * remove the function redirect to my ads

--- cypress.config.js
+++ cypress.config.js
@@ -197,6 +197,14 @@ module.exports = defineConfig({
         ID: process.env.E2E_LOGIN_ID_P2P_EMPTYSTATE,
         PSWD: process.env.E2E_QA_ACCOUNT_PASSWORD
       },
+      p2pFilterPaymentMethodBase: {
+        ID: process.env.E2E_P2P_FILTER_PM_BASE,
+        PSWD: process.env.E2E_QA_ACCOUNT_PASSWORD
+      },
+      p2pFilterPaymentMethodSelector: {
+        ID: process.env.E2E_P2P_FILTER_PM_SELECTOR,
+        PSWD: process.env.E2E_QA_ACCOUNT_PASSWORD
+      },
       allcrypto: {
         ID: process.env.E2E_CRYPTO,
         PSWD: process.env.E2E_QA_ACCOUNT_PASSWORD,
