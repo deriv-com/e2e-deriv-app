d233ef0c82a0a4766ab330b96b5f72586d8fa3fe
Indulekha <indulekha@regentmarkets.com>
2024-06-03 22:42:07 +0400

    trade account switcher

--- cypress/e2e/wipWallet/tradepageAccountswticher.cy.js
+++ cypress/e2e/wipWallet/tradepageAccountswticher.cy.js
@@ -1,56 +1,77 @@
 import '@testing-library/cypress/add-commands'
-function validateAccountSwticher(swticherTitle,CFDbanner) {
-    cy.findByTestId('dt_acc_info').should('be.visible').click().then(() => {
-    cy.findByRole('heading', { name: 'Options accounts',  timeout: 20000 }).should('be.visible')})
-    cy.get(swticherTitle)
+function validateAccountSwitcher(swticherTitle, CFDbanner) {
+  cy.findByTestId('dt_acc_info')
+    .should('be.visible')
+    .click()
+    .then(() => {
+      cy.findByRole('heading', {
+        name: 'Options accounts',
+        timeout: 20000,
+      }).should('be.visible')
+    })
+  cy.log('the parm passed' + CFDbanner)
+  cy.log('2nd parm ' + swticherTitle)
+  cy.get(swticherTitle)
     .should('have.length.greaterThan', 0)
     .each(($el) => {
-      cy.wrap($el)
-        .find('.dc-text')
-        .contains('Options')
-        .should('exist')})
-        cy.get(CFDbanner).contains('.dc-text', 'Looking for CFDs? Go to Trader’s Hub').should('be.visible').click().then(() => {
-            cy.findByText('CFDs').should('be.visible')
-  })}
+      cy.wrap($el).find('.dc-text').contains('Options').should('exist')
+    })
+  cy.get(CFDbanner)
+    .contains('.dc-text', 'Looking for CFDs? Go to Trader’s Hub')
+    .should('be.visible')
+    .click()
+    .then(() => {
+      cy.findByText('CFDs').should('be.visible')
+    })
+}
+function navigateToaccountSwitcher() {
+  const derivAppProdUrl = `${Cypress.env('prodURL')}dtrader?chart_type=`
+  const derivAppStagingUrl = `${Cypress.env('stagingUrl')}dtrader?chart_type=`
+  cy.get('.wallets-deriv-apps-section__title-and-badge')
+    .contains('.wallets-text', 'Options')
+    .should('be.visible')
+    .then(() => {
+      cy.get('.wallets-list-card__badge')
+        .contains('.wallets-text', 'SVG')
+        .should('be.visible')
+      cy.get('.wallets-options-and-multipliers-listing__content__details')
+        .contains('.wallets-text', 'Deriv Trader')
+        .should('be.visible')
+        .click() //Navigate to Trade page
+      if (Cypress.config().baseUrl.includes('staging'))
+        cy.url().should('include', derivAppStagingUrl)
+      else cy.url().should('include', derivAppProdUrl)
+    })
+}
 
 describe('QATEST-129858 -  Trade page account switcher', () => {
   beforeEach(() => {
     cy.c_login({ user: 'walletloginEmail' })
   })
-  it('Trade page account switcher', () => {
-    const derivAppProdUrl = `${Cypress.env('prodURL')}dtrader?chart_type=`
-    const derivAppStagingUrl = `${Cypress.env('stagingUrl')}dtrader?chart_type=`
+  it('Navigate to account swticher from Trade page &  Manage account settings page', () => {
     cy.c_visitResponsive('/', 'large')
-    cy.get('.wallets-deriv-apps-section__title-and-badge').contains('.wallets-text', 'Options').should('be.visible').then(() =>{
-    cy.get('.wallets-list-card__badge').contains('.wallets-text', 'SVG').should('be.visible')
-    cy.get('.wallets-options-and-multipliers-listing__content__details').contains('.wallets-text', 'Deriv Trader').should('be.visible').click()//Navigate to Trade page
-    if (Cypress.config().baseUrl.includes('staging'))
-        cy.url().should('include', derivAppStagingUrl)
-      else cy.url().should('include', derivAppProdUrl)
-      cy.get('#dt_reports_tab').should('be.visible')
-      validateAccountSwticher()  
+    navigateToaccountSwitcher()
+    cy.get('#dt_reports_tab').should('be.visible')
+    validateAccountSwitcher(
+      '.acc-switcher-wallet-item__content',
+      '.account-switcher-wallet__looking-for-cfds'
+    )
     cy.findByRole('banner').findByTestId('dt_span').findByRole('link').click()
     cy.findByText('Settings').should('be.visible')
-    validateAccountSwticher() 
-      })
-      
-    })
-  it.only('Responsive - Trade page account switcher', () => {
-    const derivAppProdUrl = `${Cypress.env('prodURL')}dtrader?chart_type=`
-    const derivAppStagingUrl = `${Cypress.env('stagingUrl')}dtrader?chart_type=`
+    validateAccountSwitcher(
+      '.acc-switcher-wallet-item__content',
+      '.account-switcher-wallet__looking-for-cfds'
+    )
+  })
+
+  it('Responsive - Navigate to account swticher from Trade page &  Manage account settings page', () => {
     cy.c_visitResponsive('/', 'small')
     cy.findByText('Options').click()
-    cy.get('.wallets-deriv-apps-section__title-and-badge').contains('.wallets-text', 'Options').should('be.visible').then(() =>{
-    cy.get('.wallets-list-card__badge').contains('.wallets-text', 'SVG').should('be.visible')
-    cy.get('.wallets-options-and-multipliers-listing__content__details').contains('.wallets-text', 'Deriv Trader').should('be.visible').click()//Navigate to Trade page
-    if (Cypress.config().baseUrl.includes('staging'))
-        cy.url().should('include', derivAppStagingUrl)
-      else cy.url().should('include', derivAppProdUrl)
-      cy.get('.header__menu-left-extensions').should('be.visible')
-      validateAccountSwticher()  
-    cy.findByRole('banner').findByTestId('dt_span').findByRole('link').click()
-    cy.findByText('Personal details').should('be.visible')
-    validateAccountSwticher('.dc-mobile-dialog__header','.account-switcher-wallet-mobile__footer') 
-      })
+    navigateToaccountSwitcher()
+    cy.get('#dt_positions_toggle').should('be.visible')
+    validateAccountSwitcher(
+      '.acc-switcher-wallet-item__content',
+      '.account-switcher-wallet-mobile__footer'
+    )
   })
 })
