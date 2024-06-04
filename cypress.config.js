af756288177c6511573681cd485bbcd4f52f452d
rajendra-deriv <140414984+rajendra-deriv@users.noreply.github.com>
2024-06-04 13:00:10 +0400

    Adding Passkeys Folder in Smoke and Adding Basic Passkeys Button Veri… (#314)
    * Adding Passkeys Folder in Smoke and Adding Basic Passkeys Button Verification on Login page
    
    * Removing beforeEach() method and using passkeyAppId directly instead of assigning stdConfigAppId
    
    * Removing cy.visit() method and using c_visitResponsive directly
    
    * Updated Passkeys.cy.js as per comments

--- cypress.config.js
+++ cypress.config.js
@@ -388,6 +388,8 @@ module.exports = defineConfig({
     actualAmount : process.env.E2E_ACTUAL_AMOUNT,
     appRegisterID: process.env.E2E_APP_REGISTER,
     appRegisterUrl: process.env.E2E_APP_REGISTER_URL,
+    passkeyUrl: process.env.E2E_PASSKEY_URL,
+    passkeyAppId: process.env.E2E_PASSKEY_APP_ID,
     countries: {
       ZA: "South Africa",
       CO: "Colombia",
