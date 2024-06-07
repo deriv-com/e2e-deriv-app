9c0547c8eff87656409efb833028a744fa80e5bf
usman-deriv <usman@deriv.com>
2024-06-07 16:02:21 +0800

    wip

--- cypress/e2e/smoke/P2P/createCrossBorderSellOrderFloatingRate.cy.js
+++ cypress/e2e/smoke/P2P/createCrossBorderSellOrderFloatingRate.cy.js
@@ -12,6 +12,10 @@ let nicknameAndAmount = {
   seller: '',
   amount: '',
 }
+let currency = {
+  name: 'Mongolian Tögrög',
+  code: 'MNT',
+}
 let fiatCurrency = 'USD'
 
 let isSellAdUser = true
@@ -29,14 +33,14 @@ describe('QATEST-50478, QATEST-2709, QATEST-2542, QATEST-2769, QATEST-2610  - Ad
     if (isSellAdUser == true) {
       loginWithNewUser('p2pFloatingSellAd1', false)
     } else {
-      loginWithNewUser('p2pFloatingSellAd2', true)
+      loginWithNewUser('p2pVerifyEmptyStateAdScreen', true)
     }
     cy.c_visitResponsive('/appstore/traders-hub', 'small'),
       {
         rateLimitCheck: true,
       }
   })
-  it.only('Should be able to create sell type advert and verify all fields and messages for floating rate.', () => {
+  it('Should be able to create sell type advert and verify all fields and messages for floating rate.', () => {
     cy.c_navigateToP2P()
     cy.findByText('My profile').should('be.visible').click()
     cy.findByText('Available Deriv P2P balance').should('be.visible')
@@ -47,8 +51,8 @@ describe('QATEST-50478, QATEST-2709, QATEST-2542, QATEST-2769, QATEST-2610  - Ad
       nicknameAndAmount.sellerBalanceBeforeSelling = balance
     })
     cy.c_clickMyAdTab()
-    cy.c_createNewAd('sell')
-    cy.c_inputAdDetails(floatRate, minOrder, maxOrder, 'Sell', 'float')
+    cy.c_createNewAd('buy')
+    cy.c_inputAdDetails(floatRate, minOrder, maxOrder, 'Buy', 'float')
   })
   it('Should be able to place an order for advert and verify all fields and messages for floating rate.', () => {
     cy.c_navigateToP2P()
@@ -60,6 +64,15 @@ describe('QATEST-50478, QATEST-2709, QATEST-2542, QATEST-2769, QATEST-2610  - Ad
     cy.c_getProfileBalance().then((balance) => {
       nicknameAndAmount.buyerBalanceBeforeBuying = balance
     })
+    cy.findByText('Buy / Sell').should('be.visible').click()
+    cy.findByRole('button', { name: 'Sell' }).should('be.visible').click()
+    cy.findByTestId('dt_dropdown_container').should('be.visible').click()
+    cy.findByText('Preferred currency').should('be.visible')
+    cy.findByText(currency.name).should('be.visible').click()
+    cy.findByTestId('dt_dropdown_container')
+      .find('.dc-dropdown__display-text')
+      .should('have.text', currency.code)
+    cy.c_loadingCheck()
     cy.then(() => {
       cy.c_createBuyOrder(
         nicknameAndAmount.seller,
