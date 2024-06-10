9e5e2ce83584cab808be1fbc213b3890acd51691
Afsaneh Rezaeinejad <afsaneh@Afsaneh-Rezaeinejads-Mac-HQXJN6631F-Standard-No-NextDNS.local>
2024-06-10 16:40:23 +0800

    update

---
+++ cypress/e2e/smoke/Wallets/derifxResetpassword.cy.js
@@ -0,0 +1,152 @@
+import '@testing-library/cypress/add-commands'
+function clickAddDerivxButton() {
+  cy.get('.wallets-available-dxtrade__icon')
+    .parent('.wallets-trading-account-card')
+    .click()
+}
+
+function verifyDerivxCreation(accountType) {
+  let expectedText
+  if (accountType === 'Real') {
+    expectedText = 'Create a Deriv X password'
+    cy.get('div').contains(expectedText).should('be.visible')
+    cy.findByPlaceholderText('Deriv X password')
+      .click()
+      .type(Cypress.env('mt5Password'))
+    cy.findByRole('button', { name: 'Create Deriv X password' }).click()
+  } else {
+    expectedText = 'Enter your Deriv X password' // Adjust this text based on your actual requirement
+    cy.get('div').contains(expectedText).should('be.visible')
+    cy.findByPlaceholderText('Deriv X password')
+      .click()
+      .type(Cypress.env('mt5Password'))
+    cy.findByRole('button', { name: 'Add account' }).click()
+  }
+}
+
+function verifyTransferFundsMessage(accountType) {
+  if (accountType === 'Real') {
+    cy.findByText('Your Deriv X account is ready').should('be.visible')
+    cy.findByRole('button', { name: 'Maybe later' }).should('exist')
+    cy.findByRole('button', { name: 'Transfer funds' }).should('exist')
+    cy.findByRole('button', { name: 'Maybe later' }).click()
+  } else {
+    cy.findByText('Your Deriv X demo account is ready').should('be.visible')
+    cy.findByRole('button', { name: 'OK' }).click()
+  }
+}
+
+function expandDemoWallet() {
+  cy.get('label').find('span').click()
+  cy.findByText('USD Demo Wallet').should('be.visible')
+}
+function existingAccountCheck(walletBanner) {
+  cy.get(walletBanner).contains('.wallets-text', ' USD').should('be.visible') //To check page load
+  cy.findByText('CFDs', { exact: true }).should('be.visible').click()
+  return cy
+    .get('.wallets-added-dxtrade__details, .wallets-available-dxtrade__details')
+    .then(($details) => {
+      if ($details.hasClass('wallets-added-dxtrade__details')) {
+        return 'added'
+      } else if ($details.hasClass('wallets-available-dxtrade__details')) {
+        return 'available'
+      } else {
+        return 'none'
+      }
+    })
+}
+function addDerivXaccount(status, accountType) {
+  if (status === 'available') {
+    cy.log(accountType + ' DerivX account ready to add')
+    clickAddDerivxButton()
+    verifyDerivxCreation(accountType)
+    verifyTransferFundsMessage(accountType)
+  } else if (status === 'added') {
+    cy.log(accountType + ' DerivX account added already')
+    cy.get('.wallets-added-dxtrade__details')
+      .should('exist')
+      .within(() => {
+        cy.contains('.wallets-text', 'Deriv X').should('exist')
+      })
+  } else {
+    cy.log('Neither found')
+  }
+}
+function changeDerivxPassword() {
+  cy.findByText('Deriv X').parent().click()
+  cy.findByText('Deriv X SVG').should('be.visible')
+  cy.findByText('Password')
+    .parent()
+    .within(() => {
+      cy.get('.wallets-tooltip').click()
+    })
+  cy.findByText('Manage Deriv X password').should('be.visible')
+  cy.findByText(
+    'Use this password to log in to your Deriv X accounts on the desktop, web, and mobile apps.'
+  ).should('be.visible')
+  cy.findByRole('button', { name: 'Change password' }).click()
+  cy.findByText('Confirm to change your Deriv X password').should('be.visible')
+  cy.contains('This will change the password to all of your ').should('exist')
+  cy.findByRole('button', { name: 'Confirm' }).click()
+  cy.findByText('We’ve sent you an email').should('exist')
+  cy.findByRole('button', { name: "Didn't receive the email?" }).should('exist')
+  cy.c_emailVerification(
+    'New%20Deriv%20X%20password%20request.html',
+    'QA script',
+    { baseUrl: Cypress.env('configServer') + '/emails' }
+  )
+  cy.then(() => {
+    cy.c_visitResponsive(Cypress.env('verificationUrl'), 'large')
+  })
+  cy.get('div').contains('Create a new Deriv X password').should('be.visible')
+  cy.findByPlaceholderText('Deriv X password')
+    .click()
+    .type(Cypress.env('mt5Password'))
+  cy.findByRole('button', { name: 'Create' }).click()
+}
+describe('QATEST-121523 - Forget DerivX password', () => {
+  beforeEach(() => {
+    cy.c_login({ user: 'walletloginEmail' })
+  })
+
+  it('should be able to change derivx password', () => {
+    cy.log('change derivx password')
+    cy.c_visitResponsive('/', 'large')
+    cy.findByText('CFDs', { exact: true }).should('be.visible')
+    cy.wait(3000)
+    cy.findByText('Deriv X', { timeout: 10000 }).should('exist')
+    cy.findByText(
+      'CFDs on financial and derived instruments via a customisable platform.'
+    )
+      .should(() => {})
+      .then(($el) => {
+        if ($el.length) {
+          cy.log(`Derivx account doesn't exist`)
+          addDerivXaccount('available', 'Real')
+          changeDerivxPassword()
+        } else {
+          changeDerivxPassword()
+        }
+      })
+  })
+  it('should be able to change derivx password in responsive', () => {
+    cy.log('change derivx password')
+    cy.c_visitResponsive('/', 'small')
+    cy.findByText('CFDs', { exact: true }).should('be.visible')
+    cy.c_skipPasskeysV2()
+    cy.findByText('Deriv X', { timeout: 10000 }).should('exist')
+    cy.findByText(
+      'CFDs on financial and derived instruments via a customisable platform.'
+    )
+      .should(() => {})
+      .then(($el) => {
+        if ($el.length) {
+          cy.log(`Derivx account doesn't exist`)
+          addDerivXaccount('available', 'Real')
+          changeDerivxPassword()
+        } else {
+          changeDerivxPassword()
+        }
+      })
+  })
+})
