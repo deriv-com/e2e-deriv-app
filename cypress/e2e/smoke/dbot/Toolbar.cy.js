1b20017ea1aef44b8a739cb844df9ac7ebf0cec6
thivya-deriv <82857131+thivya-deriv@users.noreply.github.com>
2024-06-11 12:58:02 +0800

    Import & Export on dbot responsive (#366)
    * Run test on responsive
    
    * Add check for page load
    
    * Adding logs
    
    * Code update post review

--- cypress/e2e/smoke/dbot/Toolbar.cy.js
+++ cypress/e2e/smoke/dbot/Toolbar.cy.js
@@ -3,35 +3,42 @@ import BotDashboard from '../../../support/pageobjects/dbot/bot_dashboard_page'
 import BotBuilder from '../../../support/pageobjects/dbot/bot_builder_page'
 
 describe('QATEST-99418: Verify toolbar on bot builder page', () => {
+  const size = ['small', 'desktop']
   const botDashboard = new BotDashboard()
   const botBuilder = new BotBuilder()
   let strategyName = 'Stock_Netherland_25' + Math.random().toString()
 
   beforeEach(() => {
     cy.c_login({ user: 'dBot' })
-    cy.c_visitResponsive('/bot', 'large')
-    cy.c_loadingCheck()
-    cy.c_skipTour()
-    botBuilder.openBotBuilderTab()
-    cy.c_skipTour()
-    cy.c_switchToDemoBot()
   })
 
-  it('Save a strategy to local', () => {
-    botBuilder.changeMarketOnBlocklyWorkspace(1, 'Stock Indices')
-    botBuilder.changeMarketOnBlocklyWorkspace(2, 'European indices')
-    botBuilder.saveStrategyFromToolbar(strategyName)
-    cy.wait(5000)
-    botDashboard.goToDashboard()
-    botDashboard.strategySaveStatus(strategyName).should('have.text', 'Local')
-  })
-
-  it('Import strategy from local', () => {
-    botBuilder.importStrategyFromToolbar('MartingaleOld')
-    //TODO:Update once BOT-1469 done
-    cy.get('.notification-content').should(
-      'have.text',
-      'You’ve successfully imported a bot.'
-    )
+  size.forEach((size) => {
+    it(`Import and Export strategy from toolbar on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size == 'small' ? true : false
+      cy.c_visitResponsive('appstore/traders-hub', size)
+      cy.findAllByTestId('dt_balance_text_container').should('have.length', '2')
+      if (isMobile) cy.c_skipPasskeysV2()
+      cy.c_openDbotThub()
+      if (isMobile) cy.findByTestId('close-icon', { timeout: 7000 }).click()
+      cy.c_skipTour()
+      cy.c_switchToDemoBot()
+      botBuilder.openBotBuilderTab()
+      cy.c_skipTour()
+      cy.log('Saving strategy from Toolbar')
+      botBuilder.changeMarketOnBlocklyWorkspace(1, 'Stock Indices')
+      botBuilder.changeMarketOnBlocklyWorkspace(2, 'European indices')
+      botBuilder.saveStrategyFromToolbar(strategyName)
+      botDashboard.goToDashboard()
+      botDashboard.strategySaveStatus(strategyName).should('have.text', 'Local')
+      cy.log('Importing strategy from Toolbar')
+      botBuilder.openBotBuilderTab()
+      botBuilder.importStrategyFromToolbar('MartingaleOld')
+      cy.c_loadingCheck()
+      //TODO:Update once BOT-1469 done
+      cy.get('.notification-content').should(
+        'have.text',
+        'You’ve successfully imported a bot.'
+      )
+    })
   })
 })
