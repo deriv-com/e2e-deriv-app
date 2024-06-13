7caa66e161219aaf7a8ac3eaf74c0b6603ddc670
Indulekha <indulekha@regentmarkets.com>
2024-06-13 18:07:01 +0400

    wallet

--- cypress/e2e/smoke/Wallets/walletSwitcher.cy.js
+++ cypress/e2e/smoke/Wallets/walletSwitcher.cy.js
@@ -1,9 +1,32 @@
+function clickEachItemSequentially() {
+  // Click the dropdown to reveal the list
+  cy.get('[data-testid="dt_wallets_textfield_icon_right"]').click()
+  return cy.get('[role=listbox]').find('[role=option]').its('length')
+  }
+
+  // function clickEachWalletSequentially() {
+  //   cy.get('[data-testid="dt_wallets_textfield_icon_right"]').click()
+  //   cy.get('[role=listbox]').within(() => {
+  //       cy.get('[role=option]').then($list => {
+  //         const listItems = $list.length 
+  //         cy.log('Total list items:', listItems)
+  //         for (let i = 1; i < listItems; i++) {
+  //           cy.log('the index value is' + i)
+  //           cy.get('.wallets-list-card-dropdown__item-content',{ timeout: 10000 }).should('be.visible').eq(i).click({ force: true }).debug().then(() => {
+  //             cy.wait(1000)
+  //             cy.get('[data-testid="dt_desktop_accounts_list"]').within(() => {
+  //               cy.get('button').contains('Transfer').should('be.visible')})
+  //           cy.get('.wallets-dropdown').click({ force: true })})
+  //         }
+  //     })})
+ // }
+  
 describe('QATEST-157196 Demo and Real Wallet Switcher',() =>{
     beforeEach(() => {
         cy.c_login({ user: 'walletloginEmail' })
         cy.wait(10000)
       })
-it('Check demo and Real wallet swticher' ,() =>{
+it.only('Check demo and Real wallet swticher' ,() =>{
     cy.c_visitResponsive('/', 'large')
     //Land on Trader's Hub home Demo and Real Switcher should be visible
     cy.findByTestId('dt_themed_scrollbars').findByText('Trader\'s Hub').should('be.visible')
@@ -37,9 +60,21 @@ it('Check demo and Real wallet swticher' ,() =>{
         .find('button', { timeout: 15000 })
         .then((button) => {
           expect(button).to.contain('Added')})})
-
+          ///test
+          //clickEachWalletSequentially()
+            clickEachItemSequentially().then(listItems => {
+            cy.log('Total list items:', listItems)
+            for (let i = 1; i < listItems; i++) {
+              cy.log('the index value is' + i)
+              cy.get('.wallets-list-card-dropdown__item-content',{ timeout: 10000 }).should('be.visible').eq(i).click({ force: true }).debug().then(() => {
+                cy.get('[data-testid="dt_desktop_accounts_list"]').within(() => {
+                  cy.get('button').contains('Transfer').should('be.visible')})
+              cy.get('.wallets-dropdown').click({ force: true })})
+            }
+            })
+         
 })
-it.only('Responsive - Check demo and Real wallet switcher' ,() =>{
+it('Responsive - Check demo and Real wallet switcher' ,() =>{
     cy.c_visitResponsive('/', 'small')
     cy.findByText('Trader\'s Hub').should('be.visible')
     cy.findByText('CFDs', { exact: true }).should('be.visible')
@@ -54,7 +89,7 @@ it.only('Responsive - Check demo and Real wallet switcher' ,() =>{
       .then(() => {
         cy.findByText('Demo').should('be.visible')
         cy.contains(/VRTC/)
-      .should('be.visible').click()
+      .should('be.visible')
       })
     })
 
