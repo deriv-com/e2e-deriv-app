fdba22798a311572e1e2b5982c782ebe5d62dd5e
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-06-18 12:31:47 +0200

    modified some code

--- cypress/support/commands/kyc.js
+++ cypress/support/commands/kyc.js
@@ -11,14 +11,9 @@ Cypress.Commands.add('c_navigateToPoi', (country) => {
   cy.contains(country).click()
   cy.contains('button', 'Next').click()
 })
-Cypress.Commands.add('c_skipPassKeys', () => {
-  cy.findByText('Maybe later').click()
-  cy.c_visitResponsive('/account/proof-of-identity', 'small')
-})
 
 Cypress.Commands.add('c_navigateToPoiResponsive', (country) => {
   cy.c_visitResponsive('/account/proof-of-identity', 'small')
-  cy.c_skipPassKeys()
   cy.c_closeNotificationHeader()
   cy.get('select[name="country_input"]').select(country)
   cy.contains('button', 'Next').click()
