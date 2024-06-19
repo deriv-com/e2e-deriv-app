2be29a36331b56d576d8e2fc89f9facdf17b410f
Deborah Uwimbabazi <deborah.uwimbabazi@deriv.com>
2024-06-19 13:46:02 +0200

    added some tests

---
+++ cypress/e2e/wip/KYC/onfidoPoiExpiredDocument.cy.js
@@ -0,0 +1,43 @@
+describe('QATEST-4760 Onfido Expired document.', () => {
+  beforeEach(() => {
+    cy.c_createRealAccount('co')
+    cy.c_login()
+    cy.c_navigateToPoiResponsive('Colombia')
+  })
+  it('POI resubmission option should be dislayed.', () => {
+    cy.findByTestId('date_of_birth').type('1990-01-01')
+    cy.findByText('Choose document').should('be.visible')
+    cy.get('.dc-checkbox__box').click()
+    cy.findByText('Passport').click()
+    cy.findByText('Submit passport photo pages').should('be.visible')
+    cy.findByText('or upload photo – no scans or photocopies').click()
+    cy.findByText('Upload passport photo page').should('be.visible')
+    cy.get('input[type=file]').selectFile(
+      'cypress/fixtures/kyc/testDriversLicense.jpeg',
+      { force: true }
+    )
+    cy.findByText('Confirm').click()
+    cy.findByText('Continue').click()
+    cy.findByText('Take a selfie').should('be.visible')
+    cy.get('.onfido-sdk-ui-Camera-btn').click()
+    cy.findByText('Confirm').click()
+    //cy.findByText('Account verification required').should('be.visible')
+    cy.c_closeNotificationHeader()
+    cy.c_waitUntilElementIsFound({
+      cyLocator: () => cy.findByText('Your proof of identity is verified'),
+      timeout: 4000,
+      maxRetries: 5,
+    })
+    /* Access BO */
+    cy.c_visitResponsive('/', 'large')
+    cy.visit(BO_URL)
+    cy.findByText('Please login.').click()
+    cy.findByText('Client Management').click()
+    cy.findByPlaceholderText('email@domain.com')
+      .should('exist')
+      .clear()
+      .type(Cypress.env('credentials').test.masterUser.ID)
+    cy.findByRole('button', { name: /View \/ Edit/i }).click()
+    cy.get('.link').eq(1).should('be.visible').click()
+  })
+})
