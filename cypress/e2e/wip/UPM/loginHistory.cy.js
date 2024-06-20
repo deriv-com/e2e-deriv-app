80027583d53abdb68c3056eb7b57813373361660
munnfaye-deriv <munnfaye@regentmarkets.com>
2024-06-20 17:07:09 +0800

    fix prettier

--- cypress/e2e/wip/UPM/loginHistory.cy.js
+++ cypress/e2e/wip/UPM/loginHistory.cy.js
@@ -16,60 +16,59 @@ function isValidDateFormat(dateString) {
 }
 
 describe('UPM-1183 - Verify Login history page on desktop', () => {
-    beforeEach(() => {
-        cy.c_createRealAccount(countryCode)
-        cy.c_login()
+  beforeEach(() => {
+    cy.c_createRealAccount(countryCode)
+    cy.c_login()
+  })
+  it('Should validate Login history page is showing proper date info', () => {
+    cy.c_visitResponsive('/', 'large')
+    cy.get('.traders-hub-header__setting').click()
+    cy.get('#dc_login-history_link').click()
+    /* Validate the format of the date displayed on the page */
+    cy.get('span.dc-text')
+      .contains('GMT')
+      .invoke('text')
+      .then((dateTimeString) => {
+        const displayedDate = isValidDateFormat(dateTimeString)
+        if (displayedDate != null) {
+          expect(displayedDate).to.equal(currentDate)
+          cy.log(
+            `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
+          )
+        } else {
+          cy.log('Date format does not match expected pattern.')
+        }
       })
-      
-    it('Should validate Login history page is showing proper date info', () => {
-        cy.c_visitResponsive('/', 'large')
-        cy.get('.traders-hub-header__setting').click()
-        cy.get('#dc_login-history_link').click()
-        /* Validate the format of the date displayed on the page */
-        cy.get('span.dc-text')
-          .contains('GMT')
-          .invoke('text')
-          .then((dateTimeString) => {
-            const displayedDate = isValidDateFormat(dateTimeString)
-            if (displayedDate != null) {
-              expect(displayedDate).to.equal(currentDate)
-              cy.log(
-                `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
-              )
-            } else {
-              cy.log('Date format does not match expected pattern.')
-            }
-          })
-        /* Ensure 'Invalid date' is not displayed like in UPM-1162 */
-        cy.findByText('Invalid date').should('not.exist')
-    })
-    it('Should validate Login history page is showing proper date info', () => {
-        cy.c_visitResponsive('/', 'small')
-        cy.c_skipPasskeysV2()
-        cy.get('.traders-hub-header__setting').click()
-        derivApp.commonPage.mobileLocators.header.hamburgerMenuButton().click()
-        derivApp.commonPage.mobileLocators.sideMenu
-            .sidePanel()
-            .findByText('Account Settings')
-            .click()
-        derivApp.commonPage.mobileLocators.sideMenu
-            .sidePanel()
-            .findByRole('link', { name: 'Login history' })
-            .click()
-        cy.get('span.dc-text')
-          .contains('GMT')
-          .invoke('text')
-          .then((dateTimeString) => {
-            const displayedDate = isValidDateFormat(dateTimeString)
-            if (displayedDate != null) {
-              expect(displayedDate).to.equal(currentDate)
-              cy.log(
-                `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
-              )
-            } else {
-              cy.log('Date format does not match expected pattern.')
-            }
-          })
-        cy.findByText('Invalid date').should('not.exist')
-    })
+    /* Ensure 'Invalid date' is not displayed like in UPM-1162 */
+    cy.findByText('Invalid date').should('not.exist')
+  })
+  it('Should validate Login history page is showing proper date info', () => {
+    cy.c_visitResponsive('/', 'small')
+    cy.c_skipPasskeysV2()
+    cy.get('.traders-hub-header__setting').click()
+    derivApp.commonPage.mobileLocators.header.hamburgerMenuButton().click()
+    derivApp.commonPage.mobileLocators.sideMenu
+      .sidePanel()
+      .findByText('Account Settings')
+      .click()
+    derivApp.commonPage.mobileLocators.sideMenu
+      .sidePanel()
+      .findByRole('link', { name: 'Login history' })
+      .click()
+    cy.get('span.dc-text')
+      .contains('GMT')
+      .invoke('text')
+      .then((dateTimeString) => {
+        const displayedDate = isValidDateFormat(dateTimeString)
+        if (displayedDate != null) {
+          expect(displayedDate).to.equal(currentDate)
+          cy.log(
+            `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
+          )
+        } else {
+          cy.log('Date format does not match expected pattern.')
+        }
+      })
+    cy.findByText('Invalid date').should('not.exist')
+  })
 })
