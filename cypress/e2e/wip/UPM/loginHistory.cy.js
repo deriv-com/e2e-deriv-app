3038ce0a55954188a773e870b8df8e18530786de
munnfaye-deriv <munnfaye@regentmarkets.com>
2024-06-20 17:04:23 +0800

    separate desktop and responsive view

--- cypress/e2e/wip/UPM/loginHistory.cy.js
+++ cypress/e2e/wip/UPM/loginHistory.cy.js
@@ -1,7 +1,6 @@
 import { derivApp } from '../../../support/locators'
 import { getCurrentDate } from '../../../support/helper/utility'
 
-const size = ['small', 'desktop']
 const currentDate = getCurrentDate()
 let countryCode = 'id'
 
@@ -15,48 +14,62 @@ function isValidDateFormat(dateString) {
     return dateOnly
   }
 }
+
 describe('UPM-1183 - Verify Login history page on desktop', () => {
-  before(() => {
-    cy.c_createDemoAccount(countryCode)
-  })
-  beforeEach(() => {
-    cy.c_login()
-  })
-  size.forEach((size) => {
+    beforeEach(() => {
+        cy.c_createRealAccount(countryCode)
+        cy.c_login()
+      })
+      
     it('Should validate Login history page is showing proper date info', () => {
-      const isMobile = size == 'small' ? true : false
-      cy.c_visitResponsive('appstore/traders-hub', size)
-      cy.get('.traders-hub-header__setting').click()
-      if (isMobile) {
+        cy.c_visitResponsive('/', 'large')
+        cy.get('.traders-hub-header__setting').click()
+        cy.get('#dc_login-history_link').click()
+        /* Validate the format of the date displayed on the page */
+        cy.get('span.dc-text')
+          .contains('GMT')
+          .invoke('text')
+          .then((dateTimeString) => {
+            const displayedDate = isValidDateFormat(dateTimeString)
+            if (displayedDate != null) {
+              expect(displayedDate).to.equal(currentDate)
+              cy.log(
+                `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
+              )
+            } else {
+              cy.log('Date format does not match expected pattern.')
+            }
+          })
+        /* Ensure 'Invalid date' is not displayed like in UPM-1162 */
+        cy.findByText('Invalid date').should('not.exist')
+    })
+    it('Should validate Login history page is showing proper date info', () => {
+        cy.c_visitResponsive('/', 'small')
+        cy.c_skipPasskeysV2()
+        cy.get('.traders-hub-header__setting').click()
         derivApp.commonPage.mobileLocators.header.hamburgerMenuButton().click()
         derivApp.commonPage.mobileLocators.sideMenu
-          .sidePanel()
-          .findByText('Account Settings')
-          .click()
+            .sidePanel()
+            .findByText('Account Settings')
+            .click()
         derivApp.commonPage.mobileLocators.sideMenu
-          .sidePanel()
-          .findByRole('link', { name: 'Login history' })
-          .click()
-      } else {
-        cy.get('#dc_login-history_link').click()
-      }
-      /* Validate the format of the date displayed on the page */
-      cy.get('span.dc-text')
-        .contains('GMT')
-        .invoke('text')
-        .then((dateTimeString) => {
-          const displayedDate = isValidDateFormat(dateTimeString)
-          if (displayedDate != null) {
-            expect(displayedDate).to.equal(currentDate)
-            cy.log(
-              `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
-            )
-          } else {
-            cy.log('Date format does not match expected pattern.')
-          }
-        })
-      /* Ensure 'Invalid date' is not displayed like in UPM-1162 */
-      cy.findByText('Invalid date').should('not.exist')
+            .sidePanel()
+            .findByRole('link', { name: 'Login history' })
+            .click()
+        cy.get('span.dc-text')
+          .contains('GMT')
+          .invoke('text')
+          .then((dateTimeString) => {
+            const displayedDate = isValidDateFormat(dateTimeString)
+            if (displayedDate != null) {
+              expect(displayedDate).to.equal(currentDate)
+              cy.log(
+                `Displayed date '${displayedDate}' matches current date '${currentDate}'.`
+              )
+            } else {
+              cy.log('Date format does not match expected pattern.')
+            }
+          })
+        cy.findByText('Invalid date').should('not.exist')
     })
-  })
 })
