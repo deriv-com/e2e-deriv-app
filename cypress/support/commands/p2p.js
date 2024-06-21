f9e41ec67bfb286f911fcfaf7ccfc97fb7e63f54
maliheh <maliheh@regentmarkets.com>
2024-06-21 16:15:35 +0800

    update inputAdDetails

--- cypress/support/commands/p2p.js
+++ cypress/support/commands/p2p.js
@@ -171,62 +171,65 @@ Cypress.Commands.add(
           .should('have.value', rateValue)
       } else if (rateType == 'float') {
         if (adType == 'buy') {
-          cy.findByTestId('float_rate_type').should(
-            'have.value',
-            '-'.concat(rateValue)
-          )
+          cy.c_verifyRate()
+          cy.findByTestId('float_rate_type')
+            .click()
+            .clear()
+            .type(-rate, { parseSpecialCharSequences: false })
+            .should('have.value', (-rate).toString())
         } else if (adType == 'sell') {
-          cy.findByTestId('float_rate_type').should(
-            'have.value',
-            '+'.concat(rateValue)
-          )
+          cy.findByTestId('float_rate_type')
+            .click()
+            .clear()
+            .type(rate, { parseSpecialCharSequences: false })
+            .should('have.value', rate.toString())
         }
-      }
-      cy.findByTestId('min_transaction')
-        .type(minOrder)
-        .should('have.value', minOrder)
-      cy.findByTestId('max_transaction')
-        .type(maxOrder)
-        .should('have.value', maxOrder)
-      if (adType == 'Sell') {
-        cy.findByTestId('contact_info')
-          .type('Contact Info Block')
-          .should('have.value', 'Contact Info Block')
-      }
-      cy.findByTestId('default_advert_description')
-        .type('Description Block')
-        .should('have.value', 'Description Block')
-      cy.findByRole('button', { name: 'Next' }).should('be.enabled').click()
-      cy.findByText('Set payment details').should('be.visible')
-      cy.findByTestId('dt_dropdown_display').click()
-      cy.get('#900').should('be.visible').click()
-      if (adType == 'Sell') {
-        cy.findByTestId('dt_payment_method_card_add_icon')
-          .should('be.visible')
-          .click()
-        cy.c_addPaymentMethod(paymentIDForCopyAdSell, paymentMethod, rateType)
-        cy.findByText(paymentIDForCopyAdSell)
-          .should('exist')
-          .parent()
-          .prev()
-          .find('.dc-checkbox')
-          .and('exist')
-          .click()
+        cy.findByTestId('min_transaction')
+          .type(minOrder)
+          .should('have.value', minOrder)
+        cy.findByTestId('max_transaction')
+          .type(maxOrder)
+          .should('have.value', maxOrder)
+        if (adType == 'Sell') {
+          cy.findByTestId('contact_info')
+            .type('Contact Info Block')
+            .should('have.value', 'Contact Info Block')
+        }
+        cy.findByTestId('default_advert_description')
+          .type('Description Block')
+          .should('have.value', 'Description Block')
         cy.findByRole('button', { name: 'Next' }).should('be.enabled').click()
-        cy.findByText('Set ad conditions').should('be.visible')
-      } else if (adType == 'Buy') {
-        cy.c_PaymentMethod()
+        cy.findByText('Set payment details').should('be.visible')
+        cy.findByTestId('dt_dropdown_display').click()
+        cy.get('#900').should('be.visible').click()
+        if (adType == 'Sell') {
+          cy.findByTestId('dt_payment_method_card_add_icon')
+            .should('be.visible')
+            .click()
+          cy.c_addPaymentMethod(paymentIDForCopyAdSell, paymentMethod, rateType)
+          cy.findByText(paymentIDForCopyAdSell)
+            .should('exist')
+            .parent()
+            .prev()
+            .find('.dc-checkbox')
+            .and('exist')
+            .click()
+          cy.findByRole('button', { name: 'Next' }).should('be.enabled').click()
+          cy.findByText('Set ad conditions').should('be.visible')
+        } else if (adType == 'Buy') {
+          cy.c_PaymentMethod()
+        }
+        cy.c_verifyPostAd()
+        cy.c_verifyAdOnMyAdsScreen(
+          adType,
+          sessionStorage.getItem('c_fiatCurrency'),
+          sessionStorage.getItem('c_localCurrency'),
+          rateValue,
+          minOrder,
+          maxOrder,
+          rateType
+        )
       }
-      cy.c_verifyPostAd()
-      cy.c_verifyAdOnMyAdsScreen(
-        adType,
-        sessionStorage.getItem('c_fiatCurrency'),
-        sessionStorage.getItem('c_localCurrency'),
-        rateValue,
-        minOrder,
-        maxOrder,
-        rateType
-      )
     })
   }
 )
@@ -448,6 +451,11 @@ Cypress.Commands.add('c_verifyRate', () => {
       } else {
         throw new Error('Text does not match the expected pattern')
       }
+
+      cy.findByTestId('float_rate_type')
+        .click()
+        .clear()
+        .type(-rate, { parseSpecialCharSequences: false })
     })
 })
 
