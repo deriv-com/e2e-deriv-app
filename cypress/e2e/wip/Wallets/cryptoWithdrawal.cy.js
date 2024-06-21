1814a0db56b624239f723af9202872551f69d8d6
rostislav-deriv <rostislav@deriv.com>
2024-06-21 14:39:39 +0300

    refactor: update test file

--- cypress/e2e/wip/Wallets/cryptoWithdrawal.cy.js
+++ cypress/e2e/wip/Wallets/cryptoWithdrawal.cy.js
@@ -1,11 +1,10 @@
-import '@testing-library/cypress/add-commands'
-
 function sendWithdrawEmail() {
   if (Cypress.config('viewportWidth') < 900) {
     cy.c_switchWalletsAccountResponsive('BTC')
   } else {
     cy.c_switchWalletsAccount('BTC')
   }
+
   cy.findByText('Withdraw').should('be.visible').click()
   cy.findByText('Confirm your identity to make a withdrawal.').should(
     'be.visible'
@@ -14,39 +13,38 @@ function sendWithdrawEmail() {
     cy.findByRole('button', { name: 'Send email' }).click()
   }
   cy.contains("We've sent you an email.")
+
   cy.findByRole('button', { name: "Didn't receive the email?" }).click()
   cy.contains(/Resend email/)
 }
-function verifyEmailandPerformWithdraw(platform) {
-  if (`${platform}` == `mobile`) {
+
+function verifyEmailandPerformWithdraw(size) {
+  if (size === 'mobile') {
     cy.c_switchWalletsAccountResponsive('BTC')
   } else {
     cy.c_switchWalletsAccount('BTC')
   }
+
   cy.findByText('Withdraw').should('be.visible').click()
   cy.c_emailVerification(
     'request_payment_withdraw.html',
-    Cypress.env('walletloginEmail')
+    Cypress.env('credentials').test.walletloginEmail.ID
   )
+
   cy.then(() => {
     let verification_code = Cypress.env('walletsWithdrawalCode')
-    if (`${platform}` == `mobile`) {
-      cy.c_visitResponsive(
-        `/wallets/cashier/withdraw?verification=${verification_code}`,
-        'small'
-      )
-    } else {
-      cy.c_visitResponsive(
-        `/wallets/cashier/withdraw?verification=${verification_code}`,
-        'large'
-      )
-    }
+
+    cy.c_visitResponsive(
+      `/wallet/withdrawal?verification=${verification_code}`,
+      size
+    )
 
     cy.contains('Transaction status')
     cy.contains('Your Bitcoin cryptocurrency wallet address').click().type(
       '1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71' //Example bitcoin wallet address
     )
     cy.contains('Amount (BTC)').click().type('0.005')
+
     cy.get('.wallets-textfield__message-container')
       .eq(1)
       .invoke('text')
@@ -69,42 +67,50 @@ function verifyEmailandPerformWithdraw(platform) {
             })
         }
       })
+
     cy.get('form').findByRole('button', { name: 'Withdraw' }).click()
+
     cy.get('#modal_root, .modal-root', { timeout: 10000 }).then(() => {
       if (cy.get('.wallets-button__loader')) {
         return
       } else {
         cy.contains('0.005000000 BTC', { exact: true })
         cy.contains('Your withdrawal is currently in process')
+
         cy.findByRole('button', { name: 'Close' }).click()
         cy.contains('Please help us verify')
       }
     })
   })
 }
-describe('WALL-2830 - Crypto withdrawal send email', () => {
-  beforeEach(() => {
-    cy.c_login({ app: 'wallets' })
-  })
 
-  it('should be able to send withdrawal verification link', () => {
-    cy.log('Send Crypto Withdrawal Verification')
-    cy.c_visitResponsive('/wallets', 'large')
-    cy.contains('Wallet', { timeout: 10000 }).should('exist')
-    sendWithdrawEmail()
+describe('WALL-2830 - Crypto withdrawal send email', () => {
+  const size = ['small', 'desktop']
+
+  size.forEach((size) => {
+    it(`should be able to send withdrawal verification link on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      cy.c_login({ user: 'walletloginEmail', size })
+
+      cy.log('Send Crypto Withdrawal Verification')
+      cy.c_visitResponsive('/', size)
+      cy.contains('Wallet', { timeout: 10000 }).should('exist')
+      sendWithdrawEmail()
+    })
   })
 })
 
 describe('QATEST-98698 - Crypto withdrawal content access from email', () => {
   //Prerequisites: Crypto wallet account in qa29 with BTC balance
-  beforeEach(() => {
-    cy.c_login({ app: 'wallets' })
-  })
+  const size = ['small', 'desktop']
 
-  it('should be able to access crypto withdrawal content and perform withdrawal', () => {
-    cy.log('Access Crypto Withdrawal Content Through Email Link')
-    cy.c_visitResponsive('/wallets', 'large')
-    cy.contains('Wallet', { timeout: 10000 }).should('exist')
-    verifyEmailandPerformWithdraw('desktop')
+  size.forEach((size) => {
+    it(`should be able to access crypto withdrawal content and perform withdrawal on ${size == 'small' ? 'mobile' : 'desktop'}`, () => {
+      cy.c_login({ user: 'walletloginEmail', size })
+
+      cy.log('Access Crypto Withdrawal Content Through Email Link')
+      cy.c_visitResponsive('/', size)
+      cy.contains('Wallet', { timeout: 10000 }).should('exist')
+      verifyEmailandPerformWithdraw(size)
+    })
   })
 })
