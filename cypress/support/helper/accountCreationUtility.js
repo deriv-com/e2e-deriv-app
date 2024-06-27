7a6b8e1986ab04cce7b0b8e4eb4a5115489a0b21
bertrand <bertrand@regentmarkets.com>
2024-06-27 14:54:38 +0200

    Final destructuring

--- cypress/support/helper/accountCreationUtility.js
+++ cypress/support/helper/accountCreationUtility.js
@@ -1,17 +1,29 @@
 require('dotenv').config()
 const data = require('../../fixtures/common/common.json')
 const {
-  new_account_real,
-  non_pep_declaration,
-  account_opening_reason,
-  address_city,
-} = data.defaultCRDetails
-
-const {
   emailGenerator,
   numbersGenerator,
   nameGenerator,
 } = require('./commonJsUtility')
+
+const {
+  new_account_real,
+  non_pep_declaration,
+  account_opening_reason,
+  country_code: defaultCountryCode,
+  currency: defaultCurrency,
+  first_name: defaultFirstName,
+  date_of_birth: defaultDateOfBirth,
+  address_line_1: defaultAddressLine1,
+  address_city: defaultAddressCity,
+  tax_residence: defaultTaxResidence,
+} = data.defaultCRDetails
+
+const {
+  new_account_virtual,
+  type,
+  country_code: DefaultVRCountryCode,
+} = data.defaultVRDetails
 
 const verifyEmail = async (api) => {
   const randomEmail = emailGenerator()
@@ -24,15 +36,15 @@ const verifyEmail = async (api) => {
 
 const createAccountVirtual = async (api, clientData = {}) => {
   const {
-    country_code = data.defaultVRDetails.country_code,
-    password = process.env.E2E_DERIV_PASSWORD,
+    country_code = DefaultVRCountryCode,
+    client_password = process.env.E2E_DERIV_PASSWORD,
   } = clientData
   try {
-    if (!password) throw new Error(`Password not on file`)
+    if (!client_password) throw new Error(`Password not on file`)
     const response = await api.basic.newAccountVirtual({
-      new_account_virtual: data.defaultVRDetails.new_account_virtual,
-      type: data.defaultVRDetails.type,
-      client_password: password,
+      new_account_virtual,
+      type,
+      client_password,
       residence: country_code,
       verification_code: process.env.E2E_EMAIL_VERIFICATION_CODE,
     })
@@ -51,22 +63,23 @@ const createAccountVirtual = async (api, clientData = {}) => {
 
 const createAccountReal = async (api, clientData = {}) => {
   const {
-    country_code = data.defaultCRDetails.country_code,
-    currency = data.defaultCRDetails.currency,
-    first_name = data.defaultCRDetails.first_name,
+    country_code = defaultCountryCode,
+    currency = defaultCurrency,
+    first_name = defaultFirstName,
     last_name = nameGenerator(),
-    date_of_birth = data.defaultCRDetails.date_of_birth,
+    date_of_birth = defaultDateOfBirth,
     phone = `+${numbersGenerator()}`,
-    address_line_1 = data.defaultCRDetails.address_line_1,
-    tax_residence = data.defaultCRDetails.tax_residence,
+    address_line_1 = defaultAddressLine1,
+    address_city = defaultAddressCity,
+    tax_residence = defaultTaxResidence,
     tax_identification_number = `${numbersGenerator()}`,
   } = clientData
   try {
-    const resp = await createAccountVirtual(api, { country_code: country_code })
+    const resp = await createAccountVirtual(api, { country_code })
     const { oauthToken } = resp
     await api.account(oauthToken) // API authentication
 
-    const clientDetails = {
+    const clientDetailsPayload = {
       new_account_real,
       non_pep_declaration,
       account_opening_reason,
@@ -78,12 +91,12 @@ const createAccountReal = async (api, clientData = {}) => {
       date_of_birth,
       phone,
       address_line_1,
-      address_city: address_city,
+      address_city,
       tax_residence,
       tax_identification_number,
     }
 
-    const response = await api.basic.newAccountReal(clientDetails)
+    const response = await api.basic.newAccountReal(clientDetailsPayload)
     const {
       new_account_real: { client_id },
       echo_req: { residence },
