5204d38ca9daa7ed4d143598d48aaa5de26a7b32
bertrand <bertrand@regentmarkets.com>
2024-06-27 15:44:31 +0200

    Create MF account creation function

--- cypress/support/helper/accountCreationUtility.js
+++ cypress/support/helper/accountCreationUtility.js
@@ -7,23 +7,48 @@ const {
 } = require('./commonJsUtility')
 
 const {
-  new_account_real,
   non_pep_declaration,
   account_opening_reason,
-  country_code: defaultCountryCode,
   currency: defaultCurrency,
   first_name: defaultFirstName,
   date_of_birth: defaultDateOfBirth,
   address_line_1: defaultAddressLine1,
   address_city: defaultAddressCity,
   tax_residence: defaultTaxResidence,
-} = data.defaultCRDetails
+} = data.commonDefaults
 
 const {
   new_account_virtual,
   type,
-  country_code: DefaultVRCountryCode,
+  country_code: defaultVRCountryCode,
 } = data.defaultVRDetails
+
+const { new_account_real, country_code: defaultCRCountryCode } =
+  data.defaultCRDetails
+
+const {
+  new_account_maltainvest,
+  accept_risk,
+  country_code: defaultMFCountryCode,
+  salutation,
+  education_level,
+  occupation,
+  employment_status,
+  employment_industry,
+  income_source,
+  net_income,
+  estimated_worth,
+  risk_tolerance,
+  source_of_experience,
+  trading_experience_financial_instruments,
+  trading_frequency_financial_instruments,
+  cfd_experience,
+  cfd_frequency,
+  cfd_trading_definition,
+  leverage_impact_trading,
+  leverage_trading_high_risk_stop_loss,
+  required_initial_margin,
+} = data.defaultMFDetails
 
 const verifyEmail = async (api) => {
   const randomEmail = emailGenerator()
@@ -36,7 +61,7 @@ const verifyEmail = async (api) => {
 
 const createAccountVirtual = async (api, clientData = {}) => {
   const {
-    country_code = DefaultVRCountryCode,
+    country_code = defaultVRCountryCode,
     client_password = process.env.E2E_DERIV_PASSWORD,
   } = clientData
   try {
@@ -63,7 +88,7 @@ const createAccountVirtual = async (api, clientData = {}) => {
 
 const createAccountReal = async (api, clientData = {}) => {
   const {
-    country_code = defaultCountryCode,
+    country_code = defaultCRCountryCode,
     currency = defaultCurrency,
     first_name = defaultFirstName,
     last_name = nameGenerator(),
@@ -111,4 +136,79 @@ const createAccountReal = async (api, clientData = {}) => {
   }
 }
 
-module.exports = { createAccountReal, createAccountVirtual, verifyEmail }
+const createAccountMF = async (api, clientData = {}) => {
+  const {
+    country_code = defaultMFCountryCode,
+    currency = defaultCurrency,
+    first_name = defaultFirstName,
+    last_name = nameGenerator(),
+    date_of_birth = defaultDateOfBirth,
+    phone = `+${numbersGenerator()}`,
+    address_line_1 = defaultAddressLine1,
+    address_city = defaultAddressCity,
+    tax_residence = defaultTaxResidence,
+    tax_identification_number = `${numbersGenerator()}`,
+  } = clientData
+  try {
+    const resp = await createAccountVirtual(api, { country_code })
+    const { oauthToken } = resp
+    await api.account(oauthToken) // API authentication
+
+    const clientDetailsPayload = {
+      new_account_real,
+      non_pep_declaration,
+      account_opening_reason,
+      currency,
+      place_of_birth: country_code,
+      residence: country_code,
+      first_name,
+      last_name,
+      date_of_birth,
+      phone,
+      address_line_1,
+      address_city,
+      tax_residence,
+      tax_identification_number,
+      new_account_maltainvest,
+      accept_risk,
+      salutation,
+      education_level,
+      occupation,
+      employment_status,
+      employment_industry,
+      income_source,
+      net_income,
+      estimated_worth,
+      risk_tolerance,
+      source_of_experience,
+      trading_experience_financial_instruments,
+      trading_frequency_financial_instruments,
+      cfd_experience,
+      cfd_frequency,
+      cfd_trading_definition,
+      leverage_impact_trading,
+      leverage_trading_high_risk_stop_loss,
+      required_initial_margin,
+    }
+
+    const response = await api.basic.newAccountMaltainvest(clientDetailsPayload)
+    const {
+      new_account_real: { client_id },
+      echo_req: { residence },
+    } = response
+    return {
+      clientID: client_id,
+      residence: residence,
+    }
+  } catch (e) {
+    console.error('Operation failed', e)
+    throw e
+  }
+}
+
+module.exports = {
+  createAccountMF,
+  createAccountReal,
+  createAccountVirtual,
+  verifyEmail,
+}
