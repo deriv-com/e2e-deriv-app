cfb15292eba66a77c1d466b2375ab104272686e3
jyothi-deriv <jyothi@regentmarkets.com>
2024-06-28 11:57:46 +0400

    account closure transactional email

--- cypress/e2e/smoke/transactionalEmails/accountClosure.cy.js
+++ cypress/e2e/smoke/transactionalEmails/accountClosure.cy.js
@@ -3,15 +3,18 @@ describe('QATEST-162618 - Verify account_closure are triggered as transactional 
     cy.c_createRealAccount()
     cy.c_login()
   })
-  it(`Verify account_closure are triggered as transactional emails on Mobile`, () => {
+  it(`should verify account_closure are triggered as transactional emails on Mobile`, () => {
     cy.c_visitResponsive('account/closing-account', 'small')
+
     cy.findByRole('button', { name: 'Close my account' }).click()
     cy.findByText('I’m closing my account for other reasons.').click()
     cy.findByRole('button', { name: 'Continue' }).click()
     cy.findByRole('button', { name: 'Close account' }).click()
+
     cy.c_emailContentVerification(
       'CustomerIO_account_closure.html',
-      Cypress.env('credentials').test.masterUser.ID
+      Cypress.env('credentials').test.masterUser.ID,
+      'account_closure'
     )
   })
 })
--- cypress/support/commands/transactionalEmail.js
+++ cypress/support/commands/transactionalEmail.js
@@ -1,9 +1,10 @@
 Cypress.Commands.add(
   'c_emailContentVerification',
-  (requestType, accountEmail) => {
+  (requestType, accountEmail, transactionalMessageId) => {
     const baseUrl = Cypress.env('configServer') + '/events'
     cy.log(accountEmail)
     cy.log(`Visit ${baseUrl}`)
+
     cy.visit(
       `https://${Cypress.env('qaBoxLoginEmail')}:${Cypress.env(
         'qaBoxLoginPassword'
@@ -16,26 +17,35 @@ Cypress.Commands.add(
         { log: false }
       )}@${baseUrl}`,
       {
-        args: [requestType, accountEmail],
+        args: [requestType, accountEmail, transactionalMessageId],
       },
-      ([requestType, accountEmail]) => {
+      ([requestType, accountEmail, transactionalMessageId]) => {
         cy.document().then((doc) => {
           const allRelatedEmails = Array.from(
             doc.querySelectorAll(`a[href*="${requestType}"]`)
           )
+
           if (allRelatedEmails.length) {
             const verificationEmail = allRelatedEmails.pop()
             cy.wrap(verificationEmail).click()
-            cy.get('p')
-              .filter(`:contains('${accountEmail}')`)
-              .last()
-              .should('be.visible')
-              .siblings()
-              .should('contain', 'transactional_message_id: account_closure')
+
+            verifyEmailContent(accountEmail)
           } else {
             cy.log('email not found')
           }
         })
+
+        function verifyEmailContent(accountEmail) {
+          cy.get('p')
+            .filter(`:contains('${accountEmail}')`)
+            .last()
+            .should('be.visible')
+            .siblings()
+            .should(
+              'contain',
+              `transactional_message_id: ${transactionalMessageId}`
+            )
+        }
       }
     )
   }
