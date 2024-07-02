63289f9c7faab097e7d1f4754e34f19289489b11
Paveetra <paveetra@regentmarkets.com>
2024-07-02 14:28:24 +0800

    [ci skip]

--- cypress/e2e/smoke/dbot/DashboardQuickAction.cy.js
+++ cypress/e2e/smoke/dbot/DashboardQuickAction.cy.js
@@ -1,24 +1,36 @@
 import BotDashboard from '../../../support/pageobjects/dbot/bot_dashboard_page'
 import QuickStrategy from '../../../support/pageobjects/dbot/quick_strategy'
+
 describe('QATEST-4128: Dashboard quick action to Quick Strategy and Bot Builder', () => {
   const botDashboard = new BotDashboard()
   const quickStrategy = new QuickStrategy()
 
-  beforeEach(() => {
-    cy.c_visitResponsive('/bot', 'large')
-    cy.c_skipTour()
-    cy.c_closeNotificationHeader()
+  const sizes = ['small', 'large']
+
+  sizes.forEach((size) => {
+    it(`Go to Bot Builder on ${size === 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size === 'small' ? true : false
+      cy.c_visitResponsive('/bot', size)
+      if (isMobile) {
+        cy.findByTestId('close-icon', { timeout: 7000 }).click()
+      }
+      cy.c_skipTour()
+      botDashboard.openQsQaction()
+      botDashboard.botBuilderActiveTab.should('exist')
+    })
   })
 
-  it('Go to bot builder page', () => {
-    botDashboard.openBotBuilderQaction()
-    cy.c_skipTour()
-    botDashboard.botBuilderActiveTab.should('exist')
-  })
-
-  it('Open quick strategy modal', () => {
-    botDashboard.openQsQaction()
-    botDashboard.botBuilderActiveTab.should('exist')
-    quickStrategy.quickStrategyMarketDropdown.should('be.visible')
+  sizes.forEach((size) => {
+    it(`Go to Quick Strategy on ${size === 'small' ? 'mobile' : 'desktop'}`, () => {
+      const isMobile = size === 'small' ? true : false
+      cy.c_visitResponsive('/bot', size)
+      if (isMobile) {
+        cy.findByTestId('close-icon', { timeout: 7000 }).click()
+      }
+      cy.c_skipTour()
+      botDashboard.openQsQaction()
+      botDashboard.botBuilderActiveTab.should('exist')
+      quickStrategy.quickStrategyMarketDropdown.should('be.visible')
+    })
   })
 })
