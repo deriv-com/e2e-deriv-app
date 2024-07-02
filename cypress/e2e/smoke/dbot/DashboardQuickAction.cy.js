ff8d42ddb79dc505afd1833f0dacacf43627b1e0
Paveetra <paveetra@regentmarkets.com>
2024-07-02 14:33:13 +0800

    [ci skip]

--- cypress/e2e/smoke/dbot/DashboardQuickAction.cy.js
+++ cypress/e2e/smoke/dbot/DashboardQuickAction.cy.js
@@ -4,12 +4,11 @@ import QuickStrategy from '../../../support/pageobjects/dbot/quick_strategy'
 describe('QATEST-4128: Dashboard quick action to Quick Strategy and Bot Builder', () => {
   const botDashboard = new BotDashboard()
   const quickStrategy = new QuickStrategy()
-
   const sizes = ['small', 'large']
 
   sizes.forEach((size) => {
     it(`Go to Bot Builder on ${size === 'small' ? 'mobile' : 'desktop'}`, () => {
-      const isMobile = size === 'small' ? true : false
+      const isMobile = size === 'small'
       cy.c_visitResponsive('/bot', size)
       if (isMobile) {
         cy.findByTestId('close-icon', { timeout: 7000 }).click()
@@ -22,7 +21,7 @@ describe('QATEST-4128: Dashboard quick action to Quick Strategy and Bot Builder'
 
   sizes.forEach((size) => {
     it(`Go to Quick Strategy on ${size === 'small' ? 'mobile' : 'desktop'}`, () => {
-      const isMobile = size === 'small' ? true : false
+      const isMobile = size === 'small'
       cy.c_visitResponsive('/bot', size)
       if (isMobile) {
         cy.findByTestId('close-icon', { timeout: 7000 }).click()
